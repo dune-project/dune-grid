a347aed6964874e23e38c5147e827691745dfb90
Robert Klöfkorn <robertk@dune-project.org>
2006-05-24 13:42:30 +0000

    files for SGrid. Somehow forgot?
    [[Imported from SVN: r2678]]

---
+++ grid/io/file/dgfparser/dgfs.cc
@@ -0,0 +1,36 @@
+// -*- tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 2 -*-
+// vi: set et ts=4 sw=2 sts=2:
+namespace Dune {
+  template <int dim,int dimworld>
+  inline SGrid<dim,dimworld>*
+  MacroGrid ::
+  Impl< SGrid<dim,dimworld> > ::
+  generate(MacroGrid& mg,const char* filename, int MPICOMM) {
+    mg.element=Cube;
+    std::ifstream gridin(filename);
+    IntervalBlock interval(gridin);
+    if(!interval.ok()) {
+      std::cerr << "Macrofile " << filename << " must have Intervall-Block "
+                << "to be used to initialize SGrid!\n"
+                << "No alternative File-Format defined" << std::endl;
+      abort();
+    }
+    mg.dimw = interval.dimw();
+    if (mg.dimw != dimworld) {
+      std::cerr << "ERROR: "
+                << "Macrofile " << filename << " is for dimension " << mg.dimw
+                << " and connot be used to initialize an SGrid of dimension "
+                << dimworld << std::endl;
+      abort();
+    }
+    FieldVector<double,dimworld> start;
+    FieldVector<double,dimworld> lang;
+    FieldVector<int,dimworld>    anz;
+    for (int i=0; i<dimworld; i++) {
+      start[i] = interval.start(i);
+      lang[i] = interval.length(i);
+      anz[i]  = interval.segments(i);
+    }
+    return new SGrid<dim,dimworld>(anz,start,lang);
+  }
+}
---
+++ grid/io/file/dgfparser/dgfs.hh
@@ -0,0 +1,16 @@
+// -*- tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 2 -*-
+// vi: set et ts=4 sw=2 sts=2:
+#ifndef DUNE_DGFPARSERS_HH
+#define DUNE_DGFPARSERS_HH
+#include <dune/grid/sgrid.hh>
+#include "dgfparser.hh"
+namespace Dune {
+  template <int dim,int dimworld>
+  class MacroGrid::Impl< SGrid<dim,dimworld> > {
+  public:
+    static SGrid<dim,dimworld>* generate(MacroGrid& mg,
+                                         const char* filename,int MPICOMM=-1);
+  };
+}
+#include "dgfs.cc"
+#endif
