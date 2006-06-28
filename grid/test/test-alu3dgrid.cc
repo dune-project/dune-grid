e379af17550c5bac3e84b0138ec3aa093ee9c619
Robert Klöfkorn <robertk@dune-project.org>
2006-06-28 14:40:51 +0000

    minor changes.
    [[Imported from SVN: r2760]]

--- grid/test/test-alu3dgrid.cc
+++ grid/test/test-alu3dgrid.cc
@@ -50,18 +50,21 @@ void makeNonConfGrid(GridType &grid,int level,int adapt) {
   grid.loadBalance();
   grid.globalRefine(level);
   grid.loadBalance();
-  for (int i=0; i<adapt; i++) {
-    if (myrank==0) {
+  for (int i=0; i<adapt; i++)
+  {
+    if (myrank==0)
+    {
       typedef typename GridType :: template Codim<0> ::
       template Partition<Interior_Partition> :: LeafIterator LeafIterator;
+
       LeafIterator endit = grid.template leafend<0,Interior_Partition>   ();
       int nr = 0;
       int size = grid.size(0);
       for(LeafIterator it    = grid.template leafbegin<0,Interior_Partition> ();
-          it != endit ; ++it,nr++ ) {
+          it != endit ; ++it,nr++ )
+      {
         grid.mark(1,it);
-        if (nr>size*0.8)
-          break;
+        if (nr>size*0.8) break;
       }
     }
     grid.adapt();
@@ -100,7 +103,10 @@ void checkALUParallel(GridType & grid, int gref, int mxl = 3)
 #else
 template <class GridType>
 void checkALUParallel(GridType & grid, int gref, int mxl = 3)
-{}
+{
+  grid.globalIdSet();
+  checkALUSerial(grid,2);
+}
 #endif
 
 int main (int argc , char **argv) {
@@ -114,6 +120,7 @@ int main (int argc , char **argv) {
       // check empty grid
       if (myrank == 0)
         std::cout << "Check empty grids" << std::endl;
+
       {
         ALUCubeGrid<3,3> grid(MPI_COMM_WORLD);
         checkALUSerial(grid);
@@ -127,7 +134,7 @@ int main (int argc , char **argv) {
          {
          std::string filename("alu-testgrid.triang");
          ALUSimplexGrid<2,2> grid(filename);
-         checkALUSerial(grid,0);
+         checkALUSerial(grid,2);
          }
        */
 
