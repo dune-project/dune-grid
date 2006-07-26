b56c74574b7aa2fa0e6388e1487de2e285640219
Robert Klöfkorn <robertk@dune-project.org>
2006-07-26 11:35:44 +0000

    minor change.
    [[Imported from SVN: r2877]]

--- grid/common/datahandleif.hh
+++ grid/common/datahandleif.hh
@@ -1,7 +1,7 @@
 // -*- tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 2 -*-
 // vi: set et ts=4 sw=2 sts=2:
-#ifndef DUNE_DATAHANDLE_HH
-#define DUNE_DATAHANDLE_HH
+#ifndef DUNE_DATAHANDLEIF_HH
+#define DUNE_DATAHANDLEIF_HH
 
 /** @file
    @author FR-dune
