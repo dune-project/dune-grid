807615f369e98f73c25febcee4366e9e9eae2565
Robert Klöfkorn <robertk@dune-project.org>
2006-07-28 11:18:39 +0000

    if readDuneGrid failed, execption is thrown.
    [[Imported from SVN: r2915]]

--- grid/io/file/dgfparser/dgfug.cc
+++ grid/io/file/dgfparser/dgfug.cc
@@ -10,35 +10,40 @@ namespace Dune {
 
     std::string str(filename);
 
-    mg.readDuneGrid(gridin);
-    if (mg.dimw != dimworld) {
-      DUNE_THROW(DGFException,
-                 "Macrofile " << filename << " is for dimension " << mg.dimw
-                              << " and connot be used to initialize an "
-                              << "UGGrid of dimension "
-                              << dimworld);
+    if(mg.readDuneGrid(gridin) == 1)
+    {
+      if (mg.dimw != dimworld) {
+        DUNE_THROW(DGFException,
+                   "Macrofile " << filename << " is for dimension " << mg.dimw
+                                << " and connot be used to initialize an "
+                                << "UGGrid of dimension "
+                                << dimworld);
+      }
+      mg.setOrientation(0);
+      UGGrid<dim,dimworld> *grid = new UGGrid<dim,dimworld>();
+      grid->createBegin();
+      for (int n=0; n<mg.nofvtx; n++) {
+        FieldVector<double,dimworld> v;
+        for (int j=0; j<dimworld; j++)
+          v[j] = mg.vtx[n][j];
+        grid->insertVertex(v);
+      }
+      for (int n=0; n<mg.nofelements; n++) {
+        std::vector<unsigned int> el;
+        for (size_t j=0; j<mg.elements[n].size(); j++)
+          el.push_back((mg.elements[n][j]));
+        if ((int) el.size()==mg.dimw+1)
+          grid->insertElement(GeometryType(GeometryType::simplex,dimworld),el);
+        else if (el.size()==pow(2,mg.dimw))
+          grid->insertElement(GeometryType(GeometryType::cube,dimworld),el);
+        else
+          DUNE_THROW(DGFException, "Wrong number of verticies for element");
+      }
+      grid->createEnd();
+      return grid;
     }
-    mg.setOrientation(0);
-    UGGrid<dim,dimworld> *grid = new UGGrid<dim,dimworld>();
-    grid->createBegin();
-    for (int n=0; n<mg.nofvtx; n++) {
-      FieldVector<double,dimworld> v;
-      for (int j=0; j<dimworld; j++)
-        v[j] = mg.vtx[n][j];
-      grid->insertVertex(v);
-    }
-    for (int n=0; n<mg.nofelements; n++) {
-      std::vector<unsigned int> el;
-      for (int j=0; j<mg.elements[n].size(); j++)
-        el.push_back((mg.elements[n][j]));
-      if (el.size()==mg.dimw+1)
-        grid->insertElement(GeometryType(GeometryType::simplex,dimworld),el);
-      else if (el.size()==pow(2,mg.dimw))
-        grid->insertElement(GeometryType(GeometryType::cube,dimworld),el);
-      else
-        DUNE_THROW(DGFException, "Wrong no. of verticies for element");
-    }
-    grid->createEnd();
-    return grid;
+    DUNE_THROW(DGFException,"Macrofile " << filename << " is not a valid DGF file\n"
+                                         << "No alternative File-Format implemented!");
+    return 0;
   }
 }
