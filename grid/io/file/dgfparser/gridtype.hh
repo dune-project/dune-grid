088e0914050f285c2158862baa1b17de6d749a49
Robert Klöfkorn <robertk@dune-project.org>
2006-08-03 14:42:27 +0000

    Docu for how-to use GRIDDIM and GRIDTYPE by configuring Dune with .--with-grid-dim and --with-grid-type. These are disabled by default and will not barther anybody who want's not use this feature.
    [[Imported from SVN: r2992]]

--- grid/io/file/dgfparser/gridtype.hh
+++ grid/io/file/dgfparser/gridtype.hh
@@ -11,9 +11,9 @@
  *#if defined ALBERTAGRID && HAVE_ALBERTA
    typedef Dune::AlbertaGrid<dimworld,dimworld> GridType;
  *#elif defined ALUGRID_CUBE && HAVE_ALUGRID
-   typedef Dune::ALU3dGrid<dimworld,dimworld,Dune::hexa> GridType;
+   typedef Dune::ALUCubeGrid<dimworld,dimworld> GridType;
  *#elif defined ALUGRID_SIMPLEX && HAVE_ALUGRID
-   typedef Dune::ALU3dGrid<dimworld,dimworld,Dune::tetra> GridType;
+   typedef Dune::ALUSimplexGrid<dimworld,dimworld> GridType;
  *#elif defined ONEDGRID
    typedef Dune::OneDGrid<1,1> GridType;
  *#elif defined SGRID
@@ -21,12 +21,13 @@
  *#elif defined YASPGRID
    typedef Dune::YaspGrid<dimworld,dimworld> GridType;
  *#else
- *#define YASPGRID
+   // default GridType is YaspGrid
    typedef Dune::YaspGrid<dimworld,dimworld> GridType;
  *#endif
  * @endcode
  * The variable dimworld is determined by
  * @code
+   // default value is 2
    const int dimworld = GRIDDIM;
  * @endcode
  * Remark:
@@ -34,12 +35,16 @@
  * -# For \c ALBERTAGRID or \c ALUGRID with \c dimworld=1 Dune::OneDGrid is used.
 
  * To reduce differences between seriell and parallel runs as much as possible
- * additional the MPIHelper class is used to toggle between serial and parallel
+ * additional the Dune::MPIHelper class is used to toggle between serial and parallel
  * runs via specialization of this class for runs using MPI und such that
  * doesn't.
  * To use this feature, the following code should always be called at the beginning
  * of the function main:
  * @code
+ *#include <dune/grid/io/file/dgfparser/gridtype.hh>
+
+   ...
+
    int main(int argc, char ** argv, char ** envp) {
 
    // get reference to the singelton MPIHelper
@@ -60,16 +65,37 @@
    // MPI_Finalize() command is called.
    }
  * @endcode
+ * To make use of this feature, one has to configure DUNE by using the
+ * \c --with-grid-dim=1|2|3 and optional --with-grid-type=ALBERTAGRID |
+ * ALUGRID_CUBE | ALUGRID_SIMPLEX | ONEDGRID | SGRID | UGGRID | YASPGRID.
+ * This will add the following to the ALL_PKG_CPPFLAGS
+ * @code
+ * -DGRIDDIM=$(GRIDDIM) -DGRIDTYPE=$(GRIDTYPE)
+ * @endcode
+ * No by adding the \c ALL_PKG_CPPFLAGS or \c GRIDDIM_CPPFLAGS to the
+ * programs \c CXXFLAGS one can choose the grids dimension and type by
+ * invoking the following make command
+ * @code
+ *  make GRIDDIM=3 GRIDTYPE=ALBERTAGRID myprogram
+ * @endcode
+ * Here the value \c 3 for grid dimension and \c ALBERTAGRID for grid type are passed as
+ * pre-processoer variables to gridtype.hh and are then used to define the \c typedef \c
+ * GridType.
+ *
  * @author Andreas Dedner
  */
 
-
+#ifndef GRIDDIM
+  #warning --- No GRIDDIM defined, defaulting to 2
+const int dimworld = 2;
+#else
 const int dimworld = GRIDDIM;
+#endif
 
 #if defined ALBERTAGRID && HAVE_ALBERTA
   #if GRIDDIM == 1
     #include "dgfoned.hh"
-typedef Dune::OneDGrid<1,1> GridType;
+typedef Dune::OneDGrid<dimworld,dimworld> GridType;
   #else
     #include "dgfalberta.hh"
 typedef Dune::AlbertaGrid<dimworld,dimworld> GridType;
@@ -110,7 +136,7 @@ const int refStepsForHalf = 1;
 typedef Dune::YaspGrid<dimworld,dimworld> GridType;
 const int refStepsForHalf = 1;
 #else
-  #define YASPGRID
+//#define YASPGRID
   #include "dgfyasp.hh"
 typedef Dune::YaspGrid<dimworld,dimworld> GridType;
 const int refStepsForHalf = 1;
