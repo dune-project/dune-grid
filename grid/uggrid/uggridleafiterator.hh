261a2d9e92f351b508fdabea93f7a85251ff61da
Oliver Sander <sander@dune-project.org>
2006-08-09 09:07:28 +0000

    don't take level information from an invalid entitypointer
    [[Imported from SVN: r3031]]

--- grid/uggrid/uggridleafiterator.hh
+++ grid/uggrid/uggridleafiterator.hh
@@ -56,16 +56,20 @@ namespace Dune {
 
     /** \brief This increment makes the iterator wander over all entities on all levels */
     void globalIncrement() {
+
+      // Store the current level, so we know it even when pointing to an invalid entity
+      int level = this->level();
+
       // Increment on this level
       this->virtualEntity_.setToTarget(UG_NS<GridImp::dimension>::succ(this->virtualEntity_.getTarget()));
 
       // If beyond the end of this level set to first of next level
-      if (!this->virtualEntity_.getTarget() && this->level() < grid_->maxLevel()) {
+      if (!this->virtualEntity_.getTarget() && level < grid_->maxLevel()) {
 
         if (pitype==All_Partition || pitype==Ghost_Partition)
-          setToTarget(UG_NS<dim>::PFirstNode(grid_->multigrid_->grids[this->level()+1]), this->level()+1);
+          setToTarget(UG_NS<dim>::PFirstNode(grid_->multigrid_->grids[level+1]), level+1);
         else
-          setToTarget(UG_NS<dim>::FirstNode(grid_->multigrid_->grids[this->level()+1]), this->level()+1);
+          setToTarget(UG_NS<dim>::FirstNode(grid_->multigrid_->grids[level+1]), level+1);
 
       }
 
@@ -121,16 +125,20 @@ namespace Dune {
 
     /** \brief This increment makes the iterator wander over all entities on all levels */
     void globalIncrement() {
+
+      // Store the current level, so we know it even when pointing to an invalid entity
+      int level = this->level();
+
       // Increment on this level
       this->virtualEntity_.setToTarget(UG_NS<GridImp::dimension>::succ(this->virtualEntity_.getTarget()));
 
       // If beyond the end of this level set to first of next level
-      if (!this->virtualEntity_.getTarget() && this->level() < grid_->maxLevel()) {
+      if (!this->virtualEntity_.getTarget() && level < grid_->maxLevel()) {
 
         if (pitype==All_Partition || pitype==Ghost_Partition)
-          setToTarget(UG_NS<dim>::PFirstElement(grid_->multigrid_->grids[this->level()+1]), this->level()+1);
+          setToTarget(UG_NS<dim>::PFirstElement(grid_->multigrid_->grids[level+1]), level+1);
         else
-          this->setToTarget(UG_NS<dim>::FirstElement(grid_->multigrid_->grids[this->level()+1]), this->level()+1);
+          this->setToTarget(UG_NS<dim>::FirstElement(grid_->multigrid_->grids[level+1]), level+1);
 
       }
 
