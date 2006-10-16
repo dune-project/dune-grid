5d3d43c56afa4be59242c764c417fe3a248b490d
Oliver Sander <sander@dune-project.org>
2006-10-16 08:08:44 +0000

    cleaned up the test code and made it more thorough
    [[Imported from SVN: r3173]]

--- grid/test/checkintersectionit.cc
+++ grid/test/checkintersectionit.cc
@@ -5,9 +5,82 @@
 
 #include <set>
 
+#include <dune/grid/common/quadraturerules.hh>
+
 /** \file
     \brief Tests for the IntersectionIterator
  */
+
+/** \brief Helper routine: Test a general geometry
+ */
+
+template <class GeometryImp>
+void checkGeometry(const GeometryImp& geometry)
+{
+  using namespace Dune;
+
+  // Get the dimensions and types
+  const int dim      = GeometryImp::mydimension;
+  const int dimworld = GeometryImp::coorddimension;
+
+  typedef typename GeometryImp::ctype ctype;
+
+  // Get the corresponding reference element
+  const ReferenceElement<double,dim>& refElement
+    = ReferenceElements<double, dim>::general(geometry.type());
+
+  // Check whether the number of corners is correct
+  if (geometry.corners() != refElement.size(dim))
+    DUNE_THROW(GridError, "Geometry has wrong number of corners!");
+
+  // check consistency between operator[] and global()
+  for (int i=0; i<refElement.size(dim); i++) {
+    FieldVector<double,dim> localPos = refElement.position(i,dim);
+    if ( (geometry[i] - geometry.global(localPos)).infinity_norm() > 1e-6)
+      DUNE_THROW(GridError, "Methods operator[] and global() are inconsistent!");
+  }
+
+  // Compute the element center just to have an argument for the following methods
+  FieldVector<ctype, dimworld> center(0);
+  for (int i=0; i<geometry.corners(); i++)
+    center += geometry[i];
+
+  center /= geometry.corners();
+
+#ifdef DUNE_UGGRID_HH
+#warning Test for quadrilateral intersections disabled!
+  if (!(geometry.type().isQuadrilateral() && dimworld==3)) {
+#endif
+  // The geometry center in local coordinates
+  FieldVector<ctype, dim> localCenter = geometry.local(center);
+
+  // Check whether center is within the intersection
+  // This implicitly assumes convex intersections
+  if (!geometry.checkInside(localCenter))
+    DUNE_THROW(GridError, "Center of geometry is not within geometry!");
+
+  // Back to global coordinates to check for correctness
+  FieldVector<ctype, dimworld> worldCenter = geometry.global(localCenter);
+  if ((center-worldCenter).infinity_norm() > 1e-6)
+    DUNE_THROW(GridError, "local() and global() are not inverse to each other!");
+
+  // The integration element at the element center
+  ctype intElement = geometry.integrationElement(localCenter);
+  if (intElement <=0)
+    DUNE_THROW(GridError, "nonpositive integration element found!");
+
+#if 0
+  // This method exists in the interface, but it is not expected to work
+  // unless dim==dimworld
+  const FieldMatrix<ctype, Geometry::mydimension, Geometry::mydimension> jacobi
+    = intersectionGlobal.jacobianInverseTransposed(localCenter);
+#endif
+#ifdef DUNE_UGGRID_HH
+}
+#endif
+
+
+}
 
 /** \brief Test the IntersectionIterator
  */
@@ -59,64 +132,33 @@ void checkIntersectionIter(const GridType & grid, const IndexSet& indexSet,
     typedef typename IntersectionIterator::Geometry Geometry;
     const Geometry& intersectionGlobal = iIt.intersectionGlobal();
 
-    if (intersectionGlobal.corners() <= 0)
-      DUNE_THROW(GridError, "Global intersection has nonpositive number of corners!");
-
-    // Compute the element center just to have an argument for the following methods
-    FieldVector<ctype, Geometry::coorddimension> center(0);
-    for (int j=0; j<intersectionGlobal.corners(); j++)
-      center += intersectionGlobal[j];
-
-    center /= intersectionGlobal.corners();
-
-#ifdef DUNE_UGGRID_HH
-#warning Test for quadrilateral intersections disabled!
-    if (!intersectionGlobal.type().isQuadrilateral()) {
-#endif
-    // The geometry center in local coordinates
-    FieldVector<ctype, Geometry::mydimension> localCenter = intersectionGlobal.local(center);
-
-    // Check whether center is within the intersection
-    // This implicitly assumes convex intersections
-    if (!intersectionGlobal.checkInside(localCenter))
-      DUNE_THROW(GridError, "Center of intersectionGlobal is not within intersectionGlobal!");
-
-    // Back to global coordinates to check for correctness
-    FieldVector<ctype, Geometry::coorddimension> worldCenter = intersectionGlobal.global(localCenter);
-    if ((center-worldCenter).infinity_norm() > 1e-6)
-      DUNE_THROW(GridError, "local() and global() are not inverse to each other!");
-
-
-    // The integration element at the element center
-    ctype intElement = intersectionGlobal.integrationElement(localCenter);
-    if (intElement <=0)
-      DUNE_THROW(GridError, "nonpositive integration element found!");
-
-#if 0
-    // This method exists in the interface, but it is not expected to work
-    const FieldMatrix<ctype, Geometry::mydimension, Geometry::mydimension> jacobi
-      = intersectionGlobal.jacobianInverseTransposed(localCenter);
-#endif
-#ifdef DUNE_UGGRID_HH
-  }
-#endif
+    checkGeometry(intersectionGlobal);
 
     // //////////////////////////////////////////////////////////
     //   Check the geometry returned by intersectionSelfLocal()
     // //////////////////////////////////////////////////////////
 
-    {
-      const typename IntersectionIterator::LocalGeometry& intersectionSelfLocal = iIt.intersectionSelfLocal();
-      if (intersectionSelfLocal.corners() <= 0)
-        DUNE_THROW(GridError, "Local intersection has nonpositive number of corners!");
+    const typename IntersectionIterator::LocalGeometry& intersectionSelfLocal = iIt.intersectionSelfLocal();
+    checkGeometry(intersectionSelfLocal);
 
-      // check points of intersection self
-      for(int k=0; k<intersectionSelfLocal.corners(); ++k)
-      {
-        if (( eIt->geometry().global( intersectionSelfLocal[k] )
-              - intersectionGlobal[k] ).infinity_norm() > 1e-6)
-          DUNE_THROW(GridError, "global( intersectionSelfLocal[" << k << "] ) is not the same as intersectionGlobal[" << k <<"]!");
-      }
+    //  Check the consistency of intersectionSelfLocal() and intersectionGlobal
+
+    if (intersectionSelfLocal.corners() != intersectionGlobal.corners())
+      DUNE_THROW(GridError, "Geometry of intersection is inconsistent from left hand side and global view!");
+
+    // (Ab)use a quadrature rule as a set of test points
+    const int interDim = IntersectionIterator::LocalGeometry::mydimension;
+    const QuadratureRule<double, interDim>& quad
+      = QuadratureRules<double, interDim>::rule(intersectionSelfLocal.type(), 2);
+
+    for (int i=0; i<quad.size(); i++) {
+
+      FieldVector<double,GridType::dimension> globalPos = intersectionGlobal.global(quad[i].position());
+      FieldVector<double,GridType::dimension> localPos  = eIt->geometry().global(intersectionSelfLocal.global(quad[i].position()));
+
+      if ( (globalPos - localPos).infinity_norm() > 1e-6)
+        DUNE_THROW(GridError, "global( intersectionSelfLocal(global() ) is not the same as intersectionGlobal.global() at " << quad[i].position() << "!");
+
     }
 
     // ////////////////////////////////////////////////////////////////
@@ -127,63 +169,25 @@ void checkIntersectionIter(const GridType & grid, const IndexSet& indexSet,
     {
 
       const typename IntersectionIterator::LocalGeometry& intersectionNeighborLocal = iIt.intersectionNeighborLocal();
-      const typename IntersectionIterator::LocalGeometry& intersectionSelfLocal = iIt.intersectionSelfLocal();
 
-      if (intersectionSelfLocal.corners() <= 0)
-        DUNE_THROW(GridError, "Local intersection has nonpositive number of corners!");
+      checkGeometry(intersectionNeighborLocal);
 
       if (intersectionSelfLocal.corners() != intersectionNeighborLocal.corners())
         DUNE_THROW(GridError, "Geometry of intersection is inconsistent from left and right hand side!");
 
-      if (intersectionSelfLocal.corners() != intersectionGlobal.corners())
-        DUNE_THROW(GridError, "Geometry of intersection is inconsistent from left hand side and global view!");
+      // (Ab)use a quadrature rule as a set of test points
+      const int interDim = IntersectionIterator::LocalGeometry::mydimension;
+      const QuadratureRule<double, interDim>& quad
+        = QuadratureRules<double, interDim>::rule(intersectionNeighborLocal.type(), 2);
 
-      typedef typename EntityType::EntityPointer EntityPointer;
-      EntityPointer outside = iIt.outside();
-      EntityPointer inside = iIt.inside();
+      for (int i=0; i<quad.size(); i++) {
 
-      typedef std::vector < FieldVector<ctype,GridType::dimensionworld> > CornerSet;
-      CornerSet corners_self, corners_neighbor, corners_global;
+        FieldVector<double,GridType::dimension> globalPos = intersectionGlobal.global(quad[i].position());
+        FieldVector<double,GridType::dimension> localPos  = iIt.outside()->geometry().global(intersectionNeighborLocal.global(quad[i].position()));
 
-      for(int k=0; k<intersectionSelfLocal.corners(); ++k) {
-        corners_self.push_back( inside->geometry().global( intersectionSelfLocal[k]) );
-        corners_neighbor.push_back( outside->geometry().global( intersectionNeighborLocal[k]) );
-        corners_global.push_back( intersectionGlobal[k] );
-      }
+        if ( (globalPos - localPos).infinity_norm() > 1e-6)
+          DUNE_THROW(GridError, "global( intersectionNeighborLocal(global() ) is not the same as intersectionGlobal.global() at " << quad[i].position() << "!");
 
-      // check arbitrary local coordinate
-      FieldVector<ctype,GridType::dimension-1> localCoord (0.1);
-
-      corners_self.push_back ( inside->geometry().global( intersectionSelfLocal.global(localCoord) ));
-      corners_neighbor.push_back( outside->geometry().global( intersectionNeighborLocal.global(localCoord) ));
-      corners_global.push_back ( intersectionGlobal.global(localCoord) );
-
-      // check points of intersection neighbor local
-      typename CornerSet::iterator s_i = corners_self.begin();
-      typename CornerSet::iterator n_i = corners_neighbor.begin();
-      typename CornerSet::iterator g_i = corners_global.begin();
-
-      for( ; g_i != corners_global.end(); ++s_i,++n_i,++g_i)
-      {
-        if (( *s_i - *g_i ).infinity_norm() > 1e-6)
-        {
-          DUNE_THROW(GridError,
-                     "global( intersectionSelfLocal[" << *s_i << "] ) is not the same as intersectionGlobal[" << *g_i <<
-                     "] (delta_max = " << ( *s_i - *g_i ).infinity_norm() << ")!");
-        }
-        if (( *n_i - *g_i ).infinity_norm() > 1e-6)
-        {
-          DUNE_THROW(GridError,
-                     "global( intersectionNeighborLocal[" << *n_i << "] ) is not the same as intersectionGlobal[" << *g_i <<
-                     "] (delta_max = " << ( *n_i - *g_i ).infinity_norm() << ")!");
-        }
-        if (( *s_i - *n_i ).infinity_norm() > 1e-6)
-        {
-          DUNE_THROW(GridError,
-                     "global( intersectionSelfLocal[" << *s_i <<
-                     "] ) is not the same as global( intersectionNeighborLocal[" << *n_i <<
-                     "] (delta_max = " << ( *s_i - *n_i ).infinity_norm() << ")!");
-        }
       }
 
     }
