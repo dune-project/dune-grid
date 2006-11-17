b61a054b4e061a3681015818869362178bdb3309
Oliver Sander <sander@dune-project.org>
2006-11-17 09:30:56 +0000

    better implementation without class specialization
    [[Imported from SVN: r3275]]

--- grid/uggrid/uggridleafiterator.hh
+++ grid/uggrid/uggridleafiterator.hh
@@ -20,7 +20,8 @@ namespace Dune {
   {
     enum {dim = GridImp::dimension};
 
-    friend class UGGridEntity<codim,dim,GridImp>;
+    // The type of the UG entity we're pointing to
+    typedef typename UG_NS<dim>::template Entity<codim>::T UGEntity;
 
   public:
 
@@ -29,10 +30,22 @@ namespace Dune {
       // Entities below this level are certainly not leaf entities
       const unsigned int startingLevel = grid.leafIndexSet_.coarsestLevelWithLeafElements_;
 
-      if (pitype==All_Partition || pitype==Ghost_Partition)
-        setToTarget(UG_NS<dim>::PFirstNode(grid_->multigrid_->grids[startingLevel]));
-      else
-        setToTarget(UG_NS<dim>::FirstNode(grid_->multigrid_->grids[startingLevel]));
+      if (codim==dim) {
+
+        if (pitype==All_Partition || pitype==Ghost_Partition)
+          setToTarget((UGEntity*)UG_NS<dim>::PFirstNode(grid_->multigrid_->grids[startingLevel]));
+        else
+          setToTarget((UGEntity*)UG_NS<dim>::FirstNode(grid_->multigrid_->grids[startingLevel]));
+
+      } else if (codim==0) {
+
+        if (pitype==All_Partition || pitype==Ghost_Partition)
+          setToTarget((UGEntity*)UG_NS<dim>::PFirstElement(grid_->multigrid_->grids[startingLevel]));
+        else
+          setToTarget((UGEntity*)UG_NS<dim>::FirstElement(grid_->multigrid_->grids[startingLevel]));
+
+      } else
+        DUNE_THROW(NotImplemented, "UGGrid leaf iterators for codimension " << codim);
 
       if (!UG_NS<dim>::isLeaf(this->virtualEntity_.getTarget()))
         increment();
@@ -61,84 +74,27 @@ namespace Dune {
       int level = this->level();
 
       // Increment on this level
-      this->virtualEntity_.setToTarget(UG_NS<GridImp::dimension>::succ(this->virtualEntity_.getTarget()));
+      this->virtualEntity_.setToTarget(UG_NS<dim>::succ(this->virtualEntity_.getTarget()));
 
       // If beyond the end of this level set to first of next level
       if (!this->virtualEntity_.getTarget() && level < grid_->maxLevel()) {
 
-        if (pitype==All_Partition || pitype==Ghost_Partition)
-          setToTarget(UG_NS<dim>::PFirstNode(grid_->multigrid_->grids[level+1]));
-        else
-          setToTarget(UG_NS<dim>::FirstNode(grid_->multigrid_->grids[level+1]));
+        if (codim==dim) {
 
-      }
+          if (pitype==All_Partition || pitype==Ghost_Partition)
+            setToTarget((UGEntity*)UG_NS<dim>::PFirstNode(grid_->multigrid_->grids[level+1]));
+          else
+            setToTarget((UGEntity*)UG_NS<dim>::FirstNode(grid_->multigrid_->grids[level+1]));
 
-    }
+        } else if (codim==0) {
 
-    // /////////////////////////////////////
-    //   Data members
-    // /////////////////////////////////////
-    const GridImp* grid_;
+          if (pitype==All_Partition || pitype==Ghost_Partition)
+            setToTarget((UGEntity*)UG_NS<dim>::PFirstElement(grid_->multigrid_->grids[level+1]));
+          else
+            setToTarget((UGEntity*)UG_NS<dim>::FirstElement(grid_->multigrid_->grids[level+1]));
 
-  };
-
-
-  template<PartitionIteratorType pitype, class GridImp>
-  class UGGridLeafIterator<0,pitype,GridImp> :
-    public Dune::UGGridEntityPointer <0,GridImp>
-  {
-    enum {dim = GridImp::dimension};
-
-    friend class UGGridEntity<0,    dim,GridImp>;
-
-  public:
-
-    UGGridLeafIterator(const GridImp& grid) : grid_(&grid) {
-
-      const int startingLevel = grid.leafIndexSet_.coarsestLevelWithLeafElements_;
-
-      if (pitype==All_Partition || pitype==Ghost_Partition)
-        setToTarget(UG_NS<dim>::PFirstElement(grid_->multigrid_->grids[startingLevel]));
-      else
-        setToTarget(UG_NS<dim>::FirstElement(grid_->multigrid_->grids[startingLevel]));
-
-      // If this is not a leaf entity already increment to find the first leaf entity
-      if (!UG_NS<dim>::isLeaf(this->virtualEntity_.getTarget()))
-        increment();
-    }
-
-    //! Constructor
-    UGGridLeafIterator()
-    {
-      this->virtualEntity_.setToTarget(0);
-    }
-
-    //! prefix increment
-    void increment() {
-      // Increment until you find a leaf entity
-      do {
-        globalIncrement();
-      } while (this->virtualEntity_.getTarget() && !UG_NS<dim>::isLeaf(this->virtualEntity_.getTarget()));
-    }
-
-  private:
-
-    /** \brief This increment makes the iterator wander over all entities on all levels */
-    void globalIncrement() {
-
-      // Store the current level, so we know it even when pointing to an invalid entity
-      int level = this->level();
-
-      // Increment on this level
-      this->virtualEntity_.setToTarget(UG_NS<GridImp::dimension>::succ(this->virtualEntity_.getTarget()));
-
-      // If beyond the end of this level set to first of next level
-      if (!this->virtualEntity_.getTarget() && level < grid_->maxLevel()) {
-
-        if (pitype==All_Partition || pitype==Ghost_Partition)
-          setToTarget(UG_NS<dim>::PFirstElement(grid_->multigrid_->grids[level+1]));
-        else
-          this->setToTarget(UG_NS<dim>::FirstElement(grid_->multigrid_->grids[level+1]));
+        } else
+          DUNE_THROW(NotImplemented, "UGGrid leaf iterators for codimension " << codim);
 
       }
 
