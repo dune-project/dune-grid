1fa6b9d1b348ea7d5855d1f472be4fef03fbdf84
Andreas Dedner <dedner@dune-project.org>
2006-12-06 15:16:27 +0000

    mit Parametertest
    [[Imported from SVN: r3336]]

--- grid/io/file/dgfparser/test/main.cc
+++ grid/io/file/dgfparser/test/main.cc
@@ -4,20 +4,38 @@
 #include <dune/grid/test/gridcheck.cc>
 #include <dune/grid/common/gridpart.hh>
 #include "../gridtype.hh"
+#include <dune/grid/common/gridpart.hh>
 using namespace Dune;
 #if HAVE_GRAPE
 #include <dune/grid/io/visual/grapegriddisplay.hh>
-void test(GridType& grid) {
-  gridcheck(grid);
-  // HierarchicGridPart<GridType> part(grid);
-  // GrapeGridDisplay<GridType> disp(part);
-  // disp.display();
+#include <dune/grid/io/visual/grapedatadisplay.hh>
+template <class GridPart>
+void test(GridPart& part,std::vector<double>& dat,int nofParams,int cdim) {
+  gridcheck(part.grid());
+  return;
+  if (nofParams>0) {
+    GrapeDataDisplay<typename GridPart::GridType> disp(part);
+    if (cdim == 0)
+      disp.displayVector("el. Paramters",dat,part.indexSet(),0,
+                         nofParams,false);
+    else
+      disp.displayVector("vtx. Paramters",dat,part.indexSet(),1,
+                         nofParams,true);
+  } else {
+    GrapeGridDisplay<typename GridPart::GridType> disp(part.grid());
+    disp.display();
+  }
 }
 #else
-void test(GridType& grid) {
-  gridcheck(grid);
+template <class GridPart>
+void test(GridPart& part,std::vector<double>& dat,int nofParams,int cdim) {
+  gridcheck(part.grid());
 }
 #endif
+template <class GridPart>
+void test(GridPart& part) {
+  gridcheck(part.grid());
+}
 
 int main(int argc, char ** argv, char ** envp)
 try {
@@ -33,16 +51,60 @@ try {
   std::cout << "tester: start grid reading" << std::endl;
 
   // create Grid from DGF parser
-  GridPtr<GridType> grid(argv[1], mpiHelper.getCommunicator() );
+  GridPtr<GridType> grid;
+  {
+    grid = GridPtr<GridType>(argv[1], mpiHelper.getCommunicator() );
+  }
+  typedef LeafGridPart<GridType> GridPartType;
+  typedef GridPartType::IndexSetType IndexSetType;
+  GridPartType gridPart(*grid);
+  const IndexSetType& index = gridPart.indexSet();
+  std::vector<double> eldat(0),vtxdat(0);
+  if (grid.nofParameters(0)>0)
+  {
+    std::cout << "Reading Element Parameters:" << std::endl;
+    eldat.resize(index.size(0)*grid.nofParameters(0));
+    typedef GridPartType::Codim<0>::IteratorType IteratorType;
+    IteratorType endit = gridPart.end<0>();
+    for (IteratorType iter=gridPart.begin<0>(); iter!=endit; ++iter) {
+      std::vector<double>& param = grid.parameters(*iter);
+      assert(param.size() == grid.nofParameters(0));
+      for (int i=0; i<param.size(); i++) {
+        // std::cout << param[i] << " ";
+        eldat[index.index(*iter)*param.size()+i] = param[i];
+      }
+      // std::cout << std::endl;
+    }
+  }
+  if (grid.nofParameters(GridType::dimension)>0)
+  {
+    std::cout << "Reading Vertex Parameters:" << std::endl;
+    vtxdat.resize(index.size(GridType::dimension)*grid.nofParameters(GridType::dimension));
+    typedef GridPartType::Codim<GridType::dimension>::IteratorType IteratorType;
+    IteratorType endit = gridPart.end<GridType::dimension>();
+    for (IteratorType iter=gridPart.begin<GridType::dimension>();
+         iter!=endit; ++iter) {
+      std::vector<double>& param = grid.parameters(*iter);
+      assert(param.size() == grid.nofParameters(GridType::dimension));
+      // std::cout << (*iter).geometry()[0] << " -\t ";
+      for (int i=0; i<param.size(); i++) {
+        // std::cout << param[i] << " ";
+        vtxdat[index.index(*iter)*param.size()+i] = param[i];
+      }
+      // std::cout << std::endl;
+    }
+  }
 
   // display
-  // if (myrank <= 0) test(*grid);
+  if (myrank <= 0)
+    if (0 && grid.nofParameters(0)>0)
+      test(gridPart,eldat,grid.nofParameters(0),0);
+    else
+      test(gridPart,vtxdat,grid.nofParameters(GridType::dimension),GridType::dimension);
   // refine
   std::cout << "tester: refine grid" << std::endl;
-  grid->globalRefine(DGFGridInfo<GridType>::refineStepsForHalf());
-  // display
-  if (myrank <= 0) test(*grid);
-
+  grid->globalRefine(Dune::DGFGridInfo<GridType>::refineStepsForHalf());
+  test(gridPart);
   return 0;
 }
 catch (Dune::Exception &e) {
