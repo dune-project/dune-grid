aed3d90026f6d8c0fdbefec7babb30d94e1ad142
Oliver Sander <sander@dune-project.org>
2007-04-05 10:25:18 +0000

    Correctly implemented the default mechanism for Entity::type(). Now the type() method can be implemented in the Entity classes of those grids where the detour through the geometry is costly.
    [[Imported from SVN: r3627]]

--- grid/common/entity.hh
+++ grid/common/entity.hh
@@ -149,7 +149,7 @@ namespace Dune
     /** \brief Return the name of the reference element. The type can
        be used to access the Dune::ReferenceElement.
      */
-    GeometryType type () const { return realEntity.geometry().type(); };
+    GeometryType type () const { return realEntity.type(); };
 
     //===========================================================
     /** @name Interface for the implementor
@@ -295,7 +295,7 @@ namespace Dune
     /** \brief Return the name of the reference element. The type can
         be used to access the Dune::ReferenceElement.
      */
-    GeometryType type () const { return realEntity.geometry().type(); };
+    GeometryType type () const { return realEntity.type(); };
 
     //===========================================================
     /** @name Extended interface of entities of codimension 0
@@ -512,6 +512,11 @@ namespace Dune
     //! define type used for coordinates in grid module
     typedef ct ctype;
 
+    /** \brief Return the name of the reference element. The type can
+        be used to access the Dune::ReferenceElement.
+     */
+    GeometryType type () const { return asImp().geometry().type(); };
+
   private:
     //!  Barton-Nackman trick
     EntityImp<cd,dim,GridImp>& asImp ()
@@ -562,6 +567,11 @@ namespace Dune
        In bisection or hanging node refinement this is always true.
      */
     bool isRegular() const { return true; }
+
+    /** \brief Return the name of the reference element. The type can
+        be used to access the Dune::ReferenceElement.
+     */
+    GeometryType type () const { return asImp().geometry().type(); };
 
     /* maybe in later versions
      * \brief Default implementation for access to boundaryId of sub entities
