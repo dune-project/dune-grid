7598062e5152bd8c76703c01f0917a13b56b8569
Robert Klöfkorn <robertk@dune-project.org>
2007-09-12 17:22:42 +0000

    removed if, because is checked in the implementations aswell.
    [[Imported from SVN: r3846]]

--- grid/io/file/dgfparser/dgf.hh
+++ grid/io/file/dgfparser/dgf.hh
@@ -3,15 +3,9 @@
 #ifndef DUNE_DGFALLIMPLEMENTATIONS_HH
 #define DUNE_DGFALLIMPLEMENTATIONS_HH
 /* include the implementations */
-#if HAVE_ALBERTA
 #include "dgfalberta.hh"
-#endif
-#if HAVE_ALUGRID
 #include "dgfalu.hh"
-#endif
-#if HAVE_UG
 #include "dgfug.hh"
-#endif
 #include "dgfoned.hh"
 #include "dgfyasp.hh"
 #include "dgfs.hh"
