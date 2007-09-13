267f31fdc060b7c5355d6574255eed46784c541f
Robert Klöfkorn <robertk@dune-project.org>
2007-09-13 08:46:54 +0000

    moved dgf.hh to ../ and renamed to dgfparser.hh as the rule is. Now dgfparser.hh includes the parser and all applications.
    [[Imported from SVN: r3847]]

---
+++ grid/io/file/dgfparser.hh
@@ -0,0 +1,14 @@
+// -*- tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 2 -*-
+// vi: set et ts=4 sw=2 sts=2:
+#ifndef DUNE_DGFPARSER_HH
+#define DUNE_DGFPARSER_HH
+// include dgf parser
+#include "dgfparser/dgfparser.hh"
+/* include the implementations */
+#include "dgfparser/dgfalberta.hh"
+#include "dgfparser/dgfalu.hh"
+#include "dgfparser/dgfug.hh"
+#include "dgfparser/dgfoned.hh"
+#include "dgfparser/dgfyasp.hh"
+#include "dgfparser/dgfs.hh"
+#endif
