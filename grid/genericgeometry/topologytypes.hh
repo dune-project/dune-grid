8da360075e068ca2e131c6df0708ae9d2c0c4eaf
Martin Nolte <mnolte@dune-project.org>
2008-07-13 07:37:31 +0000

    added ids to topoloty types
    [[Imported from SVN: r4242]]

--- grid/genericgeometry/topologytypes.hh
+++ grid/genericgeometry/topologytypes.hh
@@ -16,6 +16,8 @@ namespace Dune
       enum { dimension = 0 };
       enum { numCorners = 1 };
 
+      enum { id = 0 };
+
       static std :: string name ()
       {
         return "p";
@@ -29,6 +31,8 @@ namespace Dune
       enum { dimension = BaseTopology :: dimension + 1 };
       enum { numCorners = 2 * BaseTopology :: numCorners };
 
+      enum { id = BaseTopology :: id + (1 << (dimension-1)) };
+
       static std :: string name ()
       {
         return BaseTopology :: name() + "'";
@@ -41,6 +45,8 @@ namespace Dune
     {
       enum { dimension = BaseTopology :: dimension + 1 };
       enum { numCorners = BaseTopology :: numCorners + 1 };
+
+      enum { id = BaseTopology :: id };
 
       static std :: string name ()
       {
@@ -63,6 +69,16 @@ namespace Dune
     struct BaseTopology< Pyramid< Base > >
     {
       typedef Base type;
+    };
+
+
+
+    template< class Topology >
+    struct IsHybrid
+    {
+      // Only cubes and simplices are nonhybrid
+      enum { value = ((Topology :: id | 1) == (1 << Topology :: dimension) - 1)
+                     || ((Topology :: id >> 1) == 0) };
     };
 
   }
