e219a99501bd89d6e2a1d94b930a0dd6aa9d8c99
Martin Nolte <mnolte@dune-project.org>
2008-07-16 13:55:15 +0000

    add outermost wrapper
    [[Imported from SVN: r4255]]

---
+++ grid/genericgeometry/genericgeometry.hh
@@ -0,0 +1,153 @@
+// -*- tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 2 -*-
+// vi: set et ts=4 sw=2 sts=2:
+#ifndef DUNE_GENERICGEOMETRY_GENERICGEOMETRY_HH
+#define DUNE_GENERICGEOMETRY_GENERICGEOMETRY_HH
+
+#include <dune/grid/genericgeometry/geometry.hh>
+
+namespace Dune
+{
+
+  namespace GenericGeometry
+  {
+
+    template< class ctype, int cdim >
+    struct CoordTraitsBase
+    {
+      typedef typename ctype FieldType;
+      enum { dimCoord = cdim };
+
+      template< int dim >
+      struct Vector
+      {
+        typedef FieldVector< FieldType, dim > Type;
+      };
+
+      template< int dimR, int dim C >
+      struct Matrix
+      {
+        typedef FieldMatrix< FieldType, dimR, dimC > Type;
+      };
+
+      typedef typename Vector< dimCoord > CoordinateType;
+    };
+
+
+
+    template< class GridImp >
+    struct GeometryTraits;
+
+    template< class GridImp >
+    struct GeometryTraits< const GridImp >
+      : public GeometryTraits< GridImp >
+    {};
+
+
+
+    template< int mydim, int cdim, class GridImp >
+    class DuneGeometry
+    {
+      typedef GeometryTraits< GridImp > Traits;
+
+      enum { dimGrid = Traits :: dimGrid };
+
+    public:
+      enum { mydimension = mydim };
+      enum { coorddimension = cdim };
+
+      typedef typename GridImp :: ctype ctype;
+
+      typedef FieldVector< ctype, mydimension > LocalCoordinate;
+      typedef FieldVector< ctype, coorddimension > GlobalCoordinate;
+
+    private:
+      dune_static_assert( (0 <= mydimension) && (mydimension <= dimGrid),
+                          "Invalid geometry dimension." );
+      enum { codimension = dimGrid - mydimension };
+
+      typedef typename Traits :: CoordTraits CoordTraits;
+
+      template< bool >
+      struct Hybrid
+      {
+        typedef HybridGeometry< dimGrid, CoordTraits, Traits :: template Caching >
+        GeometryType;
+      };
+
+      template< bool >
+      struct NonHybrid
+      {
+        typedef typename Convert< Traits :: dunetype, dimGrid > :: type Topology;
+        typedef Geometry< Topology, CoordTraits, Traits :: template Caching >
+        GeometryType;
+      };
+
+      typedef ProtectedIf< Traits :: hybrid, Hybrid, NonHybrid > :: GeometryType
+      ElementGeometryType;
+      typedef typename SubGeometry< ElementGeometryType, codimension > :: type
+      GeometryType;
+
+      GeometryType *geometry_;
+
+    public:
+      Dune :: GeometryType type () const
+      {
+        return geometry().type();
+      }
+
+      int corners () const
+      {
+        return geometry().corners();
+      }
+
+      const GlobalCoordinate &operator[] ( int i ) const
+      {
+        return geometry()[ i ];
+      }
+
+      GlobalCoordinate global ( const LocalCoordinate &local ) const
+      {
+        return geometry().global( local );
+      }
+
+      LocalCoordinate local ( const GlobalCoordinate &global ) const
+      {
+        return geometry().local( global );
+      }
+
+      bool checkInside ( const LocalCoordinate &local ) const
+      {
+        return geometry().checkInside( local );
+      }
+
+      bool affine () const
+      {
+        return geometry().affine();
+      }
+
+      ctype integrationElement ( const LocalCoordinate &local ) const
+      {
+        return geometry().integrationElement( local );
+      }
+
+      ctype volume () const
+      {
+        return geometry().volume();
+      }
+
+      const Jacobian &jacobianInverseTransposed ( const LocalCoordinate &local ) const
+      {
+        return geometry().jacobianInverseTransposed( local );
+      }
+
+    private:
+      const GeometryType &geometry () const
+      {
+        return *geometry_;
+      }
+    };
+  }
+
+}
+
+#endif
