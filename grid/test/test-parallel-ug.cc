c8c2b0f5e8f140b11f84eb9578c3acbc924f5e42
Oliver Sander <sander@dune-project.org>
2009-02-18 10:49:17 +0000

    new and improved tests by Andreas Lauser
    [[Imported from SVN: r4869]]

--- grid/test/test-parallel-ug.cc
+++ grid/test/test-parallel-ug.cc
@@ -6,26 +6,18 @@
 #include <config.h>
 
 #include <iostream>
+#include <vector>
 
 #include <dune/grid/uggrid.hh>
 #include <dune/grid/common/mcmgmapper.hh>
+#include <dune/grid/io/file/vtk/vtkwriter.hh>
 #include <dune/common/mpihelper.hh>
 
 using namespace Dune;
 
-//! Parameter for mapper class
-template<int dim>
+//! Use only elements for a mapper
+template <int dim>
 struct ElementLayout
-{
-  bool contains (Dune::GeometryType gt)
-  {
-    return gt.dim() == 0;
-  }
-};
-
-//! Parameter for mapper class
-template<int dim>
-struct NodeLayout
 {
   bool contains (Dune::GeometryType gt)
   {
@@ -33,23 +25,41 @@ struct NodeLayout
   }
 };
 
-struct VectorType { double blubb[3]; };
+template <int dim>
+struct NodeLayout
+{
+  bool contains (Dune::GeometryType gt)
+  {
+    return gt.dim() == 0;
+  }
+};
 
-// A DataHandle class to exchange entries of a vector
-template<class M, class V> // mapper type and vector type
-class VectorExchange
-  : public Dune::CommDataHandleIF<VectorExchange<M,V>,
-        V
-        /*typename V::value_type */>
+// A DataHandle class to exchange entries of a vector.
+template<class MapperT, int commCodim>
+class DataExchange
+  : public Dune::CommDataHandleIF<DataExchange<MapperT,
+            commCodim>,
+        double>
 {
 public:
   //! export type of data for message buffer
-  typedef V DataType;
+  typedef double DataType;
+  typedef std::vector<Dune::FieldVector<double,1> > UserDataType;
+
+  //! constructor
+  DataExchange(const MapperT &mapper,
+               UserDataType &userDataSend,
+               UserDataType &userDataReceive)
+    : mapper_(mapper),
+      userDataSend_(userDataSend),
+      userDataReceive_(userDataReceive)
+  {}
+
 
   //! returns true if data for this codim should be communicated
   bool contains (int dim, int codim) const
   {
-    return (codim==0);
+    return (codim == commCodim);
   }
 
   //! returns true if size per entity of given dim and codim is a constant
@@ -70,24 +80,20 @@ public:
 
   //! pack data from user to message buffer
   template<class MessageBuffer, class EntityType>
-  void gather (MessageBuffer& buff, const EntityType& e) const
+  void gather(MessageBuffer& buff, const EntityType& e) const
   {
-    int rank;
-    MPI_Comm_rank(MPI_COMM_WORLD,&rank);
+    DataType x = mapper_.map(e);
 
-    VectorType data;
-    for (int i = 0; i < 3; ++i) {
-      data.blubb[i] = 1e6 + rank*1e5 + mapper.map(e)*1e2 + i;
-      std::cout << "Process "
-                << Dune::MPIHelper::getCollectiveCommunication().rank()
-                << " sends for entity "
-                << mapper.map(e)
-                << ": "
-                << std::setprecision(20)
-                << data.blubb[i] << "\n";
-    }
+    userDataSend_[mapper_.map(e)][0] = x;
+    std::cout << "Process "
+              << Dune::MPIHelper::getCollectiveCommunication().rank()+1
+              << " sends for entity "
+              << mapper_.map(e)
+              << ": "
+              << std::setprecision(20)
+              << x << "\n";
 
-    buff.write(data);
+    buff.write(x);
   }
 
   /*! unpack data from message buffer to user
@@ -95,28 +101,25 @@ public:
      n is the number of objects sent by the sender
    */
   template<class MessageBuffer, class EntityType>
-  void scatter (MessageBuffer& buff, const EntityType& e, size_t n)
+  void scatter(MessageBuffer& buff, const EntityType& e, size_t n)
   {
     DataType x;
     buff.read(x);
-    for (int i = 0; i < 3; ++i) {
-      std::cout << "Process "
-                << Dune::MPIHelper::getCollectiveCommunication().rank()
-                << " received for entity "
-                << mapper.map(e)
-                << ": "
-                << std::setprecision(20)
-                << x.blubb[i] << "\n";
-    }
+
+    userDataReceive_[mapper_.map(e)][0] = x;
+    std::cout << "Process "
+              << Dune::MPIHelper::getCollectiveCommunication().rank()+1
+              << " received for entity "
+              << mapper_.map(e)
+              << ": "
+              << std::setprecision(20)
+              << x << "\n";
   }
 
-  //! constructor
-  VectorExchange (const M& mapper_)
-    : mapper(mapper_)
-  {}
-
 private:
-  const M& mapper;
+  const MapperT &mapper_;
+  UserDataType &userDataSend_;
+  UserDataType &userDataReceive_;
 };
 
 int main (int argc , char **argv) try
@@ -128,20 +131,21 @@ int main (int argc , char **argv) try
   int rank = mpiHelper.rank();
   int size = mpiHelper.size();
 
-
   std::cout << "This is process " << rank << " of " << size << ", PID " << getpid() << " .\n";
 
-  // //////////////////////////////////////////////////////////
-  //   Make a uniform grid on rank 0 for testing
-  // //////////////////////////////////////////////////////////
+  ////////////////////////////////////////////////////////////
+  // Create uniform grid on rank 0
+  ////////////////////////////////////////////////////////////
   int n = 11;
-  typedef UGGrid<2> GridType;
+  const int dim = 2;
+  const int commCodim = 0;    // codim of the entities to communicate
+  typedef UGGrid<dim> GridType;
   GridType grid;
   GridFactory<GridType> factory(&grid);
 
   for (int i=0; i<n; i++) {
     for (int j=0; j<n; j++) {
-      FieldVector<double,2> pos;
+      FieldVector<double,dim> pos;
       pos[0] = double(i)/(n-1);
       pos[1] = double(j)/(n-1);
       factory.insertVertex(pos);
@@ -155,40 +159,102 @@ int main (int argc , char **argv) try
       v[1] = i*n + j+1;
       v[2] = (i+1)*n + j;
       v[3] = (i+1)*n + j+1;
-      factory.insertElement(GeometryType(GeometryType::cube,2), v);
+      factory.insertElement(GeometryType(GeometryType::cube,dim), v);
     }
   }
 
-  //std::auto_ptr<Dune::UGGrid<2> > grid(factory.createGrid());
   factory.createGrid();
 
-  grid.loadBalance(0,     // strategy
-                   0,     // minlevel
-                   2,     // depth
-                   32,     // maxlevel
-                   1);     // minelement
+
+  //////////////////////////////////////////////////////
+  // Distribute the grid
+  //////////////////////////////////////////////////////
+  /*    grid.loadBalance(0,   // strategy
+                       0,   // minlevel
+                       2,   // depth
+                       32,   // maxlevel
+                       1);   // minelement
+   */
+  grid.loadBalance();
 
 
   std::cout << "Process " << rank << " has " << grid.size(0) << " elements." << std::endl;
 
   // make sure each process has some elements
   assert(grid.size(0) > 0);
+  // make sure each process has some nodes
+  assert(grid.size(dim) > 0);
 
-  // ////////////////////////////////////////////////////
-  //  Test element communication
-  // ////////////////////////////////////////////////////
+  //////////////////////////////////////////////////////
+  // Test level and leaf mappers/gridViews
+  //////////////////////////////////////////////////////
+  GridType::LevelGridView level0GridView = grid.levelView(0);
+  GridType::LeafGridView leafGridView = grid.leafView();
 
-  GridType::LevelGridView gridView = grid.levelView(0);
+  std::cout << "LevelGridView for level 0 has " << level0GridView.size(0)
+            << " elements and " << level0GridView.size(dim) << " nodes.\n";
+  std::cout << "LeafGridView has " << leafGridView.size(0)
+            << " elements and " << leafGridView.size(dim) << " nodes.\n";
 
-  // make a mapper for codim 0 entities in the level grid
-  typedef LevelMultipleCodimMultipleGeomTypeMapper<GridType,NodeLayout> MapperType;
-  MapperType mapper(grid, 0);
+  typedef LevelMultipleCodimMultipleGeomTypeMapper<GridType,ElementLayout > LevelMapperType;
+  LevelMapperType levelMapper(grid, 0);
 
-  //    typedef std::vector<double> VectorType;
+  typedef LeafMultipleCodimMultipleGeomTypeMapper<GridType,ElementLayout > LeafMapperType;
+  LeafMapperType leafMapper(grid);
 
-  VectorExchange<MapperType,VectorType> datahandle(mapper);
-  grid.communicate<VectorExchange<MapperType,VectorType> >(datahandle,Dune::InteriorBorder_All_Interface,
-                                                           Dune::ForwardCommunication);
+  std::cout << "Level index set has " << levelMapper.size() << " entities\n";
+  std::cout << "Leaf index set has " << leafMapper.size() << " entities\n";
+
+  // create the user data arrays
+  typedef std::vector<Dune::FieldVector<double, 1> > UserDataType;
+  UserDataType userDataSend(leafGridView.size(commCodim), 0.0);
+  UserDataType userDataReceive(leafGridView.size(commCodim), 0.0);
+  UserDataType partitionType(leafGridView.size(commCodim), -1e10);
+
+  // write the partition type of each node into the corrosponding
+  // result array
+  GridType::LeafGridView::Codim<commCodim>::Iterator
+    it = leafGridView.begin<commCodim>();
+  const GridType::LeafGridView::Codim<commCodim>::Iterator
+  &endIt = leafGridView.end<commCodim>();
+  for (; it != endIt; ++it) {
+    partitionType[leafMapper.map(*it)] = it->partitionType();
+  };
+
+  //////////////////////////////////////////////////////
+  // Test communication
+  //////////////////////////////////////////////////////
+
+  // initialize data handle (marks the nodes where some data was
+  // send or received)
+  typedef DataExchange<LeafMapperType, commCodim> MyDataHandle;
+  MyDataHandle datahandle(leafMapper,
+                          userDataSend,
+                          userDataReceive);
+  // communicate the entities at the interior border to all other
+  // processes
+  grid.communicate(datahandle,
+                   Dune::InteriorBorder_All_Interface,
+                   Dune::ForwardCommunication);
+
+  //////////////////////////////////////////////////////
+  // Write results to disc
+  //////////////////////////////////////////////////////
+  std::cout << "Writing data to disk\n";
+  Dune::VTKWriter<GridType::LeafGridView> writer(leafGridView);
+  if (commCodim == 0) {
+    writer.addCellData(userDataSend, "Send");
+    writer.addCellData(userDataReceive, "Receive");
+    writer.addCellData(partitionType, "Partition Type");
+  }
+  else if (commCodim == dim) {
+    writer.addVertexData(userDataSend, "Send");
+    writer.addVertexData(userDataReceive, "Receive");
+    writer.addVertexData(partitionType, "Partition Type");
+  }
+  writer.write("test-parallel-ug",
+               Dune::VTKOptions::ascii);
+  std::cout << "Done writing data to disk\n";
 
   return 0;
 }
