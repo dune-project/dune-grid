4c04d84817d8720655dc726173124efcc530f7a2
Martin Nolte <mnolte@dune-project.org>
2009-03-12 09:52:40 +0000

    adapt to change in interval block
    [[Imported from SVN: r4921]]

--- grid/io/file/dgfparser/dgfs.cc
+++ grid/io/file/dgfparser/dgfs.cc
@@ -1,41 +1,54 @@
 // -*- tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 2 -*-
 // vi: set et ts=4 sw=2 sts=2:
-namespace Dune {
-  template <int dim,int dimworld>
-  inline SGrid<dim,dimworld>*
-  MacroGrid ::
-  Impl< SGrid<dim,dimworld> > ::
-  generate(MacroGrid& mg,const char* filename, MPICommunicatorType ) {
-    mg.element=Cube;
-    std::ifstream gridin(filename);
-    dgf :: IntervalBlock interval(gridin);
-    if(!interval.isactive()) {
-      DUNE_THROW(DGFException,
-                 "Macrofile " << filename << " must have Intervall-Block "
-                              << "to be used to initialize SGrid!\n"
-                              << "No alternative File-Format defined");
-    }
-    mg.dimw = interval.dimw();
-    if (mg.dimw != dimworld) {
-      DUNE_THROW(DGFException,
-                 "Macrofile " << filename << " is for dimension " << mg.dimw
-                              << " and connot be used to initialize an SGrid of dimension "
-                              << dimworld);
-    }
+namespace Dune
+{
 
-    FieldVector<double,dimworld> start;
-    FieldVector<double,dimworld> upper;
-    FieldVector<int,dimworld>    anz;
+  template< int dim, int dimworld >
+  inline SGrid< dim, dimworld > *
+  MacroGrid::Impl< SGrid< dim, dimworld > >
+  ::generate ( MacroGrid &mg, const char *filename, MPICommunicatorType )
+  {
+    std::ifstream gridin( filename );
+    dgf::IntervalBlock intervalBlock( gridin );
 
-    for (int i=0; i<dimworld; ++i)
+    if( !intervalBlock.isactive() )
     {
-      start[i] = interval.start(i);
-      upper[i]  = start[i] + interval.length(i);
-      anz[i]  = interval.segments(i);
+      DUNE_THROW( DGFException,
+                  "Macrofile " << filename << " must have Intervall-Block "
+                               << "to be used to initialize SGrid!\n"
+                               << "No alternative File-Format defined");
     }
-    // SGrid gets number of cells in each dircetion
-    // position of origin of the cube
-    // position of the upper right corner of the cube
-    return new SGrid<dim,dimworld>(anz,start,upper);
+
+    if( intervalBlock.numIntervals() != 1 )
+      DUNE_THROW( DGFException, "SGrid can only handle 1 interval block." );
+
+    if( intervalBlock.dimw() != dimworld )
+    {
+      DUNE_THROW( DGFException,
+                  "Cannot read an interval of dimension " << intervalBlock.dimw()
+                                                          << "into a SGrid< " << dim << ", " << dimworld << " >." );
+    }
+
+    mg.element = Cube;
+    mg.dimw = intervalBlock.dimw();
+
+    const dgf::IntervalBlock::Interval &interval = intervalBlock.get( 0 );
+
+    FieldVector< double, dimworld > lower, upper;
+    FieldVector< int, dimworld > anz;
+
+    for( int i = 0; i < dimworld; ++i )
+    {
+      lower[ i ] = interval.p[ 0 ][ i ];
+      upper[ i ] = interval.p[ 1 ][ i ];
+      anz[ i ] = interval.n[ i ];
+    }
+
+    // SGrid gets the following arguments:
+    // - number of cells in each dircetion
+    // - position of the lower left corner of the cube
+    // - position of the upper right corner of the cube
+    return new SGrid< dim, dimworld >( anz, lower, upper );
   }
+
 }
