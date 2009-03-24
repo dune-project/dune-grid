6eb3a0d7f3327e66ebd820b164ce3ba5b9c0f387
Christian Engwer <christi@dune-project.org>
2009-03-24 08:57:04 +0000

    properly escale '#' in comment block
    [[Imported from SVN: r4961]]

--- grid/io/visual/grape/grape_undefs.hh
+++ grid/io/visual/grape/grape_undefs.hh
@@ -1,7 +1,7 @@
 // -*- tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 2 -*-
 // vi: set et ts=4 sw=2 sts=2:
 /** \file
- * \brief Contains <tt>#undef</tt>s for all preprocessor macros
+ * \brief Contains <tt>\#undef</tt>s for all preprocessor macros
  * defined by grape.
  *
  * This file is created automatically by the perl script <tt>undefAllMacros.pl</tt>.
