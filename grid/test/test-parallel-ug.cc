6cb4b1086378fd6a0b6a324cb79f3b4cfc2f1f79
Oliver Sander <sander@dune-project.org>
2009-05-04 17:09:03 +0000

    updated test for parallel UGGrid, courtesy of Andreas Lauser
    [[Imported from SVN: r5107]]

--- grid/test/test-parallel-ug.cc
+++ grid/test/test-parallel-ug.cc
@@ -137,105 +137,101 @@ void checkConsistency(const GridView &gv)
       }
       ++ n;
     };
-    assert(n == 4);     // rectangles
+
+    assert(n == 2*GridView::dimension);     // quadrilaterals
   }
 };
 
-int main (int argc , char **argv) try
+template <class Grid>
+void insertVertices(int n, Dune::GridFactory<Grid> &factory, int coordIdx=0)
 {
-  // initialize MPI, finalize is done automatically on exit
-  Dune::MPIHelper &mpiHelper =
-    Dune::MPIHelper::instance(argc, argv);
-
-  int rank = mpiHelper.rank();
-  int size = mpiHelper.size();
-
-  std::cout << "This is process " << rank + 1 << " of " << size << ", PID " << getpid() << " .\n";
-
-  ////////////////////////////////////////////////////////////
-  // Create uniform grid on rank 0
-  ////////////////////////////////////////////////////////////
-  int n = 11;
-  const int dim = 2;
-  const int commCodim = dim;    // codim of the entities to communicate
-  typedef UGGrid<dim> GridType;
-  GridType grid;
-  GridFactory<GridType> factory(&grid);
-
-  for (int i=0; i<n; i++) {
-    for (int j=0; j<n; j++) {
-      FieldVector<double,dim> pos;
-      pos[0] = double(i)/(n-1);
-      pos[1] = double(j)/(n-1);
-      factory.insertVertex(pos);
-    }
+  static FieldVector<double,Grid::dimension> pos;
+  if (coordIdx == Grid::dimension) {
+    factory.insertVertex(pos);
+    return;
   }
 
-  for (int i=0; i<n-1; i++) {
-    for (int j=0; j<n-1; j++) {
-      std::vector<unsigned int> v(4);
-      v[0] = i*n + j;
-      v[1] = i*n + j+1;
-      v[2] = (i+1)*n + j;
-      v[3] = (i+1)*n + j+1;
-      factory.insertElement(GeometryType(GeometryType::cube,dim), v);
+  for (int i=0; i < n; ++i) {
+    pos[coordIdx] = double(i)/(n-1);
+    insertVertices(n, factory, coordIdx + 1);
+  }
+}
+
+template <class GridType>
+void insertElements(int n, Dune::GridFactory<GridType> &factory, unsigned coordIdx=0)
+{
+  const int dim = GridType::dimension;
+
+  if (dim == 3) {
+    for (int i=0; i<n-1; i++) {
+      for (int j=0; j<n-1; j++) {
+        for (int k=0; k<n-1; k++) {
+          std::vector<unsigned int> v(8);
+          v[0] = k*n*n + i*n + j;
+          v[1] = k*n*n + i*n + j+1;
+          v[2] = k*n*n + (i+1)*n + j;
+          v[3] = k*n*n + (i+1)*n + j+1;
+
+          v[4] = (k+1)*n*n + i*n + j;
+          v[5] = (k+1)*n*n + i*n + j+1;
+          v[6] = (k+1)*n*n + (i+1)*n + j;
+          v[7] = (k+1)*n*n + (i+1)*n + j+1;
+
+          factory.insertElement(GeometryType(GeometryType::cube,dim), v);
+        }
+      }
     }
   }
+  else if (dim == 2) {
+    for (int i=0; i<n-1; i++) {
+      for (int j=0; j<n-1; j++) {
+        std::vector<unsigned int> v(4);
+        v[0] = i*n + j;
+        v[1] = i*n + j+1;
+        v[2] = (i+1)*n + j;
+        v[3] = (i+1)*n + j+1;
+
+        factory.insertElement(GeometryType(GeometryType::cube,dim), v);
+      }
+    }
+  }
+};
+
+template <class GridType>
+void createGrid(GridType &grid, int n)
+{
+  typedef Dune::GridFactory<GridType> GridFactory;
+
+  GridFactory factory(&grid);
+
+  insertVertices<GridType>(n, factory);
+  insertElements<GridType>(n, factory);
 
   factory.createGrid();
+}
 
+template <class GridType, int commCodim>
+void testCommunication(GridType &grid)
+{
+  std::cout << "Testing communication for codim " << commCodim << " endities\n";
 
-  //////////////////////////////////////////////////////
-  // Distribute the grid
-  //////////////////////////////////////////////////////
-  /*    grid.loadBalance(0,   // strategy
-                       0,   // minlevel
-                       2,   // depth
-                       32,   // maxlevel
-                       1);   // minelement
-   */
-  grid.loadBalance();
-
-
-  std::cout << "Process " << rank + 1
-            << " has " << grid.size(0)
-            << " elements and " << grid.size(dim)
-            << " nodes.\n";
-
-  // make sure each process has some elements
-  assert(grid.size(0) > 0);
-  // make sure each process has some nodes
-  assert(grid.size(dim) > 0);
-
-  //////////////////////////////////////////////////////
-  // Test level and leaf mappers/gridViews
-  //////////////////////////////////////////////////////
-  GridType::LevelGridView level0GridView = grid.levelView(0);
-  GridType::LeafGridView leafGridView = grid.leafView();
-
-  std::cout << "LevelGridView for level 0 has " << level0GridView.size(0)
-            << " elements "  << level0GridView.size(dim - 1)
-            << " edges and " << level0GridView.size(dim)
-            << " nodes.\n";
-  std::cout << "LeafGridView has " << leafGridView.size(0)
-            << " elements, " << leafGridView.size(dim - 1)
-            << " edges and " << leafGridView.size(dim)
-            << " nodes.\n";
+  typedef Dune::LeafMultipleCodimMultipleGeomTypeMapper<GridType,
+      LayoutWrapper<commCodim>::template Layout> LeafMapperType;
 
   typedef LevelMultipleCodimMultipleGeomTypeMapper<GridType,
-      LayoutWrapper<commCodim>::Layout> LevelMapperType;
-  LevelMapperType levelMapper(grid, 0);
+      LayoutWrapper<commCodim>::template Layout> LevelMapperType;
 
-  typedef LeafMultipleCodimMultipleGeomTypeMapper<GridType,
-      LayoutWrapper<commCodim>::Layout> LeafMapperType;
+  LevelMapperType levelMapper(grid, 0);
   LeafMapperType leafMapper(grid);
 
-  std::cout << "Level index set has " << levelMapper.size() << " entities\n";
-  std::cout << "Leaf index set has " << leafMapper.size() << " entities\n";
+  std::cout << "Level index set has " << levelMapper.size() << " codim " << commCodim << " entities\n";
+  std::cout << "Leaf index set has " << leafMapper.size() << " codim " << commCodim << " entities\n";
 
-  // iterate over all elements and element intersections
-  checkConsistency(level0GridView);
-  checkConsistency(leafGridView);
+  assert(levelMapper.size() == leafMapper.size());
+
+  const int dim = GridType::dimension;
+
+  const typename GridType::LeafGridView &leafGridView = grid.leafView();
 
   // create the user data arrays
   typedef std::vector<Dune::FieldVector<double, 1> > UserDataType;
@@ -244,20 +240,16 @@ int main (int argc , char **argv) try
   UserDataType entityIndex(leafGridView.size(commCodim), -1e10);
   UserDataType partitionType(leafGridView.size(commCodim), -1e10);
 
-  // write the partition type of each node into the corrosponding
+  // write the partition type of each entity into the corrosponding
   // result array
-  GridType::LeafGridView::Codim<commCodim>::Iterator
-    it = leafGridView.begin<commCodim>();
-  const GridType::LeafGridView::Codim<commCodim>::Iterator
-  &endIt = leafGridView.end<commCodim>();
+  typename GridType::LeafGridView::template Codim<commCodim>::Iterator
+  it = leafGridView.template begin<commCodim>();
+  const typename GridType::LeafGridView::template Codim<commCodim>::Iterator
+  &endIt = leafGridView.template end<commCodim>();
   for (; it != endIt; ++it) {
     entityIndex[leafMapper.map(*it)] = leafMapper.map(*it);
     partitionType[leafMapper.map(*it)] = it->partitionType();
   };
-
-  //////////////////////////////////////////////////////
-  // Test communication
-  //////////////////////////////////////////////////////
 
   // initialize data handle (marks the nodes where some data was
   // send or received)
@@ -272,10 +264,10 @@ int main (int argc , char **argv) try
                    Dune::ForwardCommunication);
 
   //////////////////////////////////////////////////////
-  // Write results to disc
+  // Write results to disk
   //////////////////////////////////////////////////////
   std::cout << "Writing data to disk\n";
-  Dune::VTKWriter<GridType::LeafGridView> writer(leafGridView);
+  Dune::VTKWriter<typename GridType::LeafGridView> writer(leafGridView);
   if (commCodim == 0) {
     writer.addCellData(userDataSend, "Send");
     writer.addCellData(userDataReceive, "Receive");
@@ -288,9 +280,95 @@ int main (int argc , char **argv) try
     writer.addVertexData(partitionType, "Partition Type");
     writer.addVertexData(entityIndex, "Entity Index");
   }
-  writer.write("test-parallel-ug",
+
+  char fileName[1024];
+  sprintf(fileName, "test-parallel-ug-dim=%d-commCodim=%d", dim, commCodim);
+  writer.write(fileName,
                Dune::VTKOptions::ascii);
   std::cout << "Done writing data to disk\n";
+};
+
+template <int dim>
+void testParallelUG()
+{
+  std::cout << "Testing parallel UGGrid for " << dim << "D\n";
+
+  ////////////////////////////////////////////////////////////
+  // Create uniform grid on rank 0
+  ////////////////////////////////////////////////////////////
+  int n = 11;
+  typedef UGGrid<dim> GridType;
+  GridType grid;
+  createGrid(grid, n);
+
+
+  //////////////////////////////////////////////////////
+  // Distribute the grid
+  //////////////////////////////////////////////////////
+  /*    grid.loadBalance(0,   // strategy
+                       0,   // minlevel
+                       2,   // depth
+                       32,   // maxlevel
+                       1);   // minelement
+   */
+  grid.loadBalance();
+
+
+  std::cout << "Process " << grid.comm().rank() + 1
+            << " has " << grid.size(0)
+            << " elements and " << grid.size(dim)
+            << " nodes.\n";
+
+  // make sure each process has some elements
+  assert(grid.size(0) > 0);
+  // make sure each process has some nodes
+  assert(grid.size(dim) > 0);
+
+  //////////////////////////////////////////////////////
+  // Test level and leaf mappers/gridViews
+  //////////////////////////////////////////////////////
+  typename GridType::LevelGridView level0GridView = grid.levelView(0);
+  typename GridType::LeafGridView leafGridView = grid.leafView();
+
+  std::cout << "LevelGridView for level 0 has " << level0GridView.size(0)
+            << " elements "  << level0GridView.size(dim - 1)
+            << " edges and " << level0GridView.size(dim)
+            << " nodes.\n";
+  std::cout << "LeafGridView has " << leafGridView.size(0)
+            << " elements, " << leafGridView.size(dim - 1)
+            << " edges and " << leafGridView.size(dim)
+            << " nodes.\n";
+
+  // iterate over all elements and element intersections
+  checkConsistency(level0GridView);
+  checkConsistency(leafGridView);
+
+  //////////////////////////////////////////////////////
+  // Test element and node communication
+  //////////////////////////////////////////////////////
+  testCommunication<GridType, 0>(grid);
+  testCommunication<GridType, dim>(grid);
+};
+
+int main (int argc , char **argv) try
+{
+  // initialize MPI, finalize is done automatically on exit
+  Dune::MPIHelper &mpiHelper =
+    Dune::MPIHelper::instance(argc, argv);
+
+  std::cout << "This is process "
+            << mpiHelper.rank() + 1
+            << " of "
+            << mpiHelper.size()
+            << ", PID "
+            << getpid()
+            << " .\n";
+
+  // test 2D grid
+  testParallelUG<2>();
+
+  // test 3D grid
+  testParallelUG<3>();
 
   return 0;
 }
