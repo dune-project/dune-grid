b50546a2463c6b7741c5ebc93aa178d76f9dbb15
Robert Klöfkorn <robertk@dune-project.org>
2009-05-14 12:45:25 +0000

    docu bug.
    [[Imported from SVN: r5195]]

--- grid/albertagrid/entity.hh
+++ grid/albertagrid/entity.hh
@@ -259,7 +259,7 @@ namespace Dune
      *
      *  \returns an EntityPointer to the subentity
      *
-     *  \Note: The subentities are numbered 0, ..., count< codim >-1
+     *  \note: The subentities are numbered 0, ..., count< codim >-1
      */
     template< int codim >
     typename Codim< codim >::EntityPointer subEntity ( int i ) const;
