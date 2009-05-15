4005e7c2e2aaa31b5697d0c000d3ca628d120f3b
Martin Nolte <mnolte@dune-project.org>
2009-05-15 12:26:57 +0000

    add a method to check whether a certain topology can be represented by a GeometryType
    [[Imported from SVN: r5201]]

--- grid/genericgeometry/conversion.hh
+++ grid/genericgeometry/conversion.hh
@@ -623,31 +623,49 @@ namespace Dune
     }
 
 
+
+    // hasGeometryType
+    // ---------------
+
+    inline bool
+    hasGeometryType ( const unsigned int topologyId, const unsigned int dimension )
+    {
+      assert( topologyId < ((unsigned int)1 << dimension) );
+      if( dimension > 3 )
+      {
+        const unsigned int dimBit = ((unsigned int)1 << (dimension-1));
+        const unsigned int baseId = topologyId & (dimBit-1);
+        return ((baseId | 1) == 1) || ((baseId | 1) == (dimBit-1));
+      }
+      else
+        return true;
+    }
+
+
+
     // geometryType
     // ------------
 
     inline GeometryType
     geometryType ( const unsigned int topologyId, const unsigned int dimension )
     {
-      static const GeometryType::BasicType basicType1[ 2 ]
-        = { GeometryType::simplex, GeometryType::cube };
-      static const GeometryType::BasicType basicType2[ 4 ]
-        = { GeometryType::simplex, GeometryType::pyramid, GeometryType::prism, GeometryType::cube };
+      assert( hasGeometryType( topologyId, dimension ) );
 
-      assert( topologyId < ((unsigned int)1 << dimension) );
-
-      if( dimension == 0 )
-        return GeometryType( GeometryType::simplex, 0 );
-      if( dimension <= 2 )
+      if( dimension >= 3 )
+      {
+        static const GeometryType::BasicType basicType2[ 4 ]
+          = { GeometryType::simplex, GeometryType::pyramid, GeometryType::prism, GeometryType::cube };
+        const unsigned int t = topologyId >> (dimension-2);
+        return GeometryType( basicType2[ t ], dimension );
+      }
+      else if( dimension > 0 )
+      {
+        static const GeometryType::BasicType basicType1[ 2 ]
+          = { GeometryType::simplex, GeometryType::cube };
         return GeometryType( basicType1[ topologyId >> (dimension-1) ], dimension );
-
-      const unsigned int dimBit = (1 << (dimension-1));
-      const unsigned int baseId = topologyId & (dimBit-1);
-      if( ((baseId | 1) != 1) && ((baseId | 1) != (dimBit-1)) )
-        DUNE_THROW( RangeError, "Topology " << topologyId << " has no corresponding GeometryType." );
-
-      const unsigned int t = topologyId >> (dimension-2);
-      return GeometryType( basicType2[ t ], dimension );
+      }
+      else
+        return GeometryType( GeometryType::simplex, 0 );
     }
 
   }
