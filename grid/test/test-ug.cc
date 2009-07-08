8e12c88c99327985121e24caab50fb0a5739b451
Oliver Sander <sander@dune-project.org>
2009-07-08 14:21:26 +0000

    the file with hybrid test grids has changed location
    [[Imported from SVN: r5298]]

--- grid/test/test-ug.cc
+++ grid/test/test-ug.cc
@@ -13,7 +13,7 @@
  */
 
 #include <dune/grid/uggrid.hh>
-#include <dune/doc/grids/ugascplusplus/ugtestgrids.hh>
+#include <dune/doc/grids/gridfactory/hybridtestgrids.hh>
 
 #include "gridcheck.cc"
 #include "checkcommunicate.cc"
