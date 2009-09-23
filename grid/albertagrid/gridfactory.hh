47714539ca1abe6157ab65cef454f7afc922374f
Martin Nolte <mnolte@dune-project.org>
2009-09-23 09:11:10 +0000

    * always reorient in 2d (for faster computation of normal) * never reorient in 3d (prone to breaking the recursive bisection algorithm)
    [[Imported from SVN: r5445]]

--- grid/albertagrid/gridfactory.hh
+++ grid/albertagrid/gridfactory.hh
@@ -222,9 +222,9 @@ namespace Dune
     Grid *createGrid ( const std::string &gridName )
     {
       macroData_.finalize();
-#if DUNE_ALBERTA_VERSION < 0x300
-      macroData_.setOrientation( Alberta::Real( 1 ) );
-#endif // #if DUNE_ALBERTA_VERSION < 0x300
+      if( dimension < 3 )
+        macroData_.setOrientation( Alberta::Real( 1 ) );
+      assert( macroData_.checkNeighbors() );
       return new Grid( macroData_, gridName, boundaryProjection_ );
     }
 
@@ -264,9 +264,9 @@ namespace Dune
     {
       dune_static_assert( type != pgm, "AlbertaGridFactory: writing pgm format is not supported." );
       macroData_.finalize();
-#if DUNE_ALBERTA_VERSION < 0x300
-      macroData_.setOrientation( Alberta::Real( 1 ) );
-#endif // #if DUNE_ALBERTA_VERSION < 0x300
+      if( dimension < 3 )
+        macroData_.setOrientation( Alberta::Real( 1 ) );
+      assert( macroData_.checkNeighbors() );
       return macroData_.write( filename, (type == xdr) );
     }
 
