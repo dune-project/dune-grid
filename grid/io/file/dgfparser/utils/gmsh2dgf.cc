cbd9d6c398bb4c7e63a28f03d0abc14065481e38
Martin Nolte <mnolte@dune-project.org>
2009-09-26 13:35:36 +0000

    add a small demo program to convert gmsh-files into dgf-files
    [[Imported from SVN: r5528]]

---
+++ grid/io/file/dgfparser/utils/gmsh2dgf.cc
@@ -0,0 +1,64 @@
+// -*- tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 2 -*-
+// vi: set et ts=4 sw=2 sts=2:
+/** \file
+ *  \author Matrin Nolte
+ *  \brief a small program converting a gmsh file into a DGF file
+ *
+ *  gmsh2dgf is a small example program for the DGFWriter. It reads a gmsh file
+ *  into any grid (selected by gridtype.hh) and writes it back as a DGF file.
+ *
+ *  The program's usage is as follows:
+ *  \code
+ *  ./gmsh2dgf <gmshfile>
+ *  \endcode
+ */
+
+#include <config.h>
+
+#include <iostream>
+
+#include <dune/common/mpihelper.hh>
+
+#include <dune/grid/utility/gridtype.hh>
+#include <dune/grid/io/file/gmshreader.hh>
+#include <dune/grid/io/file/dgfparser/dgfwriter.hh>
+
+using namespace Dune;
+
+int main ( int argc, char *argv[] )
+try
+{
+  Dune::MPIHelper::instance( argc, argv );
+
+  if( argc < 2 )
+  {
+    std::cerr << "Usage: " << argv[ 0 ] << " <gmshfile>" << std::endl;
+    return 1;
+  }
+
+  const std::string gmshFileName( argv[ 1 ] );
+  std::string dgfFileName( gmshFileName );
+  dgfFileName.resize( dgfFileName.find_last_of( "." ) );
+  dgfFileName += ".dgf";
+
+  GridType *grid = GmshReader< GridType  >::read( gmshFileName );
+
+  typedef GridType::LeafGridView GridView;
+  GridView gridView = grid->leafView();
+
+  DGFWriter< GridView > dgfWriter( gridView );
+  dgfWriter.write( dgfFileName );
+
+  const GridView::IndexSet &indexSet = gridView.indexSet();
+  std::cerr << "Grid successfully written: "
+            << indexSet.size( GridType::dimension ) << " vertices, "
+            << indexSet.size( 0 ) << " elements."
+            << std::endl;
+
+  return 0;
+}
+catch( const Exception &exception )
+{
+  std::cerr << exception << std::endl;
+  return 1;
+}
