ac6587ff1068fec9ed136f8876d88913fe4d5bd5
Martin Nolte <mnolte@dune-project.org>
2009-09-29 09:05:25 +0000

    create access to the host intersection (required for TwistUtility)
    [[Imported from SVN: r5647]]

--- grid/utility/hostgridaccess.hh
+++ grid/utility/hostgridaccess.hh
@@ -1,31 +1,26 @@
 // -*- tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 2 -*-
 // vi: set et ts=4 sw=2 sts=2:
-#ifndef DUNE_GEOGRID_HOST_GRID_ACCESS_HH
-#define DUNE_GEOGRID_HOST_GRID_ACCESS_HH
+#ifndef DUNE_GEOGRID_HOSTGRIDACCESS_HH
+#define DUNE_GEOGRID_HOSTGRIDACCESS_HH
 
 #include <string>
 
 namespace Dune
 {
+
   // HostGridAccess
-  // ------------
+  // --------------
 
   /** \class HostGridAccess
-   *  \brief provides access to HostGrid Entities and EntityPointers
+   *  \brief provides access to host grid objects
    *
    *  \tparam GeometryGrid
    *
    *  \nosubgrouping
    */
-
   template< class GeometryGrid >
-  class HostGridAccess
+  struct HostGridAccess
   {
-
-    typedef HostGridAccess< GeometryGrid > ThisType;
-
-  public:
-
     /** \name Exported Types
      * \{ */
 
@@ -41,17 +36,34 @@ namespace Dune
     template< int codim >
     struct Codim
     {
-      //! type of HostEntity
-      typedef typename GeometryGrid :: HostGrid :: template Codim< codim > :: Entity HostEntity;
-      //! type of HostEntityPointer
-      typedef typename GeometryGrid :: HostGrid :: template Codim< codim > :: EntityPointer HostEntityPointer;
+      //! type of the GeometryGrid entity
+      typedef typename GeometryGrid :: template Codim< codim > :: Entity Entity;
+      //! type of the GeometryGrid entity pointer
+      typedef typename GeometryGrid :: template Codim< codim > :: EntityPointer
+      EntityPointer;
+
+      //! type of the host entity
+      typedef typename HostGrid :: template Codim< codim > :: Entity HostEntity;
+      //! type of the host entity pointer
+      typedef typename HostGrid :: template Codim< codim > :: EntityPointer
+      HostEntityPointer;
     };
+
+    //! type of the GeometryGrid leaf intersection
+    typedef typename GeometryGrid :: Traits :: LeafIntersection LeafIntersection;
+    //! type of the GeometryGrid level intersection
+    typedef typename GeometryGrid :: Traits :: LevelIntersection LevelIntersection;
+
+    //! type of the host leaf intersection
+    typedef typename HostGrid :: Traits :: LeafIntersection HostLeafIntersection;
+    //! type of the host level intersection
+    typedef typename HostGrid :: Traits :: LevelIntersection HostLevelIntersection;
 
     /** \brief Get underlying HostGrid.
      *  \param[in] grid  GeometryGrid
      *  \returns HostGrid
      */
-    static const HostGrid & hostGrid (const GeometryGrid & grid)
+    static const HostGrid &hostGrid ( const GeometryGrid &grid )
     {
       return grid.hostGrid();
     }
@@ -63,11 +75,10 @@ namespace Dune
      *  \param[in] entity  GeometryGridEntity
      *  \returns HostGridEntity
      */
-    template < int codim >
-    static const typename ThisType :: template Codim< codim > :: HostEntity &
-    getHostEntity ( const GeometryGrid & grid,
-                    const typename GeometryGrid :: template Codim< codim > :: Entity & entity
-                    )
+    template< int codim >
+    static const typename Codim< codim > :: HostEntity &
+    getHostEntity ( const GeometryGrid &grid,
+                    const typename Codim< codim > :: Entity &entity )
     {
       return grid.template getHostEntity< codim > ( entity );
     }
@@ -79,15 +90,25 @@ namespace Dune
      *  \param[in] entityPointer  GeometryGridEntityPointer
      *  \returns HostEntityPointer
      */
-    template < int codim >
-    static const typename ThisType :: template Codim< codim > :: HostEntityPointer &
-    getHostEntityPointer ( const GeometryGrid & grid,
-                           const typename GeometryGrid :: template Codim< codim > :: EntityPointer & entityPointer
-                           )
+    template< int codim >
+    static const typename Codim< codim > :: HostEntityPointer &
+    getHostEntityPointer ( const GeometryGrid &grid,
+                           const typename Codim< codim > :: EntityPointer &entityPointer )
     {
       return grid.template getHostEntityPointer< codim > ( entityPointer );
     }
 
+    static const HostLeafIntersection &
+    getIntersection ( const LeafIntersection &intersection )
+    {
+      return GeometryGrid :: getRealImplementation( intersection ).hostIntersection();
+    }
+
+    static const HostLevelIntersection &
+    getIntersection ( const LevelIntersection &intersection )
+    {
+      return GeometryGrid :: getRealImplementation( intersection ).hostIntersection();
+    }
   };
 
 }
