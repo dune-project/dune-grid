8c92f9fb609ce9bd1cd8666a46bee8dc93619fae
Martin Nolte <mnolte@dune-project.org>
2009-09-29 09:10:46 +0000

    missing file
    [[Imported from SVN: r5656]]

---
+++ grid/geogrid/backuprestore.hh
@@ -0,0 +1,66 @@
+// -*- tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 2 -*-
+// vi: set et ts=4 sw=2 sts=2:
+#ifndef DUNE_GEOGRID_BACKUPRESTORE_HH
+#define DUNE_GEOGRID_BACKUPRESTORE_HH
+
+#include <dune/grid/utility/grapedataioformattypes.hh>
+
+namespace Dune
+{
+
+  namespace GeoGrid
+  {
+
+    // BackupRestoreFacilities
+    // -----------------------
+
+    template< class Grid, bool hasBackupRestoreFacilities >
+    class BackupRestoreFacilities
+    {};
+
+    template< class Grid >
+    class BackupRestoreFacilities< Grid, true >
+    {
+      typedef BackupRestoreFacilities< Grid, true > This;
+
+    protected:
+      BackupRestoreFacilities ()
+      {}
+
+    private:
+      BackupRestoreFacilities ( const This & );
+      This &operator= ( const This & );
+
+    public:
+      template< GrapeIOFileFormatType type >
+      bool writeGrid ( const std :: string &filename, double time ) const
+      {
+        return asImp().hostGrid().template writeGrid< type >( filename, time );
+      }
+
+      template< GrapeIOFileFormatType type >
+      bool readGrid ( const std :: string &filename, double &time )
+      {
+        const bool success
+          = asImp().hostGrid().template readGrid< type >( filename, time );
+        asImp().update();
+        return success;
+      }
+
+    protected:
+      const Grid &asImp () const
+      {
+        return static_cast< const Grid & >( *this );
+      }
+
+      Grid &asImp ()
+      {
+        return static_cast< Grid & >( *this );
+      }
+    };
+
+  }
+
+}
+
+#endif
