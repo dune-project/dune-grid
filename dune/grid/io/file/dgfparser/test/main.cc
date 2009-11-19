0bff09afff18259e94e0c6e89a1c2cd86d9722f2
Martin Nolte <mnolte@dune-project.org>
2009-11-19 16:23:08 +0000

    more memory efficient use of GridPtr
    [[Imported from SVN: r5960]]

--- dune/grid/io/file/dgfparser/test/main.cc
+++ dune/grid/io/file/dgfparser/test/main.cc
@@ -8,6 +8,11 @@
 #include <dune/grid/test/gridcheck.cc>
 #include "../dgfgridtype.hh"
 
+#if HAVE_GRAPE
+#include <dune/grid/io/visual/grapegriddisplay.hh>
+#include <dune/grid/io/visual/grapedatadisplay.hh>
+#endif
+
 namespace Dune
 {
 
@@ -17,9 +22,6 @@ namespace Dune
 }
 
 using namespace Dune;
-#if HAVE_GRAPE
-#include <dune/grid/io/visual/grapegriddisplay.hh>
-#include <dune/grid/io/visual/grapedatadisplay.hh>
 
 template< int dim, int dimworld >
 struct EnableLevelIntersectionIteratorCheck< AlbertaGrid< dim, dimworld > >
@@ -28,11 +30,11 @@ struct EnableLevelIntersectionIteratorCheck< AlbertaGrid< dim, dimworld > >
 };
 
 template< class GridView >
-void test ( const GridView &view,
-            std::vector< double > &elDat, int nofElParams,
-            std::vector< double > &vtxDat, int nofVtxParams )
+void display ( const GridView &view,
+               std::vector< double > &elDat, int nofElParams,
+               std::vector< double > &vtxDat, int nofVtxParams )
 {
-  gridcheck( const_cast< typename GridView::Grid & >( view.grid() ) );
+#if HAVE_GRAPE
   if( nofElParams + nofVtxParams > 0 )
   {
     GrapeDataDisplay< typename GridView::Grid > disp( view );
@@ -45,14 +47,9 @@ void test ( const GridView &view,
     GrapeGridDisplay< typename GridView::Grid > disp( view.grid() );
     disp.display();
   }
+#endif // #if HAVE_GRAPE
 }
-#else
-template< class GridView >
-void test( const GridView &view,std::vector<double>& dat,int nofParams,int cdim)
-{
-  gridcheck( const_cast< typename GridView::Grid & >( view.grid() ) );
-}
-#endif
+
 template< class GridView >
 void test ( const GridView &view )
 {
@@ -65,76 +62,89 @@ try {
   MPIHelper & mpiHelper = MPIHelper::instance(argc,argv);
   int myrank = mpiHelper.rank();
 
-  if (argc<2) {
+  if (argc<2)
+  {
     std::cerr << "supply grid file as parameter!" << std::endl;
     return 1;
   }
 
   std::cout << "tester: start grid reading" << std::endl;
 
-  // create Grid from DGF parser
-  GridPtr<GridType> grid;
-  {
-    grid = GridPtr<GridType>(argv[1], mpiHelper.getCommunicator() );
-  }
-
   typedef GridType::LeafGridView GridView;
   typedef GridView::IndexSet IndexSetType;
 
-  GridView gridView = grid->leafView();
-  const IndexSetType &index = gridView.indexSet();
-  std::vector<double> eldat(0),vtxdat(0);
-  if (grid.nofParameters(0)>0)
+  // create Grid from DGF parser
+  GridType *grid;
+  size_t nofElParams( 0 ), nofVtxParams( 0 );
+  std::vector< double > eldat( 0 ), vtxdat( 0 );
   {
-    std::cout << "Reading Element Parameters:" << std::endl;
-    eldat.resize(index.size(0)*grid.nofParameters(0));
-    typedef GridView::Codim< 0 >::Iterator IteratorType;
-    const IteratorType endit = gridView.end< 0 >();
-    for (IteratorType iter=gridView.begin< 0 >(); iter!=endit; ++iter)
+    GridPtr< GridType > gridPtr( argv[1], mpiHelper.getCommunicator() );
+
+    GridView gridView = gridPtr->leafView();
+    const IndexSetType &indexSet = gridView.indexSet();
+    nofElParams = gridPtr.nofParameters( 0 );
+    if( nofElParams > 0 )
     {
-      std::vector<double>& param = grid.parameters(*iter);
-      assert( (int) param.size() == grid.nofParameters(0) );
-      for (size_t i=0; i<param.size(); i++)
+      std::cout << "Reading Element Parameters:" << std::endl;
+      eldat.resize( indexSet.size(0) * nofElParams );
+      typedef GridView::Codim< 0 >::Iterator IteratorType;
+      const IteratorType endit = gridView.end< 0 >();
+      for( IteratorType iter = gridView.begin< 0 >(); iter != endit; ++iter )
       {
-        //std::cout << param[i] << " ";
-        eldat[index.index(*iter)*param.size()+i] = param[i];
+        const std::vector< double > &param = gridPtr.parameters( *iter );
+        assert( param.size() == nofElParams );
+        for( size_t i = 0; i < nofElParams; ++i )
+        {
+          //std::cout << param[i] << " ";
+          eldat[ indexSet.index(*iter) * nofElParams + i ] = param[ i ];
+        }
+        //std::cout << std::endl;
       }
-      //std::cout << std::endl;
     }
-  }
-  if (grid.nofParameters(GridType::dimension)>0)
-  {
-    std::cout << "Reading Vertex Parameters:" << std::endl;
-    vtxdat.resize(index.size(GridType::dimension)*grid.nofParameters(GridType::dimension));
-    typedef GridView::Codim< GridType::dimension >::Iterator IteratorType;
-    IteratorType endit = gridView.end< GridType::dimension >();
-    for (IteratorType iter=gridView.begin< GridType::dimension >();
-         iter!=endit; ++iter) {
-      std::vector<double>& param = grid.parameters(*iter);
-      assert( (int) param.size() == grid.nofParameters(GridType::dimension));
-      // std::cout << (*iter).geometry()[0] << " -\t ";
-      for (size_t i=0; i<param.size(); i++) {
-        // std::cout << param[i] << " ";
-        vtxdat[index.index(*iter)*param.size()+i] = param[i];
+
+    nofVtxParams = gridPtr.nofParameters( GridType::dimension );
+    if( nofVtxParams > 0 )
+    {
+      std::cout << "Reading Vertex Parameters:" << std::endl;
+      vtxdat.resize( indexSet.size( GridType::dimension ) * nofVtxParams );
+      typedef GridView::Codim< GridType::dimension >::Iterator IteratorType;
+      IteratorType endit = gridView.end< GridType::dimension >();
+      for( IteratorType iter = gridView.begin< GridType::dimension >(); iter != endit; ++iter )
+      {
+        const std::vector< double > &param = gridPtr.parameters( *iter );
+        assert( param.size() == nofVtxParams );
+        // std::cout << (*iter).geometry()[0] << " -\t ";
+        for( size_t i = 0; i < nofVtxParams; ++i )
+        {
+          // std::cout << param[i] << " ";
+          vtxdat[ indexSet.index(*iter) * nofVtxParams + i ] = param[ i ];
+        }
+        // std::cout << std::endl;
       }
-      // std::cout << std::endl;
     }
+
+    grid = gridPtr.release();
   }
 
+  GridView gridView = grid->leafView();
   // display
-  if (myrank <= 0)
-    test(gridView,eldat,grid.nofParameters(0),vtxdat,grid.nofParameters(GridType::dimension));
+  if( myrank <= 0 )
+    display( gridView, eldat, nofElParams, vtxdat, nofVtxParams );
   // refine
   std::cout << "tester: refine grid" << std::endl;
   grid->globalRefine(Dune::DGFGridInfo<GridType>::refineStepsForHalf());
   test(gridView);
+
+  delete grid;
   return 0;
 }
-catch (Dune::Exception &e) {
+catch( const Dune::Exception &e )
+{
   std::cerr << e << std::endl;
   return 1;
 }
-catch (...) {
+catch (...)
+{
   std::cerr << "Generic exception!" << std::endl;
   return 1;
 }
