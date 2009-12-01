9a872527b8a06910b2a42a0d975f911066e6bda0
Andreas Dedner <dedner@dune-project.org>
2009-12-01 16:44:57 +0000

    cleanup
    [[Imported from SVN: r6000]]

--- dune/grid/io/file/dgfparser/dgfyasp.hh
+++ dune/grid/io/file/dgfparser/dgfyasp.hh
@@ -22,19 +22,13 @@ namespace Dune {
       : public GridParameterBlock
     {
     protected:
-#if 0
-      std::set<int> _periodic; // periodic grid
-#endif
-      int _overlap; // overlap for YaspGrid
+      int _overlap;     // overlap for YaspGrid
 
     public:
       //! constructor taking istream
       YaspGridParameterBlock( std::istream &in )
         : GridParameterBlock( in ),
-#if 0
-          _periodic(), // default is non periodic
-#endif
-          _overlap( 0 ) // default value
+          _overlap( 0 )  // default value
       {
         // check overlap
         if( findtoken( "overlap" ) )
@@ -57,22 +51,6 @@ namespace Dune {
                 << "defaulting to '" << _overlap << "'." << std::endl;
         }
 
-#if 0
-        // check periodic grid
-        if (findtoken("periodic"))
-        {
-          int x;
-          while (getnextentry(x))
-          {
-            _periodic.insert(x);
-          }
-        }
-        else
-        {
-          dwarn << "YaspGridParameterBlock: Parameter 'periodic' not specified, "
-                << "defaulting to no periodic boundary." << std::endl;
-        }
-#endif
       }
 
       //! get dimension of world found in block
@@ -81,13 +59,6 @@ namespace Dune {
         return _overlap;
       }
 
-#if 0
-      //! returns true if dimension is periodic
-      bool isPeriodic ( const int dim ) const
-      {
-        return (_periodic.find(dim) != _periodic.end());
-      }
-#endif
     };
 
   }
@@ -104,6 +75,96 @@ namespace Dune {
     static int refineStepsForHalf() {return 1;}
     static double refineWeight() {return pow(0.5,dim);}
   };
+
+  template< int dim >
+  inline YaspGrid< dim > *
+  MacroGrid::Impl< YaspGrid< dim > >
+  ::generate ( MacroGrid &mg, const char *filename, MPICommunicatorType MPICOMM )
+  {
+
+    std::ifstream gridin( filename );
+    dgf::IntervalBlock intervalBlock( gridin );
+
+    if( !intervalBlock.isactive() )
+    {
+      DUNE_THROW( DGFException,
+                  "Macrofile " << filename << " must have Intervall-Block "
+                               << "to be used to initialize YaspGrid!\n"
+                               << "No alternative File-Format defined");
+    }
+
+    if( intervalBlock.numIntervals() != 1 )
+      DUNE_THROW( DGFException, "YaspGrid can only handle 1 interval block." );
+
+    if( intervalBlock.dimw() != dim )
+    {
+      DUNE_THROW( DGFException,
+                  "Cannot read an interval of dimension " << intervalBlock.dimw()
+                                                          << "into a YaspGrid< " << dim << " >." );
+    }
+
+    mg.element = Cube;
+    mg.dimw = intervalBlock.dimw();
+
+    const dgf::IntervalBlock::Interval &interval = intervalBlock.get( 0 );
+
+    FieldVector<double,dim> lang;
+    FieldVector<int,dim>    anz;
+    for( int i = 0; i < dim; ++i )
+    {
+      // check that start point is > 0.0
+      if( fabs( interval.p[ 0 ][ i ] ) > 1e-10 )
+      {
+        DUNE_THROW( DGFException,
+                    "YaspGrid cannot handle grids with non-zero left lower corner." );
+      }
+
+      lang[ i ] = interval.p[ 1 ][ i ] - interval.p[ 0 ][ i ];
+      anz[ i ]  = interval.n[ i ];
+    }
+
+    typedef dgf::PeriodicFaceTransformationBlock::AffineTransformation Transformation;
+    dgf::PeriodicFaceTransformationBlock trafoBlock( gridin, dim );
+    FieldVector< bool, dim > per( false );
+    const int numTrafos = trafoBlock.numTransformations();
+    for( int k = 0; k < numTrafos; ++k )
+    {
+      const Transformation &trafo = trafoBlock.transformation( k );
+
+      bool identity = true;
+      for( int i = 0; i < dim; ++i )
+        for( int j = 0; j < dim; ++j )
+          identity &= (fabs( (i == j ? 1.0 : 0.0) - trafo.matrix( i, j ) ) < 1e-10);
+      if( !identity )
+        DUNE_THROW( DGFException, "YaspGrid can only handle shifts as periodic face transformations." );
+
+      int numDirs = 0;
+      int dir = -1;
+      for( int i = 0; i < dim; ++i )
+      {
+        if( fabs( trafo.shift[ i ] ) < 1e-10 )
+          continue;
+        dir = i;
+        ++numDirs;
+      }
+      if( (numDirs != 1) || (fabs( fabs( trafo.shift[ dir ] ) - lang[ dir ] ) >= 1e-10) )
+      {
+        std::cerr << "Tranformation '" << trafo
+                  << "' does not map boundaries on boundaries." << std::endl;
+      }
+      else
+        per[ dir ] = true;
+    }
+
+    // get grid parameters
+    dgf::YaspGridParameterBlock grdParam( gridin );
+
+    #if HAVE_MPI
+    return new YaspGrid<dim>(MPICOMM,lang, anz, per , grdParam.overlap() );
+    #else
+    return new YaspGrid<dim>(lang, anz, per , grdParam.overlap() );
+    #endif
+  }
+
 }
-#include "dgfyasp.cc"
 #endif
