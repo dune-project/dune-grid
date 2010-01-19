ae20e060a8b9698e588416780954a700406b48a9
Christian Engwer <christi@dune-project.org>
2010-01-19 17:08:52 +0000

    search the alberta grid files in the right directory
    [[Imported from SVN: r6193]]

--- dune/grid/test/check-albertareader.cc
+++ dune/grid/test/check-albertareader.cc
@@ -14,7 +14,7 @@ void checkAlbertaReader ()
   std::cout << ">>> Checking AlbertaReader..." << std::endl;
 
   std::ostringstream filename;
-  filename << "grid-" << Grid::dimension << "-" << Grid::dimensionworld << ".amc";
+  filename << SRCDIR << "grid-" << Grid::dimension << "-" << Grid::dimensionworld << ".amc";
 
   Dune::AlbertaReader< Grid > reader;
   Dune::GridFactory< Grid > factory;
