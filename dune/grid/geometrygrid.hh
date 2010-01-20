00d7b87adea82d24e45b9aa6e0d0ffe7ca6770d2
Oliver Sander <sander@dune-project.org>
2010-01-20 16:57:08 +0000

    Polish the documentation.  I think it is more understandable now. Also there is an example for a discretely given deformation function now.
    [[Imported from SVN: r6212]]

--- dune/grid/geometrygrid.hh
+++ dune/grid/geometrygrid.hh
@@ -25,34 +25,35 @@
  *    \end{array}\right).
  *  \f]
  *  Though YaspGrid can only model plane, Carthesian grids, using GeometryGrid
- *  we have obtained a parallel surface grid with quadrilateral elements.
+ *  we have obtained a nonplanar surface grid with quadrilateral elements.
  *
  *  \section features Features
  *
  *  Features of the GeometryGrid include:
- *  - complete wrapper of the host grid
+ *  - Complete wrapper of the host grid
  *    (i.e., no non-geometric feature of the host grid is lost)
  *  - Only uses the coordinate of the corners of each entity -
- *    no other geometric information of the underlying grid is used.
- *  - provides entities for all codimensions, even if the host grid does not
+ *    no other geometric information needs to be provided.
+ *  - Provides entities for all codimensions, even if the host grid does not
  *    (though communication is not extended to these codimensions)
  *  .
  *
  *  \section usage Usage
  *
- *  There are three different construction mechanisms for a geometry grid.
- *  In each case a instance of the host grid must be provided and in
- *  addition:
- *  - a function mapping
- *    global coordinates from the host grid to some space
- *    with larger or equal dimension. For an entity \c e of the host grid
- *    with geometry \c e.g the resulting entity in the Dune::GeometryGrid has
- *    corners \c F(e.g.corner(i)) where \c F is the global coordinate
- *    mapping provided. For the geometry of the Dune::GeometryGrid the class
- *    Dune::GenericGeometry::CornerMapping is used.
- *  - a vector like container assinging each corner of a host entity a codimension.
+ *  There are two different construction mechanisms for a geometry grid.
+ *  They differ in how the new geometry is provided.
+ *  - The geometry can be specified by giving a global functions that maps the
+ *    world space of the host grid to another Euclidean space.  In principle
+ *    this target space can have any dimension, but in practice its dimension will
+ *    be larger than or equal to the dimension of the host grid world.
+ *    The function will be evaluated at the vertex positions of the host grid.
+ *    It may be given analytically or defined on some grid.  In the latter
+ *    case note, however, that the function arguments are global coordinates
+ *    and you may have efficiency problems.
+ *  - The geometry can also be specified by giving a vector containing new positions
+ *    for each vertex of the host grid.
  *  .
- *  Remark: in the second case no geometry class has to be implemented by the
+ *  Remark: for the second case no geometry class has to be implemented by the
  *          host grid.
  *          In the first case the host grid must provide an implementation of
  *          the method <tt>corner</tt> on the geometry class for codimension
@@ -63,8 +64,8 @@
  *    GeometryGrid<HostGridType,CoordFunction> grid(hostGrid,coordFunction);
  *  \endcode
  *  The class \c CoordFunction must either be derived from
- *  Dune::AnalyticalCoordFunction or from
- *  Dune::DiscreteCoordFunction.
+ *  Dune::AnalyticalCoordFunction or from Dune::DiscreteCoordFunction.
+ *  If you want to use the first approach derive from Dune::AnalyticalCoordFunction.
  *  An example of a analytical coordinate function is given by the following code:
  *  \code
  *  class ExampleFunction
@@ -85,13 +86,63 @@
  *    }
  *  };
  *  \endcode
- *  For a discrete coordinate function a method of the form
+ *
+ *  If you want to prescribe your geometry by a set of coordinates you have to write
+ *  a deformation class and have it derive from Dune::DiscreteCoordFunction.
+ *  An example is given by the following code snippet.  Central to the class are the
+ *  two evaluate methods.  The first one accepts a host grid vertex and computes its new
+ *  position.  The second one accepts an element and a local corner number and
+ *  computes the position of this corner.  It is trivial to implement this using the
+ *  first evaluate method, and I don't know why it isn't done by default.
  *  \code
- *  template< class HostEntity >
- *  void evaluate ( const HostEntity &hostEntity, unsigned int corner,
- *                  RangeVector &y ) const
+   template <class GridView>
+   class DeformationFunction
+    : public Dune :: DiscreteCoordFunction< double, dim, DeformationFunction<GridView> >
+   {
+    typedef DeformationFunction<GridView> This;
+    typedef Dune :: DiscreteCoordFunction< double, dim, This > Base;
+
+   public:
+
+    DeformationFunction(const GridView& gridView,
+                        const double* deformedPosition)
+        : gridView_(gridView),
+          deformedPosition_(deformedPosition)
+    {}
+
+    void evaluate ( const typename GridView::template Codim<dim>::Entity& hostEntity, unsigned int corner,
+                    FieldVector<double,dim> &y ) const
+    {
+
+        const typename GridView::IndexSet& indexSet = gridView_.indexSet();
+
+        int idx = indexSet.index(hostEntity);
+
+        for (int i=0; i<dim; i++)
+            y[i] = deformedPosition_[idx*dim + i];
+    }
+
+    void evaluate ( const typename GridView::template Codim<0>::Entity& hostEntity, unsigned int corner,
+                    FieldVector<double,dim> &y ) const
+    {
+
+        const typename GridView::IndexSet& indexSet = gridView_.indexSet();
+
+        int idx = indexSet.subIndex(hostEntity, corner,dim);
+
+        for (int i=0; i<dim; i++)
+            y[i] = deformedPosition_[idx*dim + i];
+    }
+
+   private:
+
+    GridView gridView_;
+
+    const double* deformedPosition_;
+
+   };
  *  \endcode
- *  must be implemented.
+ *
  *
  *  \note The <tt>GeometryGrid</tt> is able to forward a non-standard feature
  *        of some DUNE grids: the <tt>HierarchicIndexSet</tt>.
