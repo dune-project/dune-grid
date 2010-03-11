de914c49b8309bc908d41202db4367cb4e41fe69
Martin Nolte <mnolte@dune-project.org>
2010-03-11 15:59:27 +0000

    remove long deprecated file dgfparser/gridtype.hh (already deprecated in the 1.2 series)
    [[Imported from SVN: r6404]]

--- dune/grid/io/file/dgfparser/gridtype.hh
+++
@@ -1,8 +0,0 @@
-// -*- tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 2 -*-
-// vi: set et ts=4 sw=2 sts=2:
-#ifndef DUNE_GRIDTYPE_HH_OLD
-#define DUNE_GRIDTYPE_HH_OLD
-#warning dune/grid/io/file/dgfparser/gridtype.hh: This header is deprecated!
-#warning Please use dune/grid/io/file/dgfparser/dgfgridtype.hh!
-#include "dgfgridtype.hh"
-#endif
