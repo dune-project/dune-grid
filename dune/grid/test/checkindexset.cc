f1cc71bb11cf2eeb2c2864062491d13b63e03f12
Oliver Sander <sander@dune-project.org>
2010-03-12 18:27:57 +0000

    more simplification
    [[Imported from SVN: r6428]]

--- dune/grid/test/checkindexset.cc
+++ dune/grid/test/checkindexset.cc
@@ -175,32 +175,27 @@ namespace Dune
           const int gj = Dune::GenericGeometry::MapNumberingProvider< dim-codim >::template dune2generic< dim-codim >( tid, j );
 #endif
 
-          {
-            // get entity pointer to subEntity vertex
-            typedef typename GridType::template Codim< dim >::EntityPointer VertexPointer;
-            VertexPointer vxp = en.template subEntity< dim >( local[ j ] );
+          // get entity pointer to subEntity vertex
+          typedef typename GridType::template Codim< dim >::EntityPointer VertexPointer;
+          VertexPointer vxp = en.template subEntity< dim >( local[ j ] );
 
-            // Find the global coordinate of the vertex by its index
-            if(vertexCoordsMap.find(global[j]) != vertexCoordsMap.end())
-            {
-              // Check whether index and coordinate match
-              FieldVector<coordType,dimworld> vxcheck ( vertexCoordsMap.at(global[j]) );
-              FieldVector< coordType, dimworld > vx = vxp->geometry().corner( 0 );
-              if( ! compareVec( vxcheck, vx ) )
-              {
-                std::cerr << "ERROR map global vertex [" << global[j] << "] vx " << vxcheck << " is not " << vx << "\n";
-                assert( compareVec( vxcheck, vx ) );
-              }
-            }
-          }
-
+          // Find the global coordinate of the vertex by its index
           if(vertexCoordsMap.find(global[j]) != vertexCoordsMap.end())
           {
+            // Check whether index and coordinate match
             FieldVector<coordType,dimworld> vxcheck ( vertexCoordsMap.at(global[j]) );
-            FieldVector< coordType, dimworld > vx = subEntityPtr->geometry().corner( gj );
-            if( ! compareVec( vxcheck, vx ) )
+            FieldVector< coordType, dimworld > vx1 = vxp->geometry().corner( 0 );
+            if( ! compareVec( vxcheck, vx1 ) )
             {
-              std::cerr << "Error map global vertex [" << global[j] << "] vx " << vxcheck << " is not " << vx << "\n";
+              std::cerr << "ERROR map global vertex [" << global[j] << "] vx " << vxcheck << " is not " << vx1 << "\n";
+              assert( compareVec( vxcheck, vx1 ) );
+            }
+
+            // Check again, but this time get the coordinate as a subEntity corner
+            FieldVector< coordType, dimworld > vx2 = subEntityPtr->geometry().corner( gj );
+            if( ! compareVec( vxcheck, vx2 ) )
+            {
+              std::cerr << "Error map global vertex [" << global[j] << "] vx " << vxcheck << " is not " << vx2 << "\n";
               //assert( compareVec( vxcheck, vx ) );
             }
           }
