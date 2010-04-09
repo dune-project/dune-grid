e67d73857d512a2ba01dad4685879b8e66228769
Christian Engwer <christi@dune-project.org>
2010-04-09 13:00:54 +0000

    Fix the test for 3D refinement in Alberta.
    - Output should make it completely clear now what is happening and why.
    - In addition to the 5-triangulated unit cube, also check the 6-triangulated
      unit cube and the unit tetrahedron.  The latter is a safety measure: it
      refining the unit tetrahedron results in a segfault, chances are that the
      recursive bisection is *not* the culprit.
    - The test is now expected to pass
    
    
    [[Imported from SVN: r6599]]

--- dune/grid/albertagrid/test/test-alberta3d-refine.cc
+++ dune/grid/albertagrid/test/test-alberta3d-refine.cc
@@ -20,9 +20,69 @@
 #include <dune/grid/albertagrid/gridfactory.hh>
 #endif
 
+// single unit tet
+template<typename Grid>
+class UnitTetMaker {
+  dune_static_assert(Grid::dimension == 3, "Dimension of grid must be 3");
+  dune_static_assert(Grid::dimensionworld == 3, "Dimension of world must be 3");
+public:
+  static Dune::shared_ptr<Grid> create() {
+    Dune::GridFactory<Grid> gf;
+
+    // insert vertices
+    Dune::FieldVector<typename Grid::ctype, 3> pos;
+    pos[0] = 0; pos[1] = 0; pos[2] = 0; gf.insertVertex(pos);
+    pos[0] = 1; pos[1] = 0; pos[2] = 0; gf.insertVertex(pos);
+    pos[0] = 0; pos[1] = 1; pos[2] = 0; gf.insertVertex(pos);
+    pos[0] = 0; pos[1] = 0; pos[2] = 1; gf.insertVertex(pos);
+
+
+    // insert elements
+    Dune::GeometryType type; type.makeTetrahedron();
+    std::vector<unsigned int> vid(4);
+    vid[0] = 0; vid[1] = 1; vid[2] = 2; vid[3] = 3; gf.insertElement(type, vid);
+
+    return Dune::shared_ptr<Grid>(gf.createGrid());
+  }
+};
+
+// kuhn triangulation with 6 tets
+template<typename Grid>
+class KuhnTriangulatedUnitCubeMaker {
+  dune_static_assert(Grid::dimension == 3, "Dimension of grid must be 3");
+  dune_static_assert(Grid::dimensionworld == 3, "Dimension of world must be 3");
+public:
+  static Dune::shared_ptr<Grid> create() {
+    Dune::GridFactory<Grid> gf;
+    Dune::FieldVector<typename Grid::ctype, 3> pos;
+
+    pos[0] = 0; pos[1] = 0; pos[2] = 0; gf.insertVertex(pos);
+    pos[0] = 1; pos[1] = 0; pos[2] = 0; gf.insertVertex(pos);
+    pos[0] = 0; pos[1] = 1; pos[2] = 0; gf.insertVertex(pos);
+    pos[0] = 1; pos[1] = 1; pos[2] = 0; gf.insertVertex(pos);
+    pos[0] = 0; pos[1] = 0; pos[2] = 1; gf.insertVertex(pos);
+    pos[0] = 1; pos[1] = 0; pos[2] = 1; gf.insertVertex(pos);
+    pos[0] = 0; pos[1] = 1; pos[2] = 1; gf.insertVertex(pos);
+    pos[0] = 1; pos[1] = 1; pos[2] = 1; gf.insertVertex(pos);
+
+    Dune::GeometryType type;
+    type.makeTetrahedron();
+    std::vector<unsigned int> vid(4);
+
+    vid[0] = 0; vid[1] = 1; vid[2] = 3; vid[3] = 7; gf.insertElement(type, vid);
+    vid[0] = 0; vid[1] = 2; vid[2] = 3; vid[3] = 7; gf.insertElement(type, vid);
+    vid[0] = 0; vid[1] = 2; vid[2] = 6; vid[3] = 7; gf.insertElement(type, vid);
+    vid[0] = 0; vid[1] = 4; vid[2] = 6; vid[3] = 7; gf.insertElement(type, vid);
+    vid[0] = 0; vid[1] = 4; vid[2] = 5; vid[3] = 7; gf.insertElement(type, vid);
+    vid[0] = 0; vid[1] = 1; vid[2] = 5; vid[3] = 7; gf.insertElement(type, vid);
+
+    return Dune::shared_ptr<Grid>(gf.createGrid());
+  }
+};
+
 // minimal triangulation with 5 tets, contains unit tet
 template<typename Grid>
-class TriangulatedUnitCubeMaker {
+class MinTriangulatedUnitCubeMaker {
   dune_static_assert(Grid::dimension == 3, "Dimension of grid must be 3");
   dune_static_assert(Grid::dimensionworld == 3, "Dimension of world must be 3");
 public:
@@ -72,14 +132,37 @@ int main(int argc, char** argv)
 #error ALBERTA_DIM is not set to 3 -- please check the Makefile.am
 #endif
     {
-      std::cout << "The following stuff is expected to fail currently, because the recursive" << std::endl
-                << "bisection algorithm of alberta will run into an endless recursion for" << std::endl
-                << "certain grids.  See Flyspry#569." << std::endl;
       typedef Dune::AlbertaGrid<3, 3> Grid;
-      Dune::shared_ptr<Grid> grid = TriangulatedUnitCubeMaker<Grid>::create();
-      grid->globalRefine(2);
 
-      result = 1;
+      std::cout << "The recursive-bisection refinement algorithm of alberta "
+                << "cannot be used on arbitrary meshes (see Flyspry#569 for a "
+                << "more comprehensive explanation).  However, the heuristic "
+                << "used in the GridFactory can be tuned to support certain "
+                << "types of meshes.  This test makes sure that meshes that "
+                << "worked at one time in the past continue to work in the "
+                << "future.  If a certain mesh does not work, this program "
+                << "will usually generate a segmentation fault.\n"
+                << std::endl;
+
+      std::cout << "Checking unit tetrahedron..." << std::endl;
+      std::cout << "Note: The unit tetrahdron check is a safety measure.  If "
+                << "this test already produces a segfault, the problem is "
+                << "probably not Albertas recursive bisection algorithm but "
+                << "something else." << std::endl;
+      UnitTetMaker<Grid>::create()->globalRefine(2);
+      std::cout << "Checking unit tetrahedron: success\n" << std::endl;
+
+      std::cout << "Checking 6-triangulation of unit cube..." << std::endl;
+      KuhnTriangulatedUnitCubeMaker<Grid>::create()->globalRefine(2);
+      std::cout << "Checking 6-triangulation of unit cube: success\n"
+                << std::endl;
+
+      std::cout << "Checking 5-triangulation of unit cube..." << std::endl;
+      MinTriangulatedUnitCubeMaker<Grid>::create()->globalRefine(2);
+      std::cout << "Checking 5-triangulation of unit cube: success\n"
+                << std::endl;
+
+      result = 0;
     }
 #endif
 
