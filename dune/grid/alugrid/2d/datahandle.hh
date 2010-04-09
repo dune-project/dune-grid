5744091b1b94ade8db0ef42dfbb3c887bfadbb73
Martin Nolte <mnolte@dune-project.org>
2010-04-09 12:13:42 +0000

    bugfix
    [[Imported from SVN: r6597]]

--- dune/grid/alugrid/2d/datahandle.hh
+++ dune/grid/alugrid/2d/datahandle.hh
@@ -26,7 +26,7 @@ namespace ALU2DSPACENAME {
   /////////////////////////////////////////////////////////////////
   template< class GridType, class AdaptDataHandle >
   class AdaptRestrictProlong2dImpl
-    : public AdaptRestrictProlong2d ALU2DDIMWORLD( GridType::dimensionworld,3 )
+    : public AdaptRestrictProlong2d ALU2DDIMWORLD( GridType::dimensionworld, GridType::elementType )
   {
     GridType & grid_;
     typedef Dune :: MakeableInterfaceObject<typename GridType::template Codim<0>::Entity> EntityType;
