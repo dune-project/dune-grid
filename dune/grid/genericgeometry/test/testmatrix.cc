6d297f569047748643ceb4af4ad547570a19b64e
Martin Nolte <mnolte@dune-project.org>
2010-04-22 07:09:04 +0000

    also print the inverse (just so we see)
    [[Imported from SVN: r6635]]

--- dune/grid/genericgeometry/test/testmatrix.cc
+++ dune/grid/genericgeometry/test/testmatrix.cc
@@ -3,6 +3,7 @@
 #include <config.h>
 
 #include <iostream>
+#include <iomanip>
 
 #include <dune/common/gmpfield.hh>
 
@@ -17,6 +18,8 @@ int main()
   //typedef long double Field;
   //typedef GMPField< 72 > Field;
 
+  typedef GenericGeometry::MatrixHelper< GenericGeometry::DuneCoordTraits< Field > > MatrixHelper;
+
   // Test whether I can compute the determinant of A A^T of a nearly singular matrix.
   // This particular matrix makes the detAAT method abort as of dune-grid revision 6631.
   FieldMatrix< Field, 2, 2 > A;
@@ -25,6 +28,13 @@ int main()
   A[1][0] =  0.099999999999999867;
   A[1][1] = -0.0099999999999998979;
 
-  Field detAAT = GenericGeometry::MatrixHelper<GenericGeometry::DuneCoordTraits< Field > >::detAAT<2, 2>( A );
+  std::cout << std::scientific << std::setprecision( 20 );
+
+  Field detAAT = MatrixHelper::detAAT< 2, 2 >( A );
   std::cout << "detAAT = " << detAAT << std::endl;
+
+  FieldMatrix< Field, 2, 2 > invA;
+  Field det = MatrixHelper::rightInvA< 2, 2 >( A, invA );
+  std::cout << "det = " << det << std::endl;
+  std::cout << "invA = [ " << invA[ 0 ] << ", " << invA[ 1 ] << " ]" << std::endl;
 }
