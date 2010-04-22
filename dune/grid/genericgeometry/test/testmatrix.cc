a4c808fffad14f591c8a8c2e1ac341b6dda93d22
Martin Nolte <mnolte@dune-project.org>
2010-04-22 06:37:23 +0000

    allow use of GMPField (just to see what happens) (compile with -DNDEBUG to disable the assertion, i.e., to see that the result is nan)
    [[Imported from SVN: r6634]]

--- dune/grid/genericgeometry/test/testmatrix.cc
+++ dune/grid/genericgeometry/test/testmatrix.cc
@@ -1,6 +1,10 @@
 // -*- tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 2 -*-
 // vi: set et ts=4 sw=2 sts=2:
+#include <config.h>
+
 #include <iostream>
+
+#include <dune/common/gmpfield.hh>
 
 #include "../matrix.hh"
 #include "../geometrytraits.hh"
@@ -9,14 +13,18 @@ using namespace Dune;
 
 int main()
 {
+  typedef double Field;
+  //typedef long double Field;
+  //typedef GMPField< 72 > Field;
+
   // Test whether I can compute the determinant of A A^T of a nearly singular matrix.
   // This particular matrix makes the detAAT method abort as of dune-grid revision 6631.
-  FieldMatrix<double,2,2> A;
+  FieldMatrix< Field, 2, 2 > A;
   A[0][0] =  0.099999999999999867;
   A[0][1] = -0.010000000000002118;
   A[1][0] =  0.099999999999999867;
   A[1][1] = -0.0099999999999998979;
 
-  double detAAT = GenericGeometry::MatrixHelper<GenericGeometry::DuneCoordTraits<double> >::detAAT<2, 2>(A);
+  Field detAAT = GenericGeometry::MatrixHelper<GenericGeometry::DuneCoordTraits< Field > >::detAAT<2, 2>( A );
   std::cout << "detAAT = " << detAAT << std::endl;
 }
