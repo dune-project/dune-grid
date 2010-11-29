20958dab2ef8375db70d468eb4f92e1d7b330568
Andreas Dedner <dedner@dune-project.org>
2010-11-29 23:11:28 +0000

    derive from container of host grid
    [[Imported from SVN: r7187]]

--- dune/grid/geometrygrid/persistentcontainer.hh
+++ dune/grid/geometrygrid/persistentcontainer.hh
@@ -13,21 +13,18 @@ namespace Dune
 
   template< class HostGrid, class CoordFunction, class CoordAllocator, class Data, class Allocator >
   class PersistentContainer< GeometryGrid< HostGrid, CoordFunction, CoordAllocator >, Data, Allocator >
+    : public PersistentContainer< HostGrid, Data, Allocator >
   {
-    typedef PersistentContainer< HostGrid, Data, Allocator > HostContainer;
+    typedef PersistentContainer< HostGrid, Data, Allocator > Base;
 
   public:
     typedef GeometryGrid< HostGrid, CoordFunction, CoordAllocator > GridType;
 
-    typedef typename HostContainer::ConstIterator ConstIterator;
-    typedef typename HostContainer::Iterator Iterator;
-
     typedef typename GridType::template Codim< 0 >::Entity ElementType;
 
-    //! Constructor filling the container with values using the default constructor
-    //! Depending on the implementation this could be achieved without allocating memory
     PersistentContainer ( const GridType &grid, const int codim, const Allocator &allocator = Allocator() )
-      : hostContainer_( grid.hostGrid(), codim, allocator ), grid_(grid)
+      : Base( grid.hostGrid(), codim, allocator ),
+        grid_(grid)
     {}
 
     template< class Entity >
@@ -48,57 +45,40 @@ namespace Dune
 
     const Data &operator() ( const ElementType &element, const int subEntity ) const
     {
-      return hostContainer_( GridType::template getHostEntity<0>( element ), subEntity );
+      return Base::operator()( GridType::template getHostEntity<0>( element ), subEntity );
     }
 
     Data &operator() ( const ElementType &element, const int subEntity )
     {
-      return hostContainer_( GridType::template getHostEntity<0>( element ), subEntity );
-    }
-
-    ConstIterator begin () const { return hostContainer_.begin(); }
-    Iterator begin () { return hostContainer_.begin(); }
-
-    ConstIterator end () const { return hostContainer_.end(); }
-    Iterator end () { return hostContainer_.end(); }
-
-    void resize ( )
-    {
-      hostContainer_.resize( );
-    }
-
-    void compress ( )
-    {
-      hostContainer_.compress( );
+      return Base::operator()( GridType::template getHostEntity<0>( element ), subEntity );
     }
 
   protected:
     template< class EntityImpl >
     const Data &data ( const EntityImpl &entity, integral_constant<bool, false > ) const
     {
-      return hostContainer_[ GridType::template getHostEntity<EntityImpl::codimension>( entity) ];
+      return Base::operator[]( GridType::template getHostEntity<EntityImpl::codimension>( entity) );
     }
 
     template< class EntityImpl >
     Data &data ( const EntityImpl &entity, integral_constant<bool, false > )
     {
-      return hostContainer_[ GridType::template getHostEntity<EntityImpl::codimension>( entity) ];
+      return Base::operator[]( GridType::template getHostEntity<EntityImpl::codimension>( entity) );
     }
 
     template< class EntityImpl >
     const Data &data ( const EntityImpl &entity, integral_constant<bool, true > ) const
     {
-      return hostContainer_( entity.hostElement(). entity.subEntity() );
+      return Base::operator()( entity.hostElement(). entity.subEntity() );
     }
 
     template< class EntityImpl >
     Data &data ( const EntityImpl &entity, integral_constant<bool, true > )
     {
-      return hostContainer_( entity.hostElement(). entity.subEntity() );
+      return Base::operator()( entity.hostElement(). entity.subEntity() );
     }
 
   private:
-    HostContainer hostContainer_;
     const GridType &grid_;
   };
 } // end namespace Dune
