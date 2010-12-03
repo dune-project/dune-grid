550ec281d422b2f0198104a37821fef22b91a04a
Carsten Gräser <graeser@dune-project.org>
2010-12-03 23:50:47 +0000

    Simplify implementation
    CodimTable is just a multi-type container that now
    wraps a tuple<Element<0>, ...,Element<dim> > and
    the associated get<>() functions.
    
    Since this is an internal class it could even be removed
    in favor of directly using the tuple.
    
    [[Imported from SVN: r7214]]

--- dune/grid/genericgeometry/codimtable.hh
+++ dune/grid/genericgeometry/codimtable.hh
@@ -5,6 +5,7 @@
 #define DUNE_GENERICGEOMETRY_CODIMTABLE_HH
 
 #include <dune/common/typetraits.hh>
+#include <dune/common/tupleutility.hh>
 
 namespace Dune
 {
@@ -12,126 +13,40 @@ namespace Dune
   namespace GenericGeometry
   {
 
-    template< template< int > class Element, int codim >
-    class CodimTableStorage
-    {
-      CodimTableStorage< Element, codim - 1 > map_;
-      Element< codim > element_;
-
-    public:
-      CodimTableStorage ()
-        : map_(),
-          element_()
-      {}
-
-      CodimTableStorage ( const CodimTableStorage &other )
-        : map_( other.map_ ),
-          element_( other.element_ )
-      {}
-
-      const CodimTableStorage &operator= ( const CodimTableStorage &other )
-      {
-        map_ = other.map_;
-        element_ = other.element_;
-        return *this;
-      }
-
-      template< int cd >
-      const Element< cd > &
-      operator[] ( const integral_constant< int, cd > codimVariable ) const
-      {
-        return map_[ codimVariable ];
-      }
-
-      template< int cd >
-      Element< cd > &
-      operator[] ( const integral_constant< int, cd > &codimVariable )
-      {
-        return map_[ codimVariable ];
-      }
-
-      const Element< codim > &
-      operator[] ( const integral_constant< int, codim > &codimVariable ) const
-      {
-        return element_;
-      }
-
-      Element< codim > &
-      operator[] ( const integral_constant< int, codim > &codimVariable )
-      {
-        return element_;
-      }
-    };
-
-
-
-    template< template< int > class Element >
-    class CodimTableStorage< Element, 0 >
-    {
-      Element< 0 > element_;
-
-    public:
-      CodimTableStorage ()
-        : element_()
-      {}
-
-      CodimTableStorage ( const CodimTableStorage &other )
-        : element_( other.element_ )
-      {}
-
-      CodimTableStorage &operator= ( const CodimTableStorage &other )
-      {
-        element_ = other.element_;
-        return *this;
-      }
-
-      const Element< 0 > &
-      operator[] ( const integral_constant< int, 0 > codimVaraible ) const
-      {
-        return element_;
-      }
-
-      Element< 0 > &
-      operator[] ( const integral_constant< int, 0 > codimVaraible )
-      {
-        return element_;
-      }
-    };
-
-
-
     template< template< int > class Element, int dim >
     class CodimTable
     {
-      CodimTableStorage< Element, dim > map_;
+      friend class CodimTable< Element, dim+1 >;
+
+      typedef typename PushBackTuple<
+          typename CodimTable< Element, dim-1 >::ElementTuple,
+          Element< dim > >::type ElementTuple;
+
+      ElementTuple map_;
 
     public:
-      CodimTable ()
-      {}
-
-      CodimTable ( const CodimTable &other )
-        : map_( other.map_ )
-      {}
-
-      const CodimTable &operator= ( const CodimTable &other )
-      {
-        map_ = other.map_;
-        return *this;
-      }
 
       template< int codim >
       const Element< codim > &
       operator[] ( const integral_constant< int, codim > codimVariable ) const
       {
-        return map_[ codimVariable ];
+        return Dune::get<codim>(map_);
       }
 
       template< int codim >
       Element< codim > &
       operator[] ( const integral_constant< int, codim > codimVariable )
       {
-        return map_[ codimVariable ];
+        return Dune::get<codim>(map_);
       }
+    };
+
+
+    template< template< int > class Element>
+    class CodimTable< Element, -1 >
+    {
+      friend class CodimTable< Element, 0 >;
+      typedef typename Dune::tuple<> ElementTuple;
     };
 
   }
