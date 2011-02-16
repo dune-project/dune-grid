c9e0a32c4cd543d36204e4d5c7deb1cec7f8e874
Oliver Sander <sander@dune-project.org>
2011-02-16 20:28:33 +0000

    Merge the 2d specialization into the new generic one
    [[Imported from SVN: r7428]]

--- dune/grid/io/file/gmshreader.hh
+++ dune/grid/io/file/gmshreader.hh
@@ -516,21 +516,69 @@ namespace Dune
                &(elementDofs[6]),&(elementDofs[7]),&(elementDofs[8]),
                &(elementDofs[9]));
 
-      std::vector<unsigned int> vertices(nVertices[elm_type]);
-
-      for (int i=0; i<nVertices[elm_type]; i++)
-        vertices[i] = renumber[elementDofs[i]];         // renumber vertices
-
-      // Actually insert the element
+      // correct differences between gmsh and Dune in the local vertex numbering
       switch (elm_type)
       {
-      case 1 :          // 2-node line
-        factory.insertElement(Dune::GeometryType(Dune::GeometryType::simplex,dim),vertices);
+      case 3 :          // 4-node quadrilateral
+        std::swap(elementDofs[2],elementDofs[3]);
         break;
       }
 
+      // renumber corners to account for the explicitly given vertex
+      // numbering in the file
+      std::vector<unsigned int> vertices(nVertices[elm_type]);
+
+      for (int i=0; i<nVertices[elm_type]; i++)
+        vertices[i] = renumber[elementDofs[i]];
+
+      // If it is an element, insert it as such
+      if (elementDim[elm_type] == dim) {
+
+        switch (elm_type)
+        {
+        case 1 :            // 2-node line
+          factory.insertElement(Dune::GeometryType(Dune::GeometryType::simplex,dim),vertices);
+          break;
+        case 2 :            // 3-node triangle
+          factory.insertElement(Dune::GeometryType(Dune::GeometryType::simplex,dim),vertices);
+          break;
+        case 3 :            // 4-node quadrilateral
+          factory.insertElement(Dune::GeometryType(Dune::GeometryType::cube,dim),vertices);
+          break;
+        case 9 :            // 6-node triangle
+          factory.insertElement(Dune::GeometryType(Dune::GeometryType::simplex,dim),vertices);
+          break;
+        }
+
+      } else {
+        // it must be a boundary segment then
+        if (insert_boundary_segments) {
+
+          switch (elm_type)
+          {
+          case 1 :              // 2-node line
+            factory.insertBoundarySegment(vertices);
+            break;
+          case 8 :              // 3-node line
+            typedef GmshReaderQuadraticBoundarySegment< 2, dimWorld > QuadraticBoundarySegment;
+            array<FieldVector<double,dim>, 3> v;
+            for (int i=0; i<dim; i++) {
+              v[0][i] = nodes[elementDofs[0]][i];
+              v[1][i] = nodes[elementDofs[2]][i];                    // yes, the renumbering is intended!
+              v[2][i] = nodes[elementDofs[1]][i];
+            }
+            BoundarySegment<dim,dimWorld>* newBoundarySegment
+              = (BoundarySegment<dim,dimWorld>*) new GmshReaderQuadraticBoundarySegment< 2, dimWorld >(v[0], v[1], v[2]);
+            factory.insertBoundarySegment(vertices,
+                                          shared_ptr<BoundarySegment<dim,dimWorld> >(newBoundarySegment));
+            break;
+          }
+
+        }
+      }
+
       // count elements and boundary elements
-      if (elementDim[elm_type] == 1) {
+      if (elementDim[elm_type] == dim) {
         element_index_to_physical_entity[element_count] = physical_entity;
         element_count++;
       } else {
@@ -562,102 +610,6 @@ namespace Dune
   class GmshReaderParser<GridType, 2> : public GmshReaderParserBase< GridType >
   {
     typedef GmshReaderParserBase< GridType > Base;
-    typedef typename Base::GlobalVector GlobalVector;
-    using Base::dim;
-    using Base::dimWorld;
-    using Base::buf;
-    using Base::factory;
-    using Base::verbose;
-    using Base::element_index_to_physical_entity;
-    using Base::element_count;
-    using Base::number_of_real_vertices;
-    using Base::boundary_element_count;
-    using Base::insert_boundary_segments;
-    using Base::boundary_id_to_physical_entity;
-    using Base::readfile;
-    using Base::skipline;
-
-    typedef GmshReaderQuadraticBoundarySegment< dim, dimWorld > QuadraticBoundarySegment;
-
-    void pass2HandleElement(FILE* file, const int elm_type,
-                            std::map<int,unsigned int> & renumber,
-                            const std::vector< GlobalVector > & nodes,
-                            const int physical_entity)
-    {
-      if (elm_type != 1              // 2-node line
-          and elm_type != 2          // 3-node triangle
-          and elm_type != 3          // 4-node quadrilateral
-          and elm_type != 8          // 3-node line
-          and elm_type != 9) {       // 6-node triangle
-        skipline(file);         // skip rest of line if element is unknown
-        return;
-      }
-
-      // some data about gmsh elements
-      const int nDofs[12]            = {-1, 2, 3, 4, 4, 8, 6, 5, 3, 6, -1, 10};
-      const int nVertices[12]        = {-1, 2, 3, 4, 4, 8, 6, 5, 2, 3, -1, 4};
-      const int elementDim[12] = {-1, 1, 2, 2, 3, 3, 3, 3, 1, 2, -1, 3};
-
-      // The format string for parsing is n times '%d' in a row
-      std::string formatString = "%d";
-      for (int i=1; i<nDofs[elm_type]; i++)
-        formatString += " %d";
-      formatString += "\n";
-
-      // '10' is the largest number of dofs we may encounter in a .msh file
-      std::vector<int> elementDofs(10);
-
-      readfile(file,nDofs[elm_type], formatString.c_str(),
-               &(elementDofs[0]),&(elementDofs[1]),&(elementDofs[2]),
-               &(elementDofs[3]),&(elementDofs[4]),&(elementDofs[5]),
-               &(elementDofs[6]),&(elementDofs[7]),&(elementDofs[8]),
-               &(elementDofs[9]));
-
-      // correct differences between gmsh and Dune in the local vertex numbering
-      switch (elm_type)
-      {
-      case 3 :          // 4-node quadrilateral
-        std::swap(elementDofs[2],elementDofs[3]);
-        break;
-      }
-
-      std::vector<unsigned int> vertices(nVertices[elm_type]);
-
-      for (int i=0; i<nVertices[elm_type]; i++)
-        vertices[i] = renumber[elementDofs[i]];         // renumber vertices
-
-      // Actually insert the element
-      switch (elm_type)
-      {
-      case 1 :          // 2-node line
-        if (insert_boundary_segments)
-          factory.insertBoundarySegment(vertices);
-        break;
-      case 2 :          // 3-node triangle
-        factory.insertElement(Dune::GeometryType(Dune::GeometryType::simplex,dim),vertices);
-        break;
-      case 3 :          // 4-node quadrilateral
-        factory.insertElement(Dune::GeometryType(Dune::GeometryType::cube,dim),vertices);
-        break;
-      case 8 :          // 3-node line
-        if (insert_boundary_segments)
-          factory.insertBoundarySegment(vertices, shared_ptr<BoundarySegment<dim,dimWorld> >(new QuadraticBoundarySegment(nodes[elementDofs[0]],nodes[elementDofs[2]],nodes[elementDofs[1]])));
-        break;
-      case 9 :          // 6-node triangle
-        factory.insertElement(Dune::GeometryType(Dune::GeometryType::simplex,dim),vertices);
-        break;
-      }
-
-      // count elements and boundary elements
-      if (elementDim[elm_type] == 2) {
-        element_index_to_physical_entity[element_count] = physical_entity;
-        element_count++;
-      } else {
-        boundary_id_to_physical_entity[boundary_element_count] = physical_entity;
-        boundary_element_count++;
-      }
-
-    }
   public:
     GmshReaderParser(Dune::GridFactory<GridType>& _factory, bool v, bool i) :
       Base(_factory,v,i) {}
