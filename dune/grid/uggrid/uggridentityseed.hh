d80a52711b361d5f584e8a988a5cfec4ee75de3b
Oliver Sander <sander@dune-project.org>
2011-09-24 12:53:25 +0000

    properly implement EntitySeed class for UGGrid.  Memory needed is a single pointer
    [[Imported from SVN: r7681]]

---
+++ dune/grid/uggrid/uggridentityseed.hh
@@ -0,0 +1,39 @@
+// -*- tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 2 -*-
+// vi: set et ts=4 sw=2 sts=2:
+#ifndef DUNE_UGGRID_ENTITY_SEED_HH
+#define DUNE_UGGRID_ENTITY_SEED_HH
+
+/** \file
+ *  \brief Implementation of EntitySeed for the UGGrid grid manager
+ */
+
+namespace Dune {
+
+  /** \brief Store a reference to an entity with a minimal memory footprint
+   */
+  template<int codim, class GridImp>
+  class UGGridEntitySeed
+  {
+    // grid dimension
+    enum { dim = GridImp::dimension };
+  public:
+
+    //! construct entity seed from entity
+    UGGridEntitySeed (const UGGridEntity<codim,dim,GridImp>& entity)
+      : target_(entity.target_)
+    {}
+
+    /** \brief Access to the underlying UG data structure */
+    typename UG_NS<dim>::template Entity<codim>::T* target() const
+    {
+      return target_;
+    }
+
+  private:
+    /** \brief Plain old pointer to the corresponding UG data structure */
+    typename UG_NS<dim>::template Entity<codim>::T* target_;
+  };
+
+} // end namespace Dune
+
+#endif
