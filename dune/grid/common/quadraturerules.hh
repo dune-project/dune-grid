7a8f3c6cb65ef0dd171fa1e83f43756e820d595d
Christoph Grüninger <gruenich@dune-project.org>
2011-09-30 16:19:44 +0000

    constitutional work for dune-geometry
    [[Imported from SVN: r7689]]

--- dune/grid/common/quadraturerules.hh
+++ dune/grid/common/quadraturerules.hh
@@ -12,7 +12,7 @@
 #include <dune/common/fvector.hh>
 #include <dune/common/exceptions.hh>
 #include <dune/common/stdstreams.hh>
-#include <dune/common/geometrytype.hh>
+#include <dune/geometry/type.hh>
 
 /**
    \file
