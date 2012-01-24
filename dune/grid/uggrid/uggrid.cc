c7f3580866bc08d92aa471c1e9177bffd470b514
Oliver Sander <sander@dune-project.org>
2012-01-24 11:15:46 +0000

    revert Jö's patch to avoid a set-but-unused warning -- Part II of II
    [[Imported from SVN: r7850]]

--- dune/grid/uggrid/uggrid.cc
+++ dune/grid/uggrid/uggrid.cc
@@ -21,8 +21,6 @@ using namespace Dune;
 //
 //***********************************************************************
 
-const UG_NS_Base::IgnoreType UG_NS_Base::ignore = UG_NS_Base::IgnoreType();
-
 template<> int Dune::UGGrid<2>::numOfUGGrids = 0;
 template<> int Dune::UGGrid<3>::numOfUGGrids = 0;
 
