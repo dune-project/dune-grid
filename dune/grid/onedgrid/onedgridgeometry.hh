f0ca1915fda100b3ee4e52a20f28bd7fda5526b0
Oliver Sander <sander@dune-project.org>
2012-04-10 13:12:02 +0000

    Switch the Geometry class to storing copies instead of references.
    This seems to work (i.e., pass the test) without any additional
    changes to the OneDGrid code.
    
    [[Imported from SVN: r7988]]

--- dune/grid/onedgrid/onedgridgeometry.hh
+++ dune/grid/onedgrid/onedgridgeometry.hh
@@ -233,6 +233,21 @@ namespace Dune {
 
   };
 
+  namespace FacadeOptions
+  {
+
+    /** \brief Switch on the new implementation for the Geometry interface class
+     * \deprecated Eventually the new implementation will be hardwired,
+     *             and this switch may disappear without prior warning!
+     */
+    template< int mydim, int cdim, class GridImp>
+    struct StoreGeometryReference<mydim,cdim,GridImp,OneDGridGeometry>
+    {
+      static const bool v = false;
+    };
+
+  }
+
 }  // namespace Dune
 
 #endif
