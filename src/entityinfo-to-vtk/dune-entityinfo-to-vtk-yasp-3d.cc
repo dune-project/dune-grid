5afc46f3984091b4e19ba3b5f74dfffbb3508f3d
Jorrit Fahlke <joe@dune-project.org>
2012-05-11 17:41:14 +0000

    [entityinfo-to-vtk] 3D YaspGrid binary.
    [[Imported from SVN: r8086]]

---
+++ src/entityinfo-to-vtk/dune-entityinfo-to-vtk-yasp-3d.cc
@@ -0,0 +1,12 @@
+// -*- tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 2 -*-
+// vi: set et ts=4 sw=2 sts=2:
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
+
+#include <string>
+
+const std::string programName = "dune-entityinfo-to-vtk-yasp-3d";
+static const int dim = 3;
+
+#include "entityinfo-to-vtk-main-yasp.hh"
