29336cd97843fed10caff62c9c9549ebe1775c6e
Markus Blatt <mblatt@dune-project.org>
2012-08-30 18:29:52 +0000

    Search for METIS as ALUGrid uses. Set libraries correctly.
    [[Imported from SVN: r8377]]

--- cmake/modules/FindALUGrid.cmake
+++ cmake/modules/FindALUGrid.cmake
@@ -5,13 +5,40 @@ macro(_dune_set_alugrid val)
   set(HAVE_ALUGRID ${val})
 endmacro(_dune_set_alugrid val)
 
-message(AUTHOR_WARNING "Apparently ALUGrid needs METIS, which is non-free software. Therefore
-I have to stall ALUGRID support until I either get approval from the METIS licencer or ALUGrid works without METIS")
 
-_dune_set_alugrid(FALSE)
-function(add_dune_alugrid_flags)
+function(add_dune_alugrid_flags )
+ if(ALUGRID_FOUND)
+    cmake_parse_arguments(ADD_ALU "SOURCE_ONLY;OBJECT" "" "" ${ARGN})
+    if(ADD_ALU_SOURCE_ONLY)
+      set(_prefix SOURCE)
+      set(_source_only SOURCE_ONLY)
+      set_property(DIRECTORY APPEND PROPERTY INCLUDE_DIRECTORIES ${ALUGRID_INCLUDES})
+    else()
+      if(ADD_ALU_OBJECT)
+	set(_object OBJECT)
+      else(ADD_ALU_OBJECT)
+	foreach(_target ${ADD_ALU_UNPARSED_ARGUMENTS})
+	  target_link_libraries(${_target} dunegrid ${ALUGRID_LIB} ${METIS_LIBRARIES} dunegeometry dunecommon)
+	endforeach(_target ${ADD_ALU_UNPARSED_ARGUMENTS})
+      endif(ADD_ALU_OBJECT)
+      set(_prefix TARGET)
+      set_property(${_prefix} ${ADD_ALU_UNPARSED_ARGUMENTS} APPEND
+	PROPERTY INCLUDE_DIRECTORIES ${ALUGRID_INCLUDES})
+    endif()
+
+    set_property(${_prefix} ${ADD_ALU_UNPARSED_ARGUMENTS} APPEND PROPERTY COMPILE_DEFINITIONS ENABLE_ALUGRID)
+    if(NOT (ADD_ALU_SOURCE_ONLY OR ADD_ALU_OBJECT))
+      set_property(${_prefix} ${ADD_ALU_UNPARSED_ARGUMENTS} APPEND PROPERTY LINK_LIBRARIES  dunegrid ${ALUGRID_LIB}  ${METIS_LIBRARIES} dunegeometry dunecommon)
+    endif(NOT (ADD_ALU_SOURCE_ONLY OR ADD_ALU_OBJECT))
+    if(HAVE_ALUGRID_PARALLEL_H)
+      message(STATUS "Adding MPI flags for ${ADD_ALU_UNPARSED_ARGUMENTS}")
+      add_dune_mpi_flags(${ADD_ALU_UNPARSED_ARGUMENTS} ${_source_only} ${_object})
+    endif(HAVE_ALUGRID_PARALLEL_H)
+  endif(ALUGRID_FOUND)
 endfunction(add_dune_alugrid_flags)
-return()
+
+
+find_package(METIS)
 
 if(ALUGRID_DIR)
 find_path(ALUGRID_PKGCONFIG_DIR alugrid.pc PATH ${ALUGRID_DIR}
@@ -20,20 +47,14 @@ endif(ALUGRID_DIR)
 
 find_path(ALUGRID_PKGCONFIG_DIR alugrid.pc PATH_SUFFIXES lib/pkgconfig/ alugrid/lib/pkgconfig)
 
-message("ALUGRID_PKGCONFIG_DIR=${ALUGRID_PKGCONFIG_DIR}")
-
 get_filename_component(_GUESSED_ALUGRID_DIR ${ALUGRID_PKGCONFIG_DIR}/../../ ABSOLUTE)
-message("find_path(ALUGRID_VERSION_PATH alugridversion PATH ${_GUESSED_ALUGRID_DIR} PATH_SUFFIXES bin
-  NO_DEFAULT_PATH)")
 find_file(ALUGRID_VERSION alugridversion PATH ${_GUESSED_ALUGRID_DIR}/bin
   NO_DEFAULT_PATH)
-message("ALUGRID_VERSION=${ALUGRID_VERSION} GUESSED_ALUGRID_DIR=${_GUESSED_ALUGRID_DIR}")
+
 if(ALUGRID_VERSION)
   set(ALUGRID_DIR ${_GUESSED_ALUGRID_DIR})
 else(ALUGRID_VERSION_PATH)
   get_filename_component(_GUESSED_ALUGRID_DIR ${ALUGRID_PKGCONFIG_DIR}/../../.. ABSOLUTE)
-  message("find_path(ALUGRID_VERSION alugridversion PATH ${_GUESSED_ALUGRID_DIR} PATH_SUFFIXES bin
-    NO_DEFAULT_PATH)")
   find_file(ALUGRID_VERSION alugridversion PATH ${_GUESSED_ALUGRID_DIR} PATH_SUFFIXES bin
     NO_DEFAULT_PATH)
   if(ALUGRID_VERSION)
@@ -49,7 +70,6 @@ if(NOT ALUGRID_VERSION)
   return()
 endif(NOT ALUGRID_VERSION)
 
-message("ALUGRID_VERSION=${ALUGRID_VERSION}")
 if(ALUGRID_VERSION)
   execute_process(COMMAND ${ALUGRID_VERSION} -c ${ALUGRID_VERSION_REQUIRED} OUTPUT_VARIABLE ALUGRID_VERSION)
   if(ALUGRID_VERSION LESS 0)
@@ -68,13 +88,11 @@ endif(ALUGRID_VERSION)
 
 find_path(ALUGRID_INCLUDE_PATH alugrid_serial.h PATH ${ALUGRID_DIR} PATH_SUFFIXES include include/serial NO_DEFAULT_PATH DOC "Include path of serial alugrid headers.")
 find_path(ALUGRID_INCLUDE_PATH alugrid_serial.h PATH_SUFFIXES include include/serial)
-message("ALUGRID_INCLUDE_PATH=${ALUGRID_INCLUDE_PATH}")
 
 find_library(ALUGRID_LIB alugrid PATH ${ALUGRID_DIR} PATH_SUFFIXES lib lib32 lib64
   DOC "ALUGrid library" NO_DEFAULT_PATH)
 find_library(ALUGRID_LIB alugrid PATH_SUFFIXES lib lib32 lib64)
 
-message("ALUGRID_LIB=${ALUGRID_LIB}")
 
 if(NOT ALUGRID_LIB)
   message(WARNING "ALUGrid library not found.")
@@ -92,7 +110,6 @@ if(ALUGRID_INCLUDE_PATH)
   message("CMAKE_REQUIRED_INCLUDES=${CMAKE_REQUIRED_INCLUDES}")
   set(CMAKE_REQUIRED_LIBRARIES ${CMAKE_REQUIRED_LIBRARIES} ${ALUGRID_LIB})
   set(CMAKE_REQUIRED_DEFINITIONS ${CMAKE_REQUIRED_DEFINITIONS} ${ALUGRID_DEFINITIONS})
-  message("ALUGRID_INCLUDES=${ALUGRID_INCLUDES}")
   check_include_file_cxx(stlheaders.h HAVE_ALUGRID_SERIAL_H)
   if(HAVE_ALUGRID_SERIAL_H)
     check_cxx_source_compiles("#include <alugrid_defineparallel.h>
@@ -109,18 +126,15 @@ else(ALUGRID_INCLUDE_PATH)
   message(WARN "alugrid_serial.h not found in ${ALUGRID_INCLUDE_PATH}.")
 endif(ALUGRID_INCLUDE_PATH)
 
-message("ALUGRID_PARALLEL_FOUND=${ALUGRID_PARALLEL_FOUND}")
 if(ALUGRID_PARALLEL_FOUND AND MPI_FOUND)
   # check for parallel ALUGrid
   find_path(ALUGRID_PARALLEL_INCLUDE_PATH alugrid_parallel.h PATH ${ALUGRID_DIR} PATH_SUFFIXES include include/parallel NO_DEFAULT_PATH)
   find_path(ALUGRID_PARALLEL_INCLUDE_PATH alugrid_parallel.h PATH_SUFFIXES include include/parallel)
-  message("ALUGRID_INCLUDE_PATH=${ALUGRID_PARALLEL_INCLUDE_PATH}")
 
   if(ALUGRID_PARALLEL_INCLUDE_PATH)
     cmake_push_check_state() # store rquired flags
     list(APPEND ALUGRID_INCLUDES ${ALUGRID_INCLUDE_PATH}/parallel)
     set(CMAKE_REQUIRED_INCLUDES ${CMAKE_REQUIRED_INCLUDES} ${ALUGRID_INCLUDES})
-    message("CMAKE_REQUIRED_INCLUDES=${CMAKE_REQUIRED_INCLUDES}")
     set(CMAKE_REQUIRED_LIBRARIES ${CMAKE_REQUIRED_LIBRARIES} ${ALUGRID_LIB})
     #set(CMAKE_REQUIRED_DEFINITIONS ${CMAKE_REQUIRED_DEFINITIONS} -DSOME_MORE_DEF)
     #check_include_file_cxx(alugrid_parallel.h ALUGRID_PARALLEL_HEADER)
@@ -138,12 +152,19 @@ the check produces linker errors.")
 endif(ALUGRID_PARALLEL_FOUND AND MPI_FOUND)
 
 check_library_exists(${ALUGRID_LIB} malloc "" _ALULIB_FUNCTIONAL)
-message("_ALULIB_FUNCTIONAL=${_ALULIB_FUNCTIONAL}")
 
 if(HAVE_ALUGRID_SERIAL_H)
   set(ALUGRID_FOUND TRUE)
   set(HAVE_ALUGRID TRUE)
 endif(HAVE_ALUGRID_SERIAL_H)
+
+if(NOT METIS_LIBRARIES AND ALUGRID_FOUND)
+  message(WARNING "Metis library not found but needed for linking with ALUGrid.")
+
+  set(ALUGRID_FOUND FALSE)
+  set(HAVE_ALUGRID FALSE)
+  return()
+endif(NOT METIS_LIBRARIES AND ALUGRID_FOUND)
 
 if(ALUGRID_FOUND)
   include(GridType)
@@ -158,37 +179,6 @@ if(ALUGRID_FOUND)
     HEADERS dune/grid/alugrid.hh dune/grid/io/file/dgfparser/dgfalu.hh)
 
 endif(ALUGRID_FOUND)
-
-function(add_dune_alugrid_flags )
- if(ALUGRID_FOUND)
-    cmake_parse_arguments(ADD_ALU "SOURCE_ONLY;OBJECT" "" "" ${ARGN})
-    if(ADD_ALU_SOURCE_ONLY)
-      set(_prefix SOURCE)
-      set(_source_only SOURCE_ONLY)
-      set_property(DIRECTORY APPEND PROPERTY INCLUDE_DIRECTORIES ${ALUGRID_INCLUDES})
-    else()
-      if(ADD_ALU_OBJECT)
-	set(_object OBJECT)
-      else(ADD_ALU_OBJECT)
-	foreach(_target ${ADD_ALU_UNPARSED_ARGUMENTS})
-	  target_link_libraries(${_target} ${ALUGRID_LIB} ${DUNE_LIBS})
-	endforeach(_target ${ADD_ALU_UNPARSED_ARGUMENTS})
-      endif(ADD_ALU_OBJECT)
-      set(_prefix TARGET)
-      set_property(${_prefix} ${ADD_ALU_UNPARSED_ARGUMENTS} APPEND
-	PROPERTY INCLUDE_DIRECTORIES ${ALUGRID_INCLUDES})
-    endif()
-
-    set_property(${_prefix} ${ADD_ALU_UNPARSED_ARGUMENTS} APPEND PROPERTY COMPILE_DEFINITIONS ENABLE_ALUGRID)
-    if(NOT (ADD_ALU_SOURCE_ONLY OR ADD_ALU_OBJECT))
-      set_property(${_prefix} ${ADD_ALU_UNPARSED_ARGUMENTS} APPEND PROPERTY LINK_LIBRARIES ${ALUGRID_LIB} dunegrid ${DUNE_LIBS})
-    endif(NOT (ADD_ALU_SOURCE_ONLY OR ADD_ALU_OBJECT))
-    if(HAVE_ALUGRID_PARALLEL_H)
-      message(STATUS "Adding MPI flags for ${ADD_ALU_UNPARSED_ARGUMENTS}")
-      add_dune_mpi_flags(${ADD_ALU_UNPARSED_ARGUMENTS} ${_source_only} ${_object})
-    endif(HAVE_ALUGRID_PARALLEL_H)
-  endif(ALUGRID_FOUND)
-endfunction(add_dune_alugrid_flags)
 
 
 
