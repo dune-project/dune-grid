23c1f66a201316e29c152b54b4f2b08b9feb1f4a
Christoph Grüninger <gruenich@dune-project.org>
2012-09-04 12:29:53 +0000

    [CMake] Improved FindAlberta.cmake, but still not working for me. Replaced tabs by spaces.
    [[Imported from SVN: r8390]]

--- cmake/modules/FindAlberta.cmake
+++ cmake/modules/FindAlberta.cmake
@@ -7,8 +7,9 @@ set(ALBERTA_LIBCHECK ON CACHE BOOL "Whether to try to link against libalberta_Nd
 set(ALBERTA_DIR "" CACHE FILEPATH "Root directory of Alberta installation.")
 set(ALBERTA_EXTRA_LIBS "" CACHE FILEPATH "Extra libraries needed by alberta for linking.")
 
+# look for header alberta/alberta.h
 find_path(ALBERTA_INCLUDE_DIR name alberta/alberta.h PATHS ${ALBERTA_DIR}
-  PATH_SUFFIXES alberta NO_DEFAULT_PATH DOC "Include path of Alberta")
+  PATH_SUFFIXES alberta include NO_DEFAULT_PATH DOC "Include path of Alberta")
 find_path(ALBERTA_INCLUDE_DIR name alberta/alberta.h PATHS /usr/local /opt
   PATH_SUFFIXES alberta)
 
@@ -19,6 +20,8 @@ cmake_push_check_state()
 set(CMAKE_REQUIRED_DEFINITIONS ${CMAKE_REQUIRED_DEFINITIONS} -DDIM_OF_WORLD=3 -DDEL_INDEX=0)
 set(CMAKE_REQUIRED_INCLUDES ${CMAKE_REQUIRED_INCLUDES} ${ALBERTA_INCLUDE_DIR})
 check_include_files(alberta/alberta.h ALBERTA_FOUND)
+
+# check version, Alberta 2 or 3
 if(ALBERTA_FOUND)
   include(CheckStructHasMember)
   check_struct_has_member ("struct el_info" wall_bound alberta/alberta.h ALBERTA_IS_VERSION_3)
@@ -31,9 +34,13 @@ else(ALBERTA_FOUND)
   _dune_set_alberta(False)
 endif(ALBERTA_FOUND)
 
-find_library(ALBERTA_UTIL_LIB alberta_util PATH ${ALBERTA_DIR} PATH_SUFFIXES lib lib32 lib64
+# look for libraries
+find_library(ALBERTA_UTIL_LIB alberta_util
+  PATH ${ALBERTA_DIR}
+  PATH_SUFFIXES lib lib32 lib64
   NO_DEFAULT_PATH)
-find_library(ALBERTA_UTIL_LIB alberta_util PATH PATH_SUFFIXES lib lib32 lib64)
+find_library(ALBERTA_UTIL_LIB alberta_util
+  PATH PATH_SUFFIXES lib lib32 lib64)
 if(ALBERTA_UTIL_LIB)
   set(_CMAKE_REQUIRED_LIBRARIES_SAVE ${CMAKE_REQUIRED_LIBRARIES})
   set(CMAKE_REQUIRED_LIBRARIES ${CMAKE_REQUIRED_LIBRARIES} ${ALBERTA_UTIL_LIB})
@@ -58,13 +65,11 @@ if(ALBERTA_LIBCHECK)
       set(CMAKE_REQUIRED_LIBRARIES ${_CMAKE_REQUIRED_LIBRARIES_OLD} ${ALBERTA_${dim}D_LIB} ${ALBERTA_UTIL_LIB} ${DUNE_LIBS})
       check_library_exists(${ALBERTA_${dim}D_LIB} mesh_traverse "" ALBERTA_${dim}D_LIB_FOUND)
       if(ALBERTA_${dim}D_LIB_FOUND)
-	list(APPEND ALBERTA_WORLD_DIMS ${dim})
+        list(APPEND ALBERTA_WORLD_DIMS ${dim})
       endif(ALBERTA_${dim}D_LIB_FOUND)
     endif(ALBERTA_${dim}D_LIB)
   endforeach(dim RANGE 1 9)
-    message(STATUS "Found alberta libraries for dimensions ${ALBERTA_WORLD_DIMS}")
-else(ALBERTA_LIBCHECK)
-  message(WARNING "Disabled checking whether libalberta_Nd can be linked.")
+  message(STATUS "Found alberta libraries for dimensions ${ALBERTA_WORLD_DIMS}")
 endif(ALBERTA_LIBCHECK)
 cmake_pop_check_state()
 
@@ -113,7 +118,7 @@ macro(add_dune_alberta_flags)
     else(NOT ADD_ALBERTA_WORLDDIM)
       list(FIND ALBERTA_WORLD_DIMS ${ADD_ALBERTA_WORLDDIM} _index)
       if(_index EQUAL -1)
-	message(FATAL_ERROR "There is no alberta library for dimension ${ADD_ALBERTA_GRIDDIM}.")
+        message(FATAL_ERROR "There is no alberta library for dimension ${ADD_ALBERTA_GRIDDIM}.")
       endif(_index EQUAL -1)
     endif(NOT ADD_ALBERTA_WORLDDIM)
     if(ADD_ALBERTA_SOURCE_ONLY)
@@ -122,16 +127,16 @@ macro(add_dune_alberta_flags)
       set_property(DIRECTORY APPEND PROPERTY INCLUDE_DIRECTORIES ${ALBERTA_INCLUDES})
     else()
       if(NOT ADD_ALBERTA_OBJECT)
-	# link to ALUGRID libraries
-	foreach(_target ${ADD_ALBERTA_UNPARSED_ARGUMENTS})
-	  target_link_libraries(${_target} dunealbertagrid_${ADD_ALBERTA_GRIDDIM}d
-	    ${ALBERTA_${ADD_ALBERTA_GRIDDIM}D_LIB}
-	    dunegrid ${DUNE_LIBS} ${ALBERTA_UTIL_LIB} ${ALBERTA_EXTRA_LIBS})
-	endforeach(_target ${ADD_ALBERTA_UNPARSED_ARGUMENTS})
+        # link to ALUGRID libraries
+        foreach(_target ${ADD_ALBERTA_UNPARSED_ARGUMENTS})
+          target_link_libraries(${_target} dunealbertagrid_${ADD_ALBERTA_GRIDDIM}d
+            ${ALBERTA_${ADD_ALBERTA_GRIDDIM}D_LIB}
+            dunegrid ${DUNE_LIBS} ${ALBERTA_UTIL_LIB} ${ALBERTA_EXTRA_LIBS})
+        endforeach(_target ${ADD_ALBERTA_UNPARSED_ARGUMENTS})
       endif(NOT ADD_ALBERTA_OBJECT)
       set(_prefix TARGET)
       set_property(${_prefix} ${ADD_ALBERTA_UNPARSED_ARGUMENTS} APPEND
-	PROPERTY INCLUDE_DIRECTORIES ${ALBERTA_INCLUDES})
+        PROPERTY INCLUDE_DIRECTORIES ${ALBERTA_INCLUDES})
     endif(ADD_ALBERTA_SOURCE_ONLY)
 
     replace_properties(${_prefix} ${ADD_ALBERTA_UNPARSED_ARGUMENTS}
