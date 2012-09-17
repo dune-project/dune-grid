a1fa6c61b55f1eeee3b09d15a1f346c25f1860f0
Oliver Sander <sander@dune-project.org>
2012-09-17 20:07:50 +0000

    Test local refinement on a load-balanced grid
    The new code is out-commented, because the feature is not
    working yet.
    
    Patch by Bernd Flemisch
    
    [[Imported from SVN: r8440]]

--- dune/grid/test/test-parallel-ug.cc
+++ dune/grid/test/test-parallel-ug.cc
@@ -173,9 +173,10 @@ void checkMappers(const GridView &gridView)
     ++ numEntities;
   if (numEntities != gridView.size(codim)) {
     DUNE_THROW(InvalidStateException,
-               "Number of codim " << codim
-                                  << " entities is inconsistent (iterator: " << numEntities
-                                  << " grid view: " << gridView.size(codim) << ")");
+               gridView.comm().rank() + 1
+               << ": Number of codim " << codim
+               << " entities is inconsistent (iterator: " << numEntities
+               << " grid view: " << gridView.size(codim) << ")");
   };
 
   typedef Dune::MultipleCodimMultipleGeomTypeMapper
@@ -189,15 +190,17 @@ void checkMappers(const GridView &gridView)
     int i = mapper.map(*it);
     if (i < 0 || i >= numEntities) {
       DUNE_THROW(InvalidStateException,
-                 "Mapper returns an invalid index for codim " << codim
-                                                              << " entities: " << i
-                                                              << " should be in range [0,"
-                                                              << numEntities -1 <<  "]");
+                 gridView.comm().rank() + 1
+                 << ": Mapper returns an invalid index for codim " << codim
+                 << " entities: " << i
+                 << " should be in range [0,"
+                 << numEntities -1 <<  "]");
     };
     if (indices[i] >= 0) {
       DUNE_THROW(InvalidStateException,
-                 "Mapper returns index " << i << " twice for codim " << codim
-                                         << " entities.");
+                 gridView.comm().rank() + 1
+                 << ": Mapper returns index " << i << " twice for codim " << codim
+                 << " entities.");
     }
     indices[i] = i;
   };
@@ -382,7 +385,7 @@ public:
 
 
 template <int dim>
-void testParallelUG()
+void testParallelUG(bool localRefinement)
 {
   std::cout << "Testing parallel UGGrid for " << dim << "D\n";
 
@@ -471,8 +474,37 @@ void testParallelUG()
   //  Refine globally and test again
   ////////////////////////////////////////////////////
 
-  std::cout << "Global refinement\n";
-  grid->globalRefine(1);
+  if (!localRefinement)
+  {
+    std::cout << "Global refinement\n";
+    grid->globalRefine(1);
+  }
+  else
+  {
+    std::cout << "Local refinement\n";
+
+    // mark all elements with x-coordinate < 0.5 for refinement
+    typename LeafGV::template Codim<0>::Iterator
+    it = grid->leafView().template begin<0>();
+    const typename LeafGV::template Codim<0>::Iterator
+    &endIt = grid->leafView().template end<0>();
+    for (; it != endIt; ++it) {
+      int nRefine = 1;
+      if (it->geometry().center()[0] < 0.5)
+        grid->mark(nRefine, *it);
+    }
+
+    // adapt the grid
+    grid->preAdapt();
+    grid->adapt();
+    grid->postAdapt();
+
+    // write the adapted grid to VTK
+    Dune::VTKWriter<LeafGV> writer(grid->leafView());
+    char fileName[1024];
+    sprintf(fileName, "adapted-grid-dim=%d", dim);
+    writer.write(fileName, Dune::VTK::ascii);
+  }
 
   for (int i=0; i<=grid->maxLevel(); i++) {
     checkIntersections(grid->levelView(i));
@@ -518,11 +550,19 @@ int main (int argc , char **argv) try
             << getpid()
             << " .\n";
 
-  // test 2D grid
-  testParallelUG<2>();
+  // test 2D grid with uniform refinement
+  testParallelUG<2>(false);
 
-  // test 3D grid
-  testParallelUG<3>();
+  // test 3D grid with uniform refinement
+  testParallelUG<3>(false);
+
+  // test 2D grid with adaptive refinement
+  // out-commented, because not working yet
+  //testParallelUG<2>(true);
+
+  // test 3D grid with adaptive refinement
+  // out-commented, because not working yet
+  //testParallelUG<3>(true);
 
   return 0;
 }
