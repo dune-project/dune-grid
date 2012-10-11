7aa15468cd9b16ca70419745b61f212c8d69dd5c
Christoph Grüninger <gruenich@dune-project.org>
2012-10-11 06:43:41 +0000

    Add convenience header vtk.hh which includes all available vtk writers. This fixes FS#802.
    [[Imported from SVN: r8506]]

---
+++ dune/grid/io/file/vtk.hh
@@ -0,0 +1,17 @@
+// -*- tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 2 -*-
+// vi: set et ts=4 sw=2 sts=2:
+
+#ifndef DUNE_GRID_IO_FILE_VTK_HH
+#define DUNE_GRID_IO_FILE_VTK_HH
+
+/** \file
+    \brief Convenience header which includes all available VTK writers
+ */
+
+#include "vtk/boundarywriter.hh"
+#include "vtk/subsamplingvtkwriter.hh"
+#include "vtk/vtksequencewriter.hh"
+#include "vtk/vtkwriter.hh"
+#include "vtk/volumewriter.hh"
+
+#endif // DUNE_GRID_IO_FILE_VTK_HH
