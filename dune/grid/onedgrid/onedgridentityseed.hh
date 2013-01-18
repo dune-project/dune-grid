55513fc3e87ab1514b9e5c81b0a13fb053125f8b
Oliver Sander <sander@dune-project.org>
2013-01-18 16:31:24 +0000

    OneDGridEntitySeed must export its codimension
    [[Imported from SVN: r8757]]

--- dune/grid/onedgrid/onedgridentityseed.hh
+++ dune/grid/onedgrid/onedgridentityseed.hh
@@ -20,6 +20,8 @@ namespace Dune {
     enum { dim = GridImp::dimension };
   public:
 
+    enum { codimension = codim };
+
     //! construct entity seed from entity
     OneDGridEntitySeed (const OneDGridEntity<codim,dim,GridImp>& entity)
       : target_(entity.target_)
