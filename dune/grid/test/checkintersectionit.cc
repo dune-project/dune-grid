8b7a8ffc13265d7cb3cfe14f41ba95fce5912ad2
Martin Nolte <mnolte@dune-project.org>
2013-04-17 11:31:54 +0000

    use refElement.geometry instead of the deprecated refElement.mapping
    [[Imported from SVN: r8943]]

--- dune/grid/test/checkintersectionit.cc
+++ dune/grid/test/checkintersectionit.cc
@@ -322,10 +322,10 @@ void checkIntersectionIterator(const GridViewType& view,
       const typename LocalGeometry::GlobalCoordinate &refNormal = refElement.integrationOuterNormal( indexInInside );
       typename IntersectionGeometry::GlobalCoordinate refIntNormal;
       geoInside.jacobianInverseTransposed( xInside ).mv( refNormal, refIntNormal );
-      // note: refElement.template mapping< 1 >( indexInInside ) is affine,
+      // note: refElement.template geometry< 1 >( indexInInside ) is affine,
       //       hence we may use any point to obtain the integrationElement
       refIntNormal *= geoInside.integrationElement( xInside ) * geometryInInside.integrationElement( pt )
-                      / refElement.template mapping< 1 >( indexInInside ).integrationElement( pt );
+                      / refElement.template geometry< 1 >( indexInInside ).integrationElement( pt );
 
       // Check outer normal
       // const typename IntersectionGeometry::GlobalCoordinate normal = iIt->outerNormal( pt );
