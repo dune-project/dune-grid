5b500d67128f77adc25278ba733f2bc5283a94f3
Jö Fahlke <jorrit@jorrit.de>
2013-08-29 17:03:27 +0200

    [gridviewpartitioningtest] Run each test with a timeout to catch inifinite loops.

--- dune/grid/utility/gridviewpartitioning/test/finitevolume_alusimplex.sh
+++ dune/grid/utility/gridviewpartitioning/test/finitevolume_alusimplex.sh
@@ -4,7 +4,7 @@ rm -f alusimplex_seq_plain_concentration-00001.vtu      \
       alusimplex_seq_opt_concentration-00001.vtu        \
       alusimplex_ost_plain_concentration-00001.vtu      \
       alusimplex_ost_opt_concentration-00001.vtu
-./finitevolume_alusimplex "${REFINES:-6}"
+timeout 20 ./finitevolume_alusimplex "${REFINES:-6}"
 cmp alusimplex_seq_plain_concentration-00001.vtu        \
     alusimplex_seq_opt_concentration-00001.vtu
 cmp alusimplex_seq_plain_concentration-00001.vtu        \
