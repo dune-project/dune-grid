eb57dd2467c6da44a07b04c72ea8dd84beacf929
Jö Fahlke <jorrit@jorrit.de>
2013-09-03 18:41:25 +0200

    [StridedPartitioning] Also define a partitioning class for strided entitsets.

--- dune/grid/utility/filteringentityset.hh
+++ dune/grid/utility/filteringentityset.hh
@@ -3,6 +3,8 @@
 #ifndef DUNE_GRID_UTILITY_FILTERINGENTITYSET_HH
 #define DUNE_GRID_UTILITY_FILTERINGENTITYSET_HH
 
+#include <cstddef>
+
 #include <tbb/tbb_stddef.h>
 
 #include <dune/grid/common/entityiterator.hh>
@@ -118,6 +120,38 @@ namespace Dune {
     StridedEntitySet(StridedEntitySet &other, tbb::split split) :
       Base(other, split)
     { }
+  };
+
+  template<class GridView, int codim>
+  class StridedPartitioning
+  {
+  public:
+    typedef StridedEntitySet<GridView, codim> EntitySet;
+
+    StridedPartitioning(const GridView &gv, std::size_t max_stride = 0) :
+      gv_(gv), max_stride_(max_stride)
+    { }
+
+    std::size_t partitions() const
+    {
+      return max_stride_;
+    }
+    // return number of elements visited by partition
+    std::size_t size(std::size_t partition) const
+    {
+      std::size_t totalsize = gv_.size(codim);
+      return totalsize / max_stride_ + (partition < totalsize % max_stride_);
+    }
+
+    // entityset for whole partitioning
+    EntitySet entitySet() const
+    {
+      return EntitySet(gv_, max_stride_);
+    }
+
+  private:
+    const GridView &gv_;
+    std::size_t max_stride_;
   };
 
 } // namespace Dune
