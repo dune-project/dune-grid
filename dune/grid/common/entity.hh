3d9140382143294d100bec91b403caea3f783792
Oliver Sander <sander@igpm.rwth-aachen.de>
2013-09-27 14:52:13 +0200

    [documentation] spelling

--- dune/grid/common/entity.hh
+++ dune/grid/common/entity.hh
@@ -545,7 +545,7 @@ namespace Dune
   protected:
     /** hide copy constructor */
     Entity(const Entity& rhs) : realEntity(rhs.realEntity) {}
-    /** hide assignement operator */
+    /** hide assignment operator */
     Entity & operator = (const Entity& rhs) {
       realEntity = rhs.realEntity;
       return *this;
