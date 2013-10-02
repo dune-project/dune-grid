20e33c9b82655f3a15580e6656a28d6aacf646e0
Oliver Sander <sander@igpm.rwth-aachen.de>
2013-10-02 16:19:42 +0200

    encode full file path in the single-inclusion macro

--- dune/grid/yaspgrid.hh
+++ dune/grid/yaspgrid.hh
@@ -1,7 +1,7 @@
 // -*- tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 2 -*-
 // vi: set et ts=4 sw=2 sts=2:
-#ifndef DUNE_YASPGRID_HH
-#define DUNE_YASPGRID_HH
+#ifndef DUNE_GRID_YASPGRID_HH
+#define DUNE_GRID_YASPGRID_HH
 
 #include <iostream>
 #include <vector>
