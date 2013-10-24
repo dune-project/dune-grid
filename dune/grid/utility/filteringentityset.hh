6e2445a7f972e135763a079cf836b9c671d7c08c
Jö Fahlke <jorrit@jorrit.de>
2013-10-25 00:19:56 +0200

    [cleanup][EntitySet] Remove the strided entity set, it can easily be constructed from a StridedIndexEntitySet and an IterableEntitySet.

--- dune/grid/utility/filteringentityset.hh
+++ dune/grid/utility/filteringentityset.hh
@@ -5,48 +5,22 @@
 
 #include <cstddef>
 
-#if HAVE_TBB
-#include <tbb/tbb_stddef.h>
-#endif
-
-#include <dune/grid/utility/iteratoradapters.hh>
+#include <dune/grid/utility/entityset.hh>
 #include <dune/grid/utility/iterableentityset.hh>
 
-#include "entityset.hh"
-
 namespace Dune {
-
-  template<class GV, int codim>
-  class StridedEntitySet :
-    public IterableEntitySet<
-      StridedIndexEntitySet<
-        typename GV::IndexSet,
-        typename GV::template Codim<codim>::Entity>,
-      typename GV::template Codim<codim>::Iterator>
-  {
-    typedef StridedIndexEntitySet<
-      typename GV::IndexSet,
-      typename GV::template Codim<codim>::Entity> Filter;
-    typedef IterableEntitySet<
-      Filter, typename GV::template Codim<codim>::Iterator> Base;
-
-  public:
-    StridedEntitySet(const GV &gv,
-                     typename GV::IndexSet::IndexType maxStride = 0) :
-      Base(Filter(gv.indexSet(), maxStride), gv)
-    { }
-#if HAVE_TBB
-    StridedEntitySet(StridedEntitySet &other, tbb::split split) :
-      Base(other, split)
-    { }
-#endif // HAVE_TBB
-  };
 
   template<class GridView, int codim>
   class StridedPartitioning
   {
+    typedef StridedIndexEntitySet<
+      typename GridView::IndexSet,
+      typename GridView::template Codim<codim>::Entity> Filter;
+
   public:
-    typedef StridedEntitySet<GridView, codim> EntitySet;
+    typedef IterableEntitySet<
+      Filter,
+      typename GridView::template Codimy<codim>::Iterator> EntitySet;
 
     StridedPartitioning(const GridView &gv, std::size_t max_stride = 0) :
       gv_(gv), max_stride_(max_stride)
@@ -66,7 +40,8 @@ namespace Dune {
     // entityset for whole partitioning
     EntitySet entitySet() const
     {
-      return EntitySet(gv_, max_stride_);
+      return EntitySet(Filter(gv_.indexSet(), max_stride_),
+                       gv_.template begin<codim>(), gv_.template end<codim>());
     }
 
   private:
