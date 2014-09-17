9b92decca51723fccd19f1a4f0877b130ea15a8f
Dominic Kempf <dominic.r.kempf@gmail.com>
2014-09-17 14:19:05 +0200

    Rewrite as a factory.
    The concept of a factory sounds suitable to keep the naming
    difficulties with this feature at bay.

--- dune/grid/yaspgrid/sequences.hh
+++ dune/grid/yaspgrid/sequences.hh
@@ -4,12 +4,15 @@
 #define DUNE_GRID_YASPGRID_SEQUENCES_HH
 
 #include <vector>
-
+#include <dune/common/array.hh>
 #include <dune/grid/common/exceptions.hh>
 
+#include "coordinates.hh"
+
 /** \file
- *  \brief This file provides methods to fill std::vectors with
- *  monotonous sequences as needed for a tensorproduct grid. Apart
+ *  \brief This file provides a factory class for tensorproduct
+ *  YaspGrids. This is a collection of methods to generate monotonous
+ *  sequences as needed for a tensorproduct grid. Apart
  *  from easy ones for locally equidistant grids, there are also
  *  more involved methods like splitting a range according to a
  *  geometric series.
@@ -19,163 +22,248 @@
 
 namespace Dune
 {
-
-  /** \brief pushs n intervals of length n on the vector v
-   * \param v the std::vector to operate on
-   * \param n the number of intervals to add
-   * \param h the interval length
-   *
-   * Given a vector with last element \f$x_0\f$, this will add elements
-   * \f$x_1,\dots ,x_n\f$ such that \f$x_i=x_0+i*h\f$.
-   */
-  template<class ctype>
-  void fill_intervals(std::vector<ctype>& v, int n, ctype h)
+  template<typename ctype, int dim>
+  class TensorYaspFactory
   {
-    if (v.empty())
-      v.push_back(static_cast<ctype>(0));
-    for (int i=0; i<n; i++)
-      v.push_back(v.back()+h);
+  public:
+
+    //! initialize the factory with a set of default values
+    TensorYaspFactory ()
+  : _comm (MPI_COMM_SELF), _periodic (), _overlap (1)
+  {
   }
 
-  /** \brief fills the range to end with n intervals
-   * \param v the std::vector to operate on
-   * \param n the number of intervals to add
-   * \param end the coordinate on the right border of the range
-   *
-   * Given a vector with last element \f$x_0\f$, this will add elements
-   * \f$x_1,\dots ,x_n\f$ such that \f$x_i=x_0+i*\frac{end-x_0}{n}\f$.
-   */
-  template<class ctype>
-  void fill_range(std::vector<ctype>& v, int n, ctype end)
-  {
-    if (v.empty())
-      v.push_back(static_cast<ctype>(0));
-    const ctype h = (end-v.back())/n;
-    for (int i=0; i<n-1; i++)
-      v.push_back(v.back()+h);
-    v.push_back(end);
-  }
+    //! finalizes the factory and gives a pointer to the constructed grid
+    YaspGrid<dim, TensorProductCoordinateContainer<ctype, dim> >* createGrid ()
+          {
+      if (!checkIfMonotonous (_coords))
+        DUNE_THROW(
+            Dune::Exception,
+            "TensorYaspFactory did not get enough coordinate information to construct a grid!");
+      return new YaspGrid<dim, TensorProductCoordinateContainer<ctype, dim> > (
+          _comm, _coords, _periodic, _overlap);
+          }
 
-  /** \brief adds intervals to a vector until a given coordinate is reached
-   * \param v the std::vector to operate on
-   * \param h the interval length
-   * \param end the coordinate on the right border of the range
-   *
-   *  Given a vector with last element \f$x_0\f$, this will add elements
-   * \f$x_1,\dots ,x_n\f$ such that \f$x_n < end < x_n + h\f$ and \f$x_{i+1}-x_i = h\f$.
-   */
-  template<class ctype>
-  void fill_until(std::vector<ctype>& v, ctype h, ctype end)
-  {
-    if (v.empty())
-      v.push_back(static_cast<ctype>(0));
-    while (v.back() < end)
-      v.push_back(v.back()+h);
-  }
+    //! set the communicator object
+    void setCommunicator (Dune::MPIHelper::MPICommunicator& comm)
+    {
+      _comm = comm;
+    }
 
-  /** \brief adds n intervals with a given length ratio and a given starting interval length.
-   *  \param v the std::vector to operate on
-   *  \param n the number of intervals to adds
-   *  \param ratio the ratio of \f$ h_{i+1}\f$ to \f$h_i\f$
-   *  \param h0 the starting interval length (optional)
-   *
-   *  Given a vector with last element \f$x_0\f$, this will add elements
-   *  \f$x_1,\dots ,x_n\f$ such that - with \f$h_i=x_{i+1}-x_i\f$ - \f$h_{i+1}=qh_i$
-   *  for a given ratio \f$q\f$. The first interval length can either be explicitly
-   *  given or be deduced by multiplying the ratio with the last interval
-   *  length in the container
-   */
-  template<class ctype>
-  void geometric_fill_intervals(std::vector<ctype>& v, int n, ctype ratio, ctype h0 = static_cast<ctype>(0))
-  {
-    if (v.empty())
-      v.push_back(static_cast<ctype>(0));
-    ctype h = h0;
-    if (h0 < 1e-8)
+    //! set whether the grid is periodic in direction dir
+    void setPeriodicity (int dir)
     {
-      if (v.size() < 2)
-        DUNE_THROW(GridError,"Not enough elements in coordinate container for geometric_fill_intervals");
-      else
-        h = (v.back() - v[v.size()-2]) * ratio;
+      _periodic[dir] = true;
     }
-    for (int i=0; i<n; i++)
-    {
-      v.push_back(v.back() + h);
-      h *= ratio;
-    }
-  }
 
-  /** \brief adds intervals according with a given length ratio until a given coordinate is reached
-   * \param v the std::vector to operate on
-   * \param ratio the ratio of \f$ h_{i+1}\f$ to \f$h_i\f$
-   * \param end the coordinate on the right border of the range
-   * \param h0 the starting interval length (optional)
-   *
-   *  Given a vector with last element \f$x_0\f$, this will add elements
-   *  \f$x_1,\dots ,x_n\f$ such that - with \f$h_i=x_{i+1}-x_i\f$ - \f$h_{i+1}=qh_i$
-   *  for a given ratio \f$q\f$ and that \f$x_n < end < x_n + h\f$. The first interval
-   *  length can either be explicitly given or be deduced by multiplying the ratio with
-   *  the last interval length in the container.
-   */
-  template<class ctype>
-  void geometric_fill_until(std::vector<ctype>& v, ctype ratio, ctype end, ctype h0 = static_cast<ctype>(0))
-  {
-    if (v.empty())
-      v.push_back(static_cast<ctype>(0));
-    ctype h = h0;
-    if (h0 < 1e-8)
+    //! set the number of overlap cells
+    void setOverlap (int overlap)
     {
-      if (v.size() < 2)
-        DUNE_THROW(GridError,"Not enough elements in coordinate container for geometric_fill_until");
-      else
-        h = (v.back() - v[v.size()-2]) * ratio;
+      _overlap = overlap;
     }
-    while (v.back() < end)
-    {
-      v.push_back(v.back()+h);
-      h *= ratio;
-    }
-  }
 
-  /** this implements a simple newton iteration for the function
-   *  \f$f(x) = -x^n+\frac{x_e-x_s}{h} (x-1)+1\f$
-   */
-  template<class ctype>
-  ctype newton(int n,ctype x_s,ctype x_e,ctype h)
-  {
-    ctype m = (x_e - x_s)/h;
-    ctype xold = 0.0;
-    ctype xnew = x_e-x_s;
-    while (fabs(xnew-xold) > 1E-8)
+    /** \brief set a starting value in a given direction d
+     * \param d the coordinate direction
+     * \param value the value to set
+     *
+     * This resizes the coordinate vector for the given direction to 1.
+     * Not using this function will result in 0.0 to be used as a lower
+     * bound of the coordinate range.
+     */
+    void setStart (int d, ctype value)
     {
-      xold = xnew;
-      xnew = xold - (-pow(xold,n)+m*xold-m+1)/(-n*pow(xold,n-1)+m);
+      _coords[d].resize (1);
+      _coords[d][0] = value;
     }
-    if (fabs(xnew-1) < 1E-6)
+
+    /** \brief pushs n intervals of length h in direction d
+     * \param d the coordinate direction
+     * \param n the number of intervals to add
+     * \param h the interval length
+     *
+     * Given a vector with last element \f$x_0\f$, this will add elements
+     * \f$x_1,\dots ,x_n\f$ such that \f$x_i=x_0+i*h\f$.
+     */
+    void fill_intervals (int d, int n, ctype h)
     {
-      xold = x_e-x_s;
-      xnew = 0.0;
-      while (fabs(xnew-xold) > 1E-8)
+      emptyCheck (d);
+      for (int i = 0; i < n; i++)
+        _coords[d].push_back (_coords[d].back () + h);
+    }
+
+    /** \brief fills the range to end with n intervals in direction d
+     * \param d the coordinate direction
+     * \param n the number of intervals to add
+     * \param end the coordinate on the upper border of the range
+     *
+     * Given a vector with last element \f$x_0\f$, this will add elements
+     * \f$x_1,\dots ,x_n\f$ such that \f$x_i=x_0+i*\frac{end-x_0}{n}\f$.
+     */
+    void fill_range (int d, int n, ctype end)
+    {
+      emptyCheck (d);
+      const ctype h = (end - _coords[d].back ()) / n;
+      for (int i = 0; i < n - 1; i++)
+        _coords[d].push_back (_coords[d].back () + h);
+      _coords[d].push_back (end);
+    }
+
+    /** \brief adds intervals in direction d until a given coordinate is reached
+     * \param d the coordinate direction
+     * \param h the interval length
+     * \param end the coordinate on the upper border of the range
+     *
+     *  Given a vector with last element \f$x_0\f$, this will add elements
+     * \f$x_1,\dots ,x_n\f$ such that \f$x_n < end < x_n + h\f$ and \f$x_{i+1}-x_i = h\f$.
+     */
+    void fill_until (int d, ctype h, ctype end)
+    {
+      emptyCheck (d);
+      while (_coords[d].back () < end)
+        _coords[d].push_back (_coords[d].back () + h);
+    }
+
+    /** \brief adds n intervals in direction d with a given length ratio and a given starting interval length.
+     *  \param d the coordinate direction
+     *  \param n the number of intervals to add
+     *  \param ratio the ratio of \f$ h_{i+1}\f$ to \f$h_i\f$
+     *  \param h0 the starting interval length (optional)
+     *
+     *  Given a vector with last element \f$x_0\f$, this will add elements
+     *  \f$x_1,\dots ,x_n\f$ such that - with \f$h_i=x_{i+1}-x_i\f$ - \f$h_{i+1}=qh_i$
+     *  for a given ratio \f$q\f$. The first interval length can either be explicitly
+     *  given or be deduced by multiplying the ratio with the last interval
+     *  length in the container
+     */
+    void geometric_fill_intervals (int d, int n, ctype ratio, ctype h0 =
+        static_cast<ctype> (0))
+    {
+      emptyCheck (d);
+      ctype h = h0;
+      if (h0 < 1e-8) //TODO bad comparison
+        h = lastInterval (d) * ratio;
+      for (int i = 0; i < n; i++)
       {
-        xold = xnew;
-        xnew = xold - (-pow(xold,n)+m*xold-m+1)/(-n*pow(xold,n-1)+m);
+        _coords[d].push_back (_coords[d].back () + h);
+        h *= ratio;
       }
     }
-    return xnew;
-  }
 
-  /** \brief fills a coordinate range with n intervals accoring to a geometric series
-   * \param v the std::vector to operate on
-   * \param n the number of intervals to add
-   * \param end the coordinate of the right border of the range
-   * \param h the interval length to start or end with (see below) (optional)
-   * \param first true if the given h is to be the first interval, false if last one
-   */
-  template<class ctype>
-  void geometric_fill_range(std::vector<ctype>& v, int n, ctype end, ctype h = static_cast<ctype>(0), bool first=true)
-  {
+    /** \brief adds intervals in direction d according with a given length ratio until a given coordinate is reached
+     * \param d the coordinate direction
+     * \param ratio the ratio of \f$ h_{i+1}\f$ to \f$h_i\f$
+     * \param end the coordinate on the right border of the range
+     * \param h0 the starting interval length (optional)
+     *
+     *  Given a vector with last element \f$x_0\f$, this will add elements
+     *  \f$x_1,\dots ,x_n\f$ such that - with \f$h_i=x_{i+1}-x_i\f$ - \f$h_{i+1}=qh_i$
+     *  for a given ratio \f$q\f$ and that \f$x_n < end < x_n + h\f$. The first interval
+     *  length can either be explicitly given or be deduced by multiplying the ratio with
+     *  the last interval length in the container.
+     */
+    void geometric_fill_until (int d, ctype ratio, ctype end, ctype h0 =
+        static_cast<ctype> (0))
+    {
+      emptyCheck (d);
+      ctype h = h0;
+      if (h0 < 1e-8)
+        h = lastInterval (d) * ratio;
+      while (_coords[d].back () < end)
+      {
+        _coords[d].push_back (_coords[d].back () + h);
+        h *= ratio;
+      }
+    }
 
-  }
+    /** \brief fills a coordinate range in direction d with n intervals according to a geometric series
+     * \param d the coordinate direction
+     * \param n the number of intervals to add
+     * \param end the coordinate of the upper border of the range
+     * \param h the interval length to start or end with (see below) (optional)
+     * \param first true if the given h is to be the first interval, false if last one
+     *
+     * Given a vector with last element \f$x_0\f$, this will add elements
+     * \f$x_1,\dots ,x_n\f$ such that - with \f$h_i=x_{i+1}-x_i\f$ - the ratio
+     * \f$h_{i+1} / h_i\f$ is fixed throughout the range and \f$x_n=end\f$.
+     * The first interval length can either be explicitly given or be deduced by taking
+     * the last interval length in the container. By setting the optional parameter first
+     * to false, the given h can instead be used as last interval length in the range.
+     */
+    void geometric_fill_range (int d, int n, ctype end, ctype h =
+        static_cast<ctype> (0),
+        bool first = true)
+    {
+      emptyCheck (d);
+      if (h < 1e-8)
+        h = lastInterval (d);
+      ctype ratio = newton (n, _coords[d].back (), end, h);
+      if (!first)
+      {
+        h = h * pow (ratio, n - 1);
+        ratio = 1 / ratio;
+      }
+      for (int i = 0; i < n - 1; i++)
+      {
+        _coords[d].push_back (_coords[d].back () + h);
+        h *= ratio;
+      }
+      _coords[d].push_back (end);
+    }
+
+  private:
+    // check whether the ith component is empty and add a 0.0 entry if so
+    void emptyCheck (int i)
+    {
+      if (_coords[i].empty ())
+        _coords[i].push_back (static_cast<ctype> (0));
+    }
+
+    // returns the last interval length in direction d
+    ctype lastInterval (int d)
+    {
+      if (_coords[d].size () < 2)
+        DUNE_THROW(
+            GridError,
+            "Not enough elements in coordinate container to deduce interval length in TensorYaspFactory");
+      else
+        return _coords[d].back () - _coords[d][_coords[d].size () - 2];
+    }
+
+    /** this implements a simple newton iteration for the function
+     *  \f$f(x) = -x^n+\frac{x_e-x_s}{h} (x-1)+1\f$
+     */
+    ctype newton (int n, ctype x_s, ctype x_e, ctype h)
+    {
+      ctype m = (x_e - x_s) / h;
+      ctype xold = 0.0;
+      ctype xnew = x_e - x_s;
+      while (std::abs (xnew - xold) > 1E-8)
+      {
+        xold = xnew;
+        xnew = xold
+            - (-pow (xold, n) + m * xold - m + 1)
+            / (-n * pow (xold, n - 1) + m);
+      }
+      if (std::abs (xnew - 1) < 1E-6)
+      {
+        xold = x_e - x_s;
+        xnew = 0.0;
+        while (std::abs (xnew - xold) > 1E-8)
+        {
+          xold = xnew;
+          xnew = xold
+              - (-pow (xold, n) + m * xold - m + 1)
+              / (-n * pow (xold, n - 1) + m);
+        }
+      }
+      return xnew;
+    }
+
+    Dune::array<std::vector<ctype>, dim> _coords;
+    const Dune::MPIHelper::MPICommunicator& _comm;
+    std::bitset<dim> _periodic;
+    int _overlap;
+  };
 }
 
 #endif
