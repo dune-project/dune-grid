d90bf64c154e508282bf9dd259e52d0ebf883a69
Steffen Müthing <muething@dune-project.org>
2014-10-16 14:32:50 +0200

    [Interface] Deprecate comparisons between different iterator types
    As discussed in FS#1513, comparing different types of iterators is at
    best an exercise in ambiguity, and its main use case disappears with the
    removal of EntityPointer.
    
    So this patch deprecates those kinds of comparisons. Comparing two
    iterators of the same type is now handled by two new non-template
    comparison operators. Moreover, there are now also new comparison
    operators for EntityPointer because the new templated operators shadow
    the versions in EntityPointer itself.

--- dune/grid/common/entityiterator.hh
+++ dune/grid/common/entityiterator.hh
@@ -73,11 +73,31 @@ namespace Dune
       return & this->realIterator.dereference();
     }
 
+    /** \brief Checks for equality. */
+    bool operator==(const EntityIterator& rhs) const
+    {
+      return this->realIterator.equals(rhs.realIterator);
+    }
+
+    /** \brief Checks for inequality. */
+    bool operator!=(const EntityIterator& rhs) const
+    {
+      return !this->realIterator.equals(rhs.realIterator);
+    }
+
     /** \brief Checks for equality.
             Only works for EntityPointers and iterators on the same grid.
             Due to the conversion operators one can compare
             all kinds of iterators and EntityPointer.
 
+        \deprecated Comparing different types of iterators is deprecated and will be removed after the
+                    release of dune-grid 2.4. If you want to compare the entities pointed at by the
+                    iterators, dereference the iterators before comparing them.
+     */
+    template< class ItImp >
+    DUNE_DEPRECATED_MSG("Comparing different types of iterators is deprecated and will be removed after the \
+release of dune-grid 2.4. If you want to compare the entities pointed at by the iterators, dereference the \
+iterators before comparing them.")
     bool operator== ( const EntityIterator< codim, Grid, ItImp > &rhs ) const
     {
       return this->equals( rhs );
@@ -87,9 +107,54 @@ namespace Dune
             Only works for EntityPointers and iterators on the same grid.
             Due to the conversion operators one can compare
             all kinds of iterators and EntityPointer.
+
+        \deprecated Comparing different types of iterators is deprecated and will be removed after the
+                    release of dune-grid 2.4. If you want to compare the entities pointed at by the
+                    iterators, dereference the iterators before comparing them.
      */
     template< class ItImp >
+    DUNE_DEPRECATED_MSG("Comparing different types of iterators is deprecated and will be removed after the \
+release of dune-grid 2.4. If you want to compare the entities pointed at by the iterators, dereference the \
+iterators before comparing them.")
     bool operator!= ( const EntityIterator< codim, Grid, ItImp > &rhs ) const
+    {
+      return !this->equals( rhs );
+    }
+
+    /** \brief Checks for equality.
+            Only works for EntityPointers and iterators on the same grid.
+            Due to the conversion operators one can compare
+            all kinds of iterators and EntityPointer.
+
+        \deprecated Comparing different types of iterators is deprecated and will be removed after the
+                    release of dune-grid 2.4. If you want to compare the entities pointed at by the
+                    iterators, dereference the iterators before comparing them.
+     */
+    template< class ItImp >
+    DUNE_DEPRECATED_MSG("EntityPointer is deprecated will be removed after the release of dune-grid 2.4. \
+Moreover, comparing different types of iterators is deprecated as well and will also be removed after the \
+release of dune-grid 2.4. If you want to compare the entities pointed at by the iterators, dereference the \
+iterators before comparing them.")
+    bool operator== ( const EntityPointer< Grid, ItImp > &rhs ) const
+    {
+      return this->equals( rhs );
+    }
+
+    /** \brief Checks for inequality.
+            Only works for EntityPointers and iterators on the same grid.
+            Due to the conversion operators one can compare
+            all kinds of iterators and EntityPointer.
+
+        \deprecated Comparing different types of iterators is deprecated and will be removed after the
+                    release of dune-grid 2.4. If you want to compare the entities pointed at by the
+                    iterators, dereference the iterators before comparing them.
+     */
+    template< class ItImp >
+    DUNE_DEPRECATED_MSG("EntityPointer is deprecated will be removed after the release of dune-grid 2.4. \
+Moreover, comparing different types of iterators is deprecated as well and will also be removed after the \
+release of dune-grid 2.4. If you want to compare the entities pointed at by the iterators, dereference the \
+iterators before comparing them.")
+    bool operator!= ( const EntityPointer< Grid, ItImp > &rhs ) const
     {
       return !this->equals( rhs );
     }
