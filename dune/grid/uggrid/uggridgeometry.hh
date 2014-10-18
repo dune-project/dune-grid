49d480345537839f973abc9bf1074e7fee0833c5
Oliver Sander <sander@igpm.rwth-aachen.de>
2014-10-18 20:13:52 +0200

    Use MultiLinearGeometry instead of CachedMultiLinearGeometry for UGGrid geometries
    We don't need no caching here.  The cached versions were only used because the
    non-cached ones were buggy.  This has been fixed by Ansgar a while ago.

--- dune/grid/uggrid/uggridgeometry.hh
+++ dune/grid/uggrid/uggridgeometry.hh
@@ -145,13 +145,13 @@ namespace Dune {
 
   template<class GridImp>
   class UGGridGeometry<2, 3, GridImp> :
-    public CachedMultiLinearGeometry<typename GridImp::ctype, 2, 3>
+    public MultiLinearGeometry<typename GridImp::ctype, 2, 3>
   {
   public:
 
     /** \brief Constructor from a given geometry type and a vector of corner coordinates */
     UGGridGeometry(const GeometryType& type, const std::vector<FieldVector<typename GridImp::ctype,3> >& coordinates)
-      : CachedMultiLinearGeometry<typename GridImp::ctype, 2, 3>(type, coordinates)
+      : MultiLinearGeometry<typename GridImp::ctype, 2, 3>(type, coordinates)
     {}
 
   };
@@ -185,13 +185,13 @@ namespace Dune {
 
   template<class GridImp>
   class UGGridGeometry <1, 2, GridImp> :
-    public CachedMultiLinearGeometry<typename GridImp::ctype,1,2>
+    public MultiLinearGeometry<typename GridImp::ctype,1,2>
   {
   public:
 
     /** \brief Constructor from a given geometry type and a vector of corner coordinates */
     UGGridGeometry(const GeometryType& type, const std::vector<FieldVector<typename GridImp::ctype,2> >& coordinates)
-      : CachedMultiLinearGeometry<typename GridImp::ctype, 1, 2>(type, coordinates)
+      : MultiLinearGeometry<typename GridImp::ctype, 1, 2>(type, coordinates)
     {}
 
   };
