eac6de5f471dca85d553473fa25fa27f50e2f416
Steffen Müthing <muething@dune-project.org>
2015-02-06 00:01:43 +0100

    [IdentityGrid][1/11] Port to copyable entities and intersections
    This patch prepares the EntityPointer implementation for the copyable
    entities. Instead of storing a mutable entity, it now contains a host
    EntityPointer and returns a temporary entity when
    dereferenced. Moreover, taking inspiration from GeometryGrid we now
    directly store the most-descended host iterator type instead of a plain
    host EntityPointer. The type of the host iterator is passed as an
    additional template parameter. This tricks avoids storing two separate
    host EntityPointers for iterators.

--- dune/grid/identitygrid/identitygridentitypointer.hh
+++ dune/grid/identitygrid/identitygridentitypointer.hh
@@ -14,12 +14,15 @@ namespace Dune {
 
   /** Acts as a pointer to an  entities of a given codimension.
    */
-  template<int codim, class GridImp>
+  template<int codim, class GridImp, class HostGridEntityPointer_>
   class IdentityGridEntityPointer
   {
   private:
 
     enum { dim = GridImp::dimension };
+
+    template<int, typename, typename>
+    friend class IdentityGridEntityPointer;
 
 
   public:
@@ -33,37 +36,61 @@ namespace Dune {
 
     typedef typename GridImp::template Codim<codim>::Entity Entity;
 
-    typedef IdentityGridEntityPointer<codim,GridImp> Base;
-
     // The codimension of this entitypointer wrt the host grid
     enum {CodimInHostGrid = GridImp::HostGridType::dimension - GridImp::dimension + codim};
 
     // EntityPointer to the equivalent entity in the host grid
-    typedef typename GridImp::HostGridType::Traits::template Codim<CodimInHostGrid>::EntityPointer HostGridEntityPointer;
+    typedef HostGridEntityPointer_ HostGridEntityPointer;
 
 
     //! constructor
-    template< class HostGridEntityPointer >
-    IdentityGridEntityPointer (const GridImp* identityGrid, const HostGridEntityPointer& hostEntity_) :
-      identityGrid_(identityGrid),
-      virtualEntity_(identityGrid, hostEntity_)
+    IdentityGridEntityPointer (const GridImp* identityGrid, const HostGridEntityPointer& hostEntityPointer)
+      : identityGrid_(identityGrid)
+      , hostEntityPointer_(hostEntityPointer)
+    {}
+
+    ///! copy constructor from EntityPointer storing different host EntityPointer
+    template<typename ForeignHostGridEntityPointer>
+    explicit IdentityGridEntityPointer (const IdentityGridEntityPointer<codim,GridImp,ForeignHostGridEntityPointer>& entityPointer)
+      : identityGrid_(entityPointer.identityGrid_)
+      , hostEntityPointer_(entityPointer.hostEntityPointer_)
+    {}
+
+    ///! assignment operator from EntityPointer storing different host EntityPointer
+    template<typename ForeignHostGridEntityPointer>
+    IdentityGridEntityPointer& operator=(const IdentityGridEntityPointer<codim,GridImp,ForeignHostGridEntityPointer>& entityPointer)
+    {
+      hostEntityPointer_ = entityPointer.hostEntityPointer_;
+      return *this;
+    }
+
+    //! Move constructor to avoid copying the host EntityPointer
+    IdentityGridEntityPointer (const GridImp* identityGrid, HostGridEntityPointer&& hostEntityPointer)
+      : identityGrid_(identityGrid)
+      , hostEntityPointer_(std::move(hostEntityPointer))
     {}
 
     //! Constructor from an IdentityGrid entity
     IdentityGridEntityPointer (const IdentityGridEntity<codim,dim,GridImp>& entity)
-      : identityGrid_(entity.identityGrid_),
-        virtualEntity_(entity.identityGrid_, entity.hostEntity_)
+      : identityGrid_(entity.identityGrid_)
+      , hostEntityPointer_(entity.hostEntity_)
     {}
 
     //! equality
-    bool equals(const IdentityGridEntityPointer<codim,GridImp>& i) const {
-      return virtualEntity_.getTarget() == i.virtualEntity_.getTarget();
+    bool equals(const IdentityGridEntityPointer& i) const {
+      return hostEntityPointer_ == i.hostEntityPointer_;
     }
 
+    //! equality with EntityPointer based on different host EntityPointer
+    template<typename ForeignHostGridEntityPointer>
+    bool equals(const IdentityGridEntityPointer<codim,GridImp,ForeignHostGridEntityPointer>& entityPointer) const
+    {
+      return  hostEntityPointer_ == entityPointer.hostEntityPointer_;
+    }
 
     //! dereferencing
-    Entity& dereference() const {
-      return virtualEntity_;
+    Entity dereference() const {
+      return Entity{{identityGrid_,*hostEntityPointer_}};
     }
 
     //! Make this pointer as small as possible
@@ -73,7 +100,7 @@ namespace Dune {
 
     //! ask for level of entity
     int level () const {
-      return virtualEntity_.level();
+      return hostEntityPointer_->level();
     }
 
 
@@ -81,8 +108,8 @@ namespace Dune {
 
     const GridImp* identityGrid_;
 
-    //! virtual entity
-    mutable IdentityGridMakeableEntity<codim,dim,GridImp> virtualEntity_;
+    //! host EntityPointer
+    HostGridEntityPointer hostEntityPointer_;
 
 
   };
