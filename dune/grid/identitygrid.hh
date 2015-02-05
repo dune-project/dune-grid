3b307b33c8b272a4786f842e20cda6905b6452d9
Steffen Müthing <muething@dune-project.org>
2015-02-06 00:20:05 +0100

    [IdentityGrid][3/11] Port to copyable entities and intersections
    For our EntityPointer optimization trick of directly storing the
    most-derived host iterator, we had to change the type signature of the
    implementation and thus have to somehow integrate that change into the
    GridTraits.
    
    This patch takes care of that problem by letting the actual Traits
    inherit from the default implementation and fixing the EntityPointer
    typedef.

--- dune/grid/identitygrid.hh
+++ dune/grid/identitygrid.hh
@@ -36,18 +36,37 @@ namespace Dune
   template< class Grid >
   struct HostGridAccess;
 
+  namespace {
+
+    // This is just a placeholder to stuff into the standard
+    // GridTraits, as those expect an EntityPointer implementation
+    // with two template parameters, but our EntityPointer uses three.
+    template<int,typename>
+    struct DummyIdentityGridEntityPointer
+    {};
+
+  }
 
 
   template<int dim, class HostGrid>
   struct IdentityGridFamily
   {
+
+  private:
+
+    // The type signature of our EntityPointer implementation does not fit into
+    // the scheme expected by GridTraits, so we have to provide our own Traits
+    // On the other hand, we don't want to retype everything, so we just inherit
+    // from the default Traits and just tweak the EntityPointer type in the derived
+    // Traits class
+
     typedef GridTraits<
         dim,
         HostGrid::dimensionworld,
         Dune::IdentityGrid<HostGrid>,
         IdentityGridGeometry,
         IdentityGridEntity,
-        IdentityGridEntityPointer,
+        DummyIdentityGridEntityPointer, // placeholder
         IdentityGridLevelIterator,
         IdentityGridLeafIntersection,
         IdentityGridLevelIntersection,
@@ -65,7 +84,37 @@ namespace Dune
         DefaultLevelGridViewTraits,
         DefaultLeafGridViewTraits,
         IdentityGridEntitySeed
-        > Traits;
+        > BaseTraits;
+
+  public:
+
+    // inherit everyting from the default Traits...
+    struct Traits
+      : public BaseTraits
+    {
+
+      // Except for the Codim struct, which we reimplement, but which
+      // again inherits everything from the base class...
+      template<int codim>
+      struct Codim
+        : public BaseTraits::template Codim<codim>
+      {
+
+        // ... except for the EntityPointer, which we override with our
+        // actual implementation
+        typedef Dune::EntityPointer<
+          const typename BaseTraits::Grid,
+          IdentityGridEntityPointer<
+            codim,
+            const typename BaseTraits::Grid,
+            typename HostGrid::template Codim<codim>::EntityPointer
+            >
+          > EntityPointer;
+
+      };
+
+    };
+
   };
 
   //**********************************************************************
