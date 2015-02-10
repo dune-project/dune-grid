101ca29ddb3dcbc767d7f5a9460abea7cad151e7
Steffen Müthing <muething@dune-project.org>
2015-02-10 18:50:45 +0100

    [GeometryGrid][11/12] Implement copyable entities / intersections
    Deprecate GeometryGrid::entityPointer(EntitySeed) and add replacement
    method GeometryGrid::entity(EntitySeed).

--- dune/grid/geometrygrid/grid.hh
+++ dune/grid/geometrygrid/grid.hh
@@ -4,6 +4,7 @@
 #define DUNE_GEOGRID_GRID_HH
 
 #include <dune/common/nullptr.hh>
+#include <dune/common/deprecated.hh>
 
 #include <dune/grid/common/grid.hh>
 
@@ -568,10 +569,20 @@ namespace Dune
     /** \brief obtain EntityPointer from EntitySeed. */
     template< class EntitySeed >
     typename Traits::template Codim< EntitySeed::codimension >::EntityPointer
+    DUNE_DEPRECATED_MSG("entityPointer() is deprecated and will be removed after the release of dune-grid 2.4. Use entity() instead to directly obtain an Entity object.")
     entityPointer ( const EntitySeed &seed ) const
     {
       typedef typename Traits::template Codim< EntitySeed::codimension >::EntityPointerImpl EntityPointerImpl;
       return EntityPointerImpl( *this, seed );
+    }
+
+    /** \brief obtain Entity from EntitySeed. */
+    template< class EntitySeed >
+    typename Traits::template Codim< EntitySeed::codimension >::Entity
+    entity ( const EntitySeed &seed ) const
+    {
+      typedef typename Traits::template Codim< EntitySeed::codimension >::EntityImpl EntityImpl;
+      return EntityImpl( *this, seed );
     }
 
     /** \} */
