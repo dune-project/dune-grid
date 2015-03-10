ced82cb090407d248eba3779693207f7cfefeca8
Steffen Müthing <muething@dune-project.org>
2015-03-10 16:16:52 +0100

    [Tests] Don't test deprecated grid functions in starcdreadertest
    (cherry picked from commit f4cea4a31adbcbe1c7c00123be43d1bf67f7de1d)

--- dune/grid/io/file/test/starcdreadertest.cc
+++ dune/grid/io/file/test/starcdreadertest.cc
@@ -1,6 +1,8 @@
 // -*- tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 2 -*-
 // vi: set et ts=4 sw=2 sts=2:
 
+#define DISABLE_DEPRECATED_METHOD_CHECK 1
+
 #include <config.h>
 
 #include <string>
