4f66cfb3d563261c20e54e2368cc3af9879ebbfc
Linus Seelinger <S.Linus@gmx.de>
2015-05-11 11:43:27 +0200

    [Tests] Split yaspgrid tests into multiple ones in a separate directory, reduced run time for 3d case
    (cherry picked from commit 1dda7ea599658c56d5750a863c4f3bb581bfe22e)
    
    Conflicts:
    	dune/grid/test/CMakeLists.txt

---
+++ dune/grid/test/yasp/test-yaspgrid-backuprestore.cc
@@ -0,0 +1,35 @@
+// -*- tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 2 -*-
+// vi: set et ts=4 sw=2 sts=2:
+
+#include <config.h>
+
+#include <iostream>
+#include <fstream>
+#include <sstream>
+
+#include <dune/common/parallel/mpihelper.hh>
+#include <dune/grid/yaspgrid.hh>
+#include <dune/grid/yaspgrid/backuprestore.hh>
+
+#include "test-yaspgrid.hh"
+
+int main (int argc , char **argv) {
+  try {
+    // Initialize MPI, if present
+    Dune::MPIHelper::instance(argc, argv);
+
+    // check the backup restore facility
+    check_backuprestore(YaspFactory<2,Dune::EquidistantCoordinates<double,2> >::buildGrid());
+    check_backuprestore(YaspFactory<2,Dune::EquidistantOffsetCoordinates<double,2> >::buildGrid());
+    check_backuprestore(YaspFactory<2,Dune::TensorProductCoordinates<double,2> >::buildGrid());
+
+  } catch (Dune::Exception &e) {
+    std::cerr << e << std::endl;
+    return 1;
+  } catch (...) {
+    std::cerr << "Generic exception!" << std::endl;
+    return 2;
+  }
+
+  return 0;
+}
---
+++ dune/grid/test/yasp/test-yaspgrid-tensorgridfactory.cc
@@ -0,0 +1,44 @@
+// -*- tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 2 -*-
+// vi: set et ts=4 sw=2 sts=2:
+
+#include <config.h>
+
+#include <iostream>
+#include <fstream>
+#include <sstream>
+
+#include <dune/common/parallel/mpihelper.hh>
+#include <dune/grid/yaspgrid.hh>
+#include <dune/grid/utility/tensorgridfactory.hh>
+
+#include "test-yaspgrid.hh"
+
+int main (int argc , char **argv) {
+  try {
+    // Initialize MPI, if present
+    Dune::MPIHelper::instance(argc, argv);
+
+    // check the factory class for tensorproduct grids
+    Dune::TensorGridFactory<Dune::YaspGrid<2, Dune::TensorProductCoordinates<double,2> > > factory;
+    factory.setStart(0,-100.);
+    factory.fillIntervals(0,10,20.);
+    factory.fillRange(0, 5, 130.);
+    factory.geometricFillIntervals(0, 5, 2.0);
+
+    factory.geometricFillRange(1,10,100.,1.,false);
+    factory.fillRange(1,10,200);
+    factory.geometricFillRange(1,10,250.,1.,true);
+    factory.fillUntil(1,50,1000.);
+
+    factory.createGrid();
+
+  } catch (Dune::Exception &e) {
+    std::cerr << e << std::endl;
+    return 1;
+  } catch (...) {
+    std::cerr << "Generic exception!" << std::endl;
+    return 2;
+  }
+
+  return 0;
+}
