88284e13d998a2cb58332cde5745970c93b56512
Steffen Müthing <muething@dune-project.org>
2015-05-27 11:19:36 +0200

    Merge remote-tracking branch 'linus/master'
    This branch adds a utility function that can be used to visualize the
    structure of a 2D grid, including indices of cells, edges, vertices.
    
    It was written by Linus Seelinger (who is currently a HiWi in
    Heidelberg) and has already proven very useful while debugging some
    YaspGrid issues.
    
    It is entirely self-contained and should thus not have any impact on the
    remainder of dune-grid. Linus has also written a unit test.
    
    * linus/master:
      Add a grid visualization tool 'printgrid' that uses gnuplot to show the grid structure, indices etc.
    
    (cherry picked from commit 1190e518da59c3a116f8f8868154e080a53b5962)
    Signed-off-by: Steffen Müthing <muething@dune-project.org>

---
+++ dune/grid/io/file/test/printgridtest.cc
@@ -0,0 +1,43 @@
+// -*- tab-width: 4; indent-tabs-mode: nil -*-
+
+// Based on: Boilerplate tutorial poisson_uniform.cc
+
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
+
+#include <dune/grid/yaspgrid.hh>
+
+#include <dune/grid/io/file/printgrid.hh>
+
+int main(int argc, char **argv)
+{
+  try {
+    // initialize MPI, finalize is done automatically on exit
+    Dune::MPIHelper& helper = Dune::MPIHelper::instance(argc,argv);
+
+    // make grid
+    const int dim = 2;
+    Dune::FieldVector<double,dim> L(1.0);
+    Dune::array<int,dim> N(Dune::fill_array<int,dim>(4));
+    std::bitset<dim> periodic (false);
+    periodic[0] = true;
+    int overlap = 1;
+    Dune::YaspGrid<dim> grid(L,N,periodic, overlap);
+
+    // write .plt files (one for png, one for svg) without executing gnuplot on them
+    Dune::printgrid (grid, helper, "printgridtest_yasp_svg", 4000, false, false);
+    Dune::printgrid (grid, helper, "printgridtest_yasp_png", 4000, false);
+  } catch (Dune::Exception &e) {
+    std::cerr << e << std::endl;
+    return 1;
+  } catch (std::exception &e) {
+    std::cerr << e.what() << std::endl;
+    return 1;
+  } catch (...) {
+    std::cerr << "Generic exception!" << std::endl;
+    return 2;
+  }
+  // done
+  return 0;
+}
