df405258523b59319f131b54f1dbbb690a237c71
Tobias Malkmus <tomalk@mathematik.uni-freiburg.de>
2015-05-28 09:16:05 +0200

    [bugfix] add header guards and make headercheck pass
    (cherry picked from commit 3d878e2f01ed2577fd9f784bff32dc47ce09f1cb)
    Signed-off-by: Steffen Müthing <muething@dune-project.org>

--- dune/grid/io/file/printgrid.hh
+++ dune/grid/io/file/printgrid.hh
@@ -1,9 +1,12 @@
 // -*- tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 2 -*-
 // vi: set et ts=4 sw=2 sts=2:
+#ifndef DUNE_PRINTGRID_HH
+#define DUNE_PRINTGRID_HH
 
 #include <fstream>
 #include <string>
 
+#include <dune/common/parallel/mpihelper.hh>
 #include <dune/grid/common/mcmgmapper.hh>
 
 namespace Dune {
@@ -195,3 +198,5 @@ namespace Dune {
   }
 
 }
+
+#endif // #ifndef DUNE_PRINTGRID_HH
