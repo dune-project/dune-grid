dune-project/dune-grid

commit 568fdf7a90c3dd0ac874016cfae94ad82d04aaf9

Properly implement interpolation in P1VTKFunction
The P1 function never actually did first-order interpolation, which has
caused some confusion every now and again (see FS 676).  This patch
implements the interpolation properly, mainly because I found a smart way
to use MultilinearGeometry for that.

dune/grid/io/file/vtk/function.hh
@@ -11,6 +11,7 @@
 
 #include <dune/geometry/type.hh>
 #include <dune/geometry/referenceelements.hh>
+#include <dune/geometry/multilineargeometry.hh>
 
 #include <dune/grid/common/mcmgmapper.hh>
 
@@ -178,11 +179,6 @@ namespace Dune
    * may represent a field with multiple components of which one may be
    * selected.
    *
-   * \note While this function may be evaluated anywhere on a given grid
-   *       element, it does not interpolate between the corners of the element
-   *       -- instead, it returns the value at the nearest corner (as
-   *       determined in local coordinates).
-   *
    * \tparam GV Type of GridView the vector applies to.
    * \tparam V  Type of vector.
    */
@@ -222,23 +218,16 @@ namespace Dune
     virtual double evaluate (int comp, const Entity& e,
                              const Dune::FieldVector<ctype,dim>& xi) const
     {
-      double min=1E100;
-      int imin=-1;
-      Dune::GeometryType gt = e.type();
-      const int subEntities = e.subEntities(dim);
-      for (int i=0; i<subEntities; ++i)
-      {
-        Dune::FieldVector<ctype,dim> local =
-          Dune::ReferenceElements<ctype,dim>::general(gt)
-          .position(i,dim);
-        local -= xi;
-        if (local.infinity_norm()<min)
-        {
-          min = local.infinity_norm();
-          imin = i;
-        }
-      }
-      return v[mapper.subIndex(e,imin,dim)*ncomps_+mycomp_];
+      const unsigned int dim = Entity::mydimension;
+      const unsigned int nVertices = e.subEntities(dim);
+
+      std::vector<FieldVector<typename V::field_type,1> > cornerValues(nVertices);
+      for (int i=0; i<nVertices; ++i)
+        cornerValues[i] = v[mapper.subIndex(e,i,dim)*ncomps_+mycomp_];
+
+      // (Ab)use the MultiLinearGeometry class to do multi-linear interpolation between scalars
+      const MultiLinearGeometry<ctype,dim,1> interpolation(e.type(), cornerValues);
+      return interpolation.global(xi);
     }
 
     //! get name

commit 863774404e3467191c3bd0eae2c35a2262c47e9a

[vtkfunction][regression][release] Generalize P1VTKFunction to work also with std::vector
Commit 568fdf7a90c3dd0ac874016cfae94ad82d04aaf9 broke the ability
of P1VTKFunction to handle data stored in a V=std::vector since it
requires V::field_type, which is not offered by std::vector. This
patch uses ctype instead which come closest to the situation before
the offending commit.

Jö> This fixes a regression, that's why it should go into the release.  See
Jö> FS#1700.

dune/grid/io/file/vtk/function.hh
@@ -221,7 +221,7 @@ namespace Dune
       const unsigned int dim = Entity::mydimension;
       const unsigned int nVertices = e.subEntities(dim);
 
-      std::vector<FieldVector<typename V::field_type,1> > cornerValues(nVertices);
+      std::vector<FieldVector<ctype,1> > cornerValues(nVertices);
       for (int i=0; i<nVertices; ++i)
         cornerValues[i] = v[mapper.subIndex(e,i,dim)*ncomps_+mycomp_];
 