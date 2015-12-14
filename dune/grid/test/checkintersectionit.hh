20406ff30cb83a8a4d1d48b4d9081ad71e553a3e
Christian Engwer <christian.engwer@uni-muenster.de>
2015-12-14 19:33:47 +0000

    Merge branch 'feature/make-intersectioniterator-a-forward-iterator' into 'master'
    Feature/make intersectioniterator a forward iterator
    
    In https://gitlab.dune-project.org/flyspray/FS/issues/1496 the second patch got missed.
    This branch now contains all changes needed to use the intersection iterator as a std::forward_iterator.
    
    See merge request !12
    (cherry picked from commit a41364324a3f3e108714f6011fd88b849e8e02db)
    
    Signed-off-by: Steffen Müthing <muething@dune-project.org>

--- dune/grid/test/checkintersectionit.hh
+++ dune/grid/test/checkintersectionit.hh
@@ -5,9 +5,12 @@
 
 #include <cmath>
 
+#include <dune/common/test/iteratortest.hh>
+
 #include <dune/geometry/quadraturerules.hh>
 #include <dune/geometry/referenceelements.hh>
 
+#include "checkiterators.hh"
 #include "checkgeometry.hh"
 
 /** \file
@@ -439,10 +442,12 @@ void checkIntersectionIterator ( const GridViewType &view,
   bool hasBoundaryIntersection = false;
   typename Intersection::GlobalCoordinate sumNormal( ctype( 0 ) );
 
-  // check default-constructibility and copy constructor
-  IntersectionIterator idefault;
-  idefault = view.iend( *eIt );
-  const IntersectionIterator iend = idefault;
+  // check wether intersection iterator is a forward iterator
+  NoopFunctor< Intersection > op;
+  if( 0 != testForwardIterator( view.ibegin( *eIt ), view.iend( *eIt ), op ) )
+    DUNE_THROW( Dune::Exception, "IntersectionIterator does not fulfill the forward iterator concept" );
+
+  const IntersectionIterator iend = view.iend( *eIt );
   for( IntersectionIterator iIt = view.ibegin( *eIt ); iIt != iend; ++iIt )
   {
     const Intersection &intersection = *iIt;
