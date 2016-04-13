0913d626417e8d16e954a594d6d53f65b753f109
Marco Agnese <m.agnese13@imperial.ac.uk>
2016-04-13 15:45:59 +0100

    simplify code with range based for and auto, fix some comments

--- dune/grid/onedgrid/onedgridfactory.cc
+++ dune/grid/onedgrid/onedgridfactory.cc
@@ -55,7 +55,6 @@ insertElement(const GeometryType& type,
   elements_.push_back(std::array<unsigned int,2>());
   elements_.back()[0] = vertices[0];
   elements_.back()[1] = vertices[1];
-
 }
 
 void Dune::GridFactory<Dune::OneDGrid>::
@@ -79,49 +78,31 @@ createGrid()
 {
   // Prevent a crash when this method is called twice in a row
   // You never know who may do this...
-  if (grid_==NULL)
-    return NULL;
+  if (grid_==nullptr)
+    return nullptr;
 
   // Assert that vertices are given
   assert (vertexPositions_.size() > 0);
 
-  // ////////////////////////////////////////////////////////
-  //   Insert the vertices into the grid
-  // ////////////////////////////////////////////////////////
-
+  // Insert the vertices into the grid
   grid_->entityImps_.resize(1);
+  for (const auto& vtx : vertexPositions_)
+  {
+    OneDEntityImp<0> newVertex(0, vtx.first, grid_->getNextFreeId(1));
 
-
-  VertexIterator vIt    = vertexPositions_.begin();
-  VertexIterator vEndIt = vertexPositions_.end();
-
-  for (; vIt!=vEndIt; ++vIt) {
-
-    OneDEntityImp<0> newVertex(0,
-                               vIt->first,
-                               grid_->getNextFreeId(1));
-
-    newVertex.leafIndex_ = vIt->second;
-    newVertex.levelIndex_ = vIt->second;
+    newVertex.leafIndex_ = vtx.second;
+    newVertex.levelIndex_ = vtx.second;
 
     grid_->vertices(0).push_back(newVertex);
-
   }
 
-  // //////////////////////////////////////////////////////////////////
-  //   Make an array with the vertex positions accessible by index
-  //   We'll need that several times.
-  // //////////////////////////////////////////////////////////////////
+  // Make an array with the vertex positions accessible by index
+  // We'll need that several times.
   std::vector<double> vertexPositionsByIndex(vertexPositions_.size());
-  for (std::map<FieldVector<ctype,1>, unsigned int >::iterator it = vertexPositions_.begin();
-       it != vertexPositions_.end();
-       ++it)
-    vertexPositionsByIndex[it->second] = it->first;
+  for (const auto& vtx : vertexPositions_)
+    vertexPositionsByIndex[vtx.second] = vtx.first;
 
-  // ///////////////////////////////////////////////////
-  //   Set the numbering of the boundary segments
-  // ///////////////////////////////////////////////////
-
+  // Set the numbering of the boundary segments
   if (boundarySegments_.size() > 2)
     DUNE_THROW(GridError, "You cannot provide more than two boundary segments to a OneDGrid (it must be connected).");
 
@@ -137,23 +118,19 @@ createGrid()
   // The only thing of interest are the indices.
   // ///////////////////////////////////////////////////////////////////
 
-  // first sort elements by increasing position.  That is how they are expected in the grid data structure
+  // First sort elements by increasing position. That is how they are expected in the grid data structure
   std::map<double, std::pair<std::array<unsigned int, 2>, unsigned int> > elementsByPosition;
-
-  for (size_t i=0; i<elements_.size(); i++)
+  for (std::size_t i=0; i<elements_.size(); i++)
     elementsByPosition.insert(std::make_pair(vertexPositionsByIndex[elements_[i][0]],     // order by position of left vertex
                                              std::make_pair(elements_[i], i)      // the element and its position in the insertion sequence
                                              ));
 
+  auto it = std::get<0>(grid_->entityImps_[0]).begin();
+  auto eIt = elementsByPosition.begin();
 
-  OneDGridList<OneDEntityImp<0> >::iterator it = Dune::get<0>(grid_->entityImps_[0]).begin();
-  std::map<double, std::pair<std::array<unsigned int, 2>, unsigned int> >::iterator eIt = elementsByPosition.begin();
-
-  /** \todo Looping over the vertices to get all elements assumes that
-      the grid is connected.
-   */
-  for (size_t i=0; i<vertexPositions_.size()-1; i++, ++eIt) {
-
+  // Looping over the vertices to get all elements assumes that the grid is connected
+  for (size_t i=0; i<vertexPositions_.size()-1; i++, ++eIt)
+  {
     OneDEntityImp<1> newElement(0, grid_->getNextFreeId(0), grid_->reversedBoundarySegmentNumbering_);
     newElement.vertex_[0] = it;
     it = it->succ_;
@@ -163,25 +140,18 @@ createGrid()
     newElement.leafIndex_  = eIt->second.second;
 
     grid_->elements(0).push_back(newElement);
-
   }
 
-  // ///////////////////////////////////////////////////
-  //   Create the index sets
-  // ///////////////////////////////////////////////////
-
+  // Create the index sets
   grid_->levelIndexSets_.resize(1);
   grid_->levelIndexSets_[0] = new OneDGridLevelIndexSet<const OneDGrid>(*grid_, 0);
   grid_->levelIndexSets_[0]->setSizesAndTypes(vertexPositions_.size(), elements_.size());
 
   grid_->leafIndexSet_.setSizesAndTypes(vertexPositions_.size(), elements_.size());
 
-  // ///////////////////////////////////////////////////
-  // hand over the grid and delete the member pointer
-  // ///////////////////////////////////////////////////
-
-  Dune::OneDGrid* tmp = grid_;
-  grid_ = NULL;
+  // Hand over the grid and delete the member pointer
+  auto tmp = grid_;
+  grid_ = nullptr;
   return tmp;
 }
 
@@ -189,5 +159,4 @@ void Dune::GridFactory<Dune::OneDGrid >::
 createBegin()
 {
   vertexPositions_.clear();
-
 }
