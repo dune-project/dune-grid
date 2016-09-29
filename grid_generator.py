99c4eb24b577d2c17b437a4e9c5b5c59f439fe90
Martin Nolte <nolte@mathematik.uni-freiburg.de>
2016-09-29 16:19:21 +0200

    add convenience function writeVTK to grid views

--- grid_generator.py
+++ grid_generator.py
@@ -12,18 +12,38 @@ from ..generator.generator import SimpleGenerator
 from types import ModuleType
 
 
-def triangulation(self):
-    if self.dimGrid != 2 or self.dimWorld != 2:
+def triangulation(grid):
+    if grid.dimGrid != 2 or grid.dimWorld != 2:
         raise Exception("Grid must be 2-dimensional for use as matplotlib triangulation.")
     from matplotlib.tri import Triangulation
-    x = self.coordinates()
-    triangles = self.tesselate()
+    x = grid.coordinates()
+    triangles = grid.tesselate()
     return Triangulation(x[:,0], x[:,1], triangles)
+
+
+def writeVTK(grid, name, celldata=[], pointdata=[], cellvector=[], pointvector=[], number=None, subsampling=None):
+    vtk = grid.vtkWriter() if subsampling is None else grid.vtkWriter(subsampling)
+
+    for f in celldata:
+        f.addToVTKWriter(vtk, common.DataType.CellData)
+    for f in pointdata:
+        f.addToVTKWriter(vtk, common.DataType.PointData)
+    for f in cellvector:
+        f.addToVTKWriter(vtk, vtk.CellVectorData)
+    for f in pointvector:
+        f.addToVTKWriter(vtk, vtk.PointVectorData)
+
+    if number is None:
+        vtk.write(name)
+    else:
+        vtk.write(name, number)
+    return vtk
 
 
 def addAttr(module, cls):
     setattr(cls, "_module", module)
     setattr(cls, "triangulation", triangulation)
+    setattr(cls, "writeVTK", writeVTK)
 
 
 generator = SimpleGenerator("Grid", "Dune::CorePy", "LeafGrid")
