dd7fca4b7bb1a7d6d9944d8344eac61a4bc38e0d
Robert K <robertk@posteo.org>
2016-10-13 13:49:24 +0200

    [feature][polygon/polyhedron] Added blocks for polygon and polyhedron.

---
+++ dune/grid/io/file/dgfparser/blocks/polygon.hh
@@ -0,0 +1,56 @@
+// -*- mode: C++; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*-
+// vi: set et ts=2 sw=2 sts=2:
+#ifndef DUNE_POLYGON_HH
+#define DUNE_POLYGON_HH
+
+#include <iostream>
+#include <algorithm>
+#include <numeric>
+#include <vector>
+
+#include <dune/common/typetraits.hh>
+#include <dune/grid/io/file/dgfparser/dgfparser.hh>
+
+namespace Dune
+{
+
+  namespace dgf
+  {
+
+    // PolygonBlock
+    // ------------
+
+    struct PolygonBlock
+      : public BasicBlock
+    {
+      PolygonBlock ( std::istream &in, int numVtx, int vtxOfs )
+        : BasicBlock( in, "Polygon" ), vtxBegin_( vtxOfs ), vtxEnd_( vtxOfs + numVtx )
+      {}
+
+      int get ( std::vector< std::vector< int > > &polygons )
+      {
+        reset();
+        std::vector< int > polygon;
+        while( getnextline() )
+        {
+          polygon.clear();
+          for( int vtxIdx; getnextentry( vtxIdx ); )
+          {
+            if( (vtxBegin_ > vtxIdx) || (vtxIdx >= vtxEnd_) )
+              DUNE_THROW( DGFException, "Error in " << *this << ": Invalid vertex index (" << vtxIdx << " not int [" << vtxBegin_ << ", " << vtxEnd_ << "[)" );
+            polygon.push_back( vtxIdx - vtxBegin_ );
+          }
+
+          polygons.push_back( polygon );
+        }
+        return polygons.size();
+      }
+
+    private:
+      int vtxBegin_, vtxEnd_;
+    };
+
+  } // namespace dgf
+} // end namespace Dune
+
+#endif // #ifndef DUNE_POLYGON_HH
---
+++ dune/grid/io/file/dgfparser/blocks/polyhedron.hh
@@ -0,0 +1,67 @@
+// -*- mode: C++; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*-
+// vi: set et ts=2 sw=2 sts=2:
+#ifndef DUNE_POLYHEDRON_HH
+#define DUNE_POLYHEDRON_HH
+
+#include <dune/grid/io/file/dgfparser/blocks/polygon.hh>
+
+namespace Dune
+{
+
+  namespace dgf
+  {
+
+    // PolyhedronBlock
+    // ---------------
+
+    struct PolyhedronBlock
+      : public BasicBlock
+    {
+      explicit PolyhedronBlock ( std::istream &in, int numPolys )
+        : BasicBlock( in, "Polyhedron" ), numPolys_( numPolys )
+      {}
+
+      int get ( std::vector< std::vector< int > > &polyhedra )
+      {
+        reset();
+        std::vector< int > polyhedron;
+        int minPolyId = 1;
+        while( getnextline() )
+        {
+          polyhedron.clear();
+          for( int polyIdx; getnextentry( polyIdx ); )
+          {
+            if( (polyIdx < 0) || (polyIdx > numPolys_) )
+              DUNE_THROW( DGFException, "Error in " << *this << ": Invalid polygon index (" << polyIdx << " not int [0, " << numPolys_ << "])" );
+
+            minPolyId = std::min( minPolyId, polyIdx );
+            polyhedron.push_back( polyIdx );
+          }
+
+          polyhedra.push_back( polyhedron );
+        }
+
+        // substract minimal number to have 0 starting numbering
+        if( minPolyId > 0 )
+        {
+          const size_t polySize = polyhedra.size();
+          for( size_t i=0; i<polySize; ++i )
+          {
+            const size_t pSize = polyhedra[ i ].size();
+            for( size_t j=0; j<pSize; ++j )
+            {
+              polyhedra[ i ][ j ] -= minPolyId;
+            }
+          }
+        }
+        return polyhedra.size();
+      }
+
+    private:
+      const int numPolys_;
+    };
+
+  } // namespace dgf
+} // end namespace Dune
+
+#endif // #ifndef DUNE_POLYHEDRON_HH
