00bbd5e47ab7853144cee01436a038875d689140
Christoph Grüninger <gruenich@dune-project.org>
2017-03-08 09:23:41 +0000

    Merge branch 'bugfix/gridinfo' into 'master'
    gridinfo.hh: Use `subEntities(codim)` method instead of removed `count<codim>()` template method
    
    See merge request !157

---
+++ dune/grid/test/test-gridinfo.cc
@@ -0,0 +1,36 @@
+#include "config.h"
+
+#include <dune/common/parallel/mpihelper.hh>
+
+#include <dune/grid/common/gridinfo.hh>
+#include <dune/grid/yaspgrid.hh>
+
+template<typename Grid>
+bool test_gridinfo(const Grid& grid)
+{
+  Dune::gridinfo(grid);
+  for (int level = 0; level <= grid.maxLevel(); ++level)
+    Dune::gridlevellist(grid, level, "gridlevellist");
+  Dune::gridleaflist(grid, "gridleaflist");
+
+  return true;
+}
+
+int main(int argc, char** argv)
+{
+  Dune::MPIHelper::instance(argc, argv);
+
+  {
+    Dune::YaspGrid<2> grid({1., 1.}, {4, 4});
+    grid.globalRefine(2);
+    test_gridinfo(grid);
+  }
+
+  {
+    Dune::YaspGrid<3> grid({1., 1., 1.}, {4, 4, 4});
+    grid.globalRefine(2);
+    test_gridinfo(grid);
+  }
+
+  return 0;
+}
