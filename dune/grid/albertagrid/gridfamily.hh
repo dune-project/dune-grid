0444af1ab6bebe973e38c02c3db11e6ce4f81058
Oliver Sander <oliver.sander@tu-dresden.de>
2017-03-19 22:03:12 +0100

    Remove obsolete inclusions of entitypointer.hh from within AlbertaGrid
    The file was still included, but it wasn't actually used.

--- dune/grid/albertagrid/gridfamily.hh
+++ dune/grid/albertagrid/gridfamily.hh
@@ -11,7 +11,6 @@
 #include <dune/common/parallel/collectivecommunication.hh>
 
 #include <dune/grid/common/entity.hh>
-#include <dune/grid/common/entitypointer.hh>
 #include <dune/grid/common/geometry.hh>
 #include <dune/grid/common/intersection.hh>
 #include <dune/grid/common/intersectioniterator.hh>
