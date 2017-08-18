7a47bb4aca51da407485c21abbba06cadddf95cf
Ansgar Burchardt <Ansgar.Burchardt@tu-dresden.de>
2017-08-18 13:55:57 +0200

    UseUG.cmake: use `UG_DEFINITIONS` variable
    It is already used in `dune_register_package_flags` and the compiler
    definitions should be the same there and in `dune_add_flags`.

--- cmake/modules/UseUG.cmake
+++ cmake/modules/UseUG.cmake
@@ -63,13 +63,7 @@ function(add_dune_ug_flags)
     include_directories(${UG_INCLUDES})
     set_property(${_prefix} ${ADD_UG_UNPARSED_ARGUMENTS}
       APPEND PROPERTY
-      COMPILE_DEFINITIONS ENABLE_UG)
-    if (UG_VERSION VERSION_GREATER 3.13.0
-        OR UG_VERSION VERSION_EQUAL 3.13.0)
-      set_property(${_prefix} ${ADD_UG_UNPARSED_ARGUMENTS}
-        APPEND PROPERTY
-        COMPILE_DEFINITIONS UG_USE_NEW_DIMENSION_DEFINES)
-    endif()
+      COMPILE_DEFINITIONS ${UG_DEFINITIONS})
     # Add linker arguments
     if(NOT (ADD_UG_SOURCE_ONLY OR ADD_UG_OBJECT))
       if(NOT ADD_UG_NO_LINK_DUNEGRID)
@@ -82,10 +76,6 @@ function(add_dune_ug_flags)
         LINK_LIBRARIES ${UG_LIBRARIES} ${DUNE_LIBS})
     endif(NOT (ADD_UG_SOURCE_ONLY OR ADD_UG_OBJECT))
     if(UG_PARALLEL STREQUAL "yes")
-      # Add modelp
-      set_property(${_prefix} ${ADD_UG_UNPARSED_ARGUMENTS}
-        APPEND PROPERTY
-        COMPILE_DEFINITIONS ModelP)
       # Add mpi flags.
       add_dune_mpi_flags(${ADD_UG_UNPARSED_ARGUMENTS} ${_source_only} ${_object})
     endif()
