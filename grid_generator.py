25e650ba0c96b58beb5fd5f32711d7533298fd5e
dedner <a.s.dedner@warwick.ac.uk>
2017-10-16 10:57:37 +0100

    add a convenience method to the grid to obtain an MCMGMapper

--- grid_generator.py
+++ grid_generator.py
@@ -67,6 +67,10 @@ def plot(self, function=None, *args, **kwargs):
         except AttributeError:
             dune.plotting.plotPointData(solution=self.function(function),*args,**kwargs)
 
+def mapper(self,layout):
+    from dune.grid.map import MultipleCodimMultipleGeomTypeMapper as Mapper
+    return Mapper(self,layout)
+
 @deprecated
 def globalGridFunction(gv, evaluator):
     return gv.function(evaluator)
@@ -79,6 +83,7 @@ def addAttr(module, cls):
     setattr(cls, "triangulation", triangulation)
     setattr(cls, "writeVTK", writeVTK)
     setattr(cls, "plot", plot)
+    setattr(cls, "mapper", mapper)
     setattr(cls, "globalGridFunction", globalGridFunction)
     setattr(cls, "localGridFunction", localGridFunction)
 
