a4698316c8dcf596b11aa99ba619d10f2a76e2a0
dedner <a.s.dedner@warwick.ac.uk>
2017-10-28 17:15:26 +0100

    added simplified communicate method on exported mapper

--- mapper.hh
+++ mapper.hh
@@ -8,6 +8,10 @@
 #include <dune/common/visibility.hh>
 #include <dune/grid/common/mcmgmapper.hh>
 
+#include <dune/python/grid/commops.hh>
+#include <dune/python/grid/range.hh>
+#include <dune/python/utility/numpycommdatahandle.hh>
+
 #include <dune/python/common/typeregistry.hh>
 #include <dune/python/pybind11/pybind11.h>
 #include <dune/python/pybind11/functional.h>
@@ -53,23 +57,49 @@ namespace Dune
 
         cls.def( "subIndex", [] ( const Mapper &self, const Entity &entity, int i, int c ) {
             return detail::mapperSubIndex( self, entity, i, c );
-          } );
-        cls.def( "subIndex", [] ( const Mapper &self, const Entity &entity, std::tuple< int, int > e ) {
-            return detail::mapperSubIndex( self, entity, std::get< 0 >( e ), std::get< 1 >( e ) );
-          } );
+          }, "entity"_a, "i"_a, "c"_a,
+          R"doc(
+            Args:
+                entity:     an entity of the grid
+                i:          number of subentity of `entity`
+                c:          codimension of the subentity
 
-        cls.def( "subIndices", [] ( const Mapper &self, const Entity &entity, int c ) {
-            if( (c < Entity::codimension) || (c > Entity::dimension) )
-              throw pybind11::value_error( "Invalid codimension: " + std::to_string( c ) + " (must be in [" + std::to_string( Entity::codimension ) + ", " + std::to_string( Entity::dimension ) + "])" );
-            const int size = entity.subEntities( c );
+            Returns: the starting index of the dofs attached to the `ith` subentity
+                      of codimension `c` of the given entity.
+          )doc" );
+
+        cls.def( "subIndex", [] ( const Mapper &self, const Entity &entity, std::tuple< int, int > subEntity ) {
+            return detail::mapperSubIndex( self, entity, std::get< 0 >( subEntity ), std::get< 1 >( subEntity ) );
+          }, "entity"_a, "subEntity"_a,
+          R"doc(
+            Args:
+                entity:     an entity of the grid
+                (i,c):      number and codimension of a subentity of `entity`
+
+            Returns: the starting index of the dofs attached to the `ith` subentity
+                      of codimension `c` of the given entity.
+          )doc" );
+
+        cls.def( "subIndices", [] ( const Mapper &self, const Entity &entity, int codim ) {
+            if( (codim < Entity::codimension) || (codim > Entity::dimension) )
+              throw pybind11::value_error( "Invalid codimension: " + std::to_string( codim ) + " (must be in [" + std::to_string( Entity::codimension ) + ", " + std::to_string( Entity::dimension ) + "])" );
+            const int size = entity.subEntities( codim );
             pybind11::tuple subIndices( size );
             for( int i = 0; i < size; ++i )
             {
               typename Mapper::Index index;
-              subIndices[ i ] = (self.contains( entity, i, c, index ) ? pybind11::cast( index ) : pybind11::none());
+              subIndices[ i ] = (self.contains( entity, i, codim, index ) ? pybind11::cast( index ) : pybind11::none());
             }
             return subIndices;
-          }, "entity"_a, "codim"_a );
+          }, "entity"_a, "codim"_a,
+          R"doc(
+            Args:
+                entity:     an entity of the grid
+                codim:      codimension of a subentity of `entity`
+
+            Returns: indices of dofs attached to all subentity
+                     of codimension `codim` of the given entity.
+          )doc" );
       }
 
       template< class Entity, class Mapper, class... options >
@@ -82,9 +112,85 @@ namespace Dune
         registerMapperSubIndex< Entity >( cls, PriorityTag< 42 >() );
       }
 
+      template <class Mapper>
+      void mapperCommunicate(const Mapper &mapper, CommOp commOp,
+                             std::vector<pybind11::array_t<double>> data,
+                             InterfaceType iftype, CommunicationDirection dir )
+      {
+        switch (commOp)
+        {
+        case add:
+          {
+          auto dataHandle = numPyCommDataHandle( mapper, data,
+              [] (double local, double remote) {return remote+local;});
+          mapper.gridView().communicate( dataHandle, iftype, dir );
+          }
+        case set:
+          {
+          auto dataHandle = numPyCommDataHandle( mapper, data,
+              [] (double local, double remote) {return remote;});
+          mapper.gridView().communicate( dataHandle, iftype, dir );
+          }
+        }
+      }
     } // namespace detail
 
-
+    // registerMapperCommunicate
+    // -------------------------
+    template < Dune::PartitionIteratorType fromType,
+               Dune::PartitionIteratorType toType,
+               Dune::InterfaceType interfaceType,
+               class Mapper >
+    inline static void registerMapperCommunicate(pybind11::class_<Mapper> cls)
+    {
+      using pybind11::operator""_a;
+      typedef typename Mapper::GridView GridView;
+      cls.def( "communicate",
+        [] ( const Mapper &self,
+          GridViewPartition< GridView, fromType > from,
+          GridViewPartition< GridView, toType > to,
+          detail::CommOp commOp, pybind11::args args ) {
+            std::vector<pybind11::array_t<double>> data(args.size());
+            for (int i=0;i<args.size();++i)
+              data[i] = pybind11::array_t<double>(args[i]);
+            mapperCommunicate(self,commOp, data, interfaceType, ForwardCommunication);
+          }, "from"_a, "to"_a, "commOp"_a);
+      cls.def( "communicate",
+        [] ( const Mapper &self,
+          GridViewPartition< GridView, fromType > from,
+          GridViewPartition< GridView, toType > to,
+          pybind11::function operation, pybind11::args args ) {
+            std::vector<pybind11::array_t<double>> data(args.size());
+            for (int i=0;i<args.size();++i)
+              data[i] = pybind11::array_t<double>(args[i]);
+            auto dataHandle = numPyCommDataHandle( self, data, operation.template cast<std::function<double(double,double)>>());
+            self.gridView().communicate( dataHandle, interfaceType, ForwardCommunication);
+          }, "from"_a, "to"_a, "operation"_a );
+      if (fromType != toType)
+      {
+        cls.def( "communicate",
+          [] ( const Mapper &self,
+            GridViewPartition< GridView, fromType > from,
+            GridViewPartition< GridView, toType > to,
+            detail::CommOp commOp, pybind11::args args ) {
+              std::vector<pybind11::array_t<double>> data(args.size());
+              for (int i=0;i<args.size();++i)
+                data[i] = pybind11::array_t<double>(args[i]);
+              mapperCommunicate(self,commOp, data, interfaceType, BackwardCommunication);
+            }, "from"_a, "to"_a, "commOp"_a );
+        cls.def( "communicate",
+          [] ( const Mapper &self,
+            GridViewPartition< GridView, fromType > from,
+            GridViewPartition< GridView, toType > to,
+            pybind11::function operation, pybind11::args args ) {
+              std::vector<pybind11::array_t<double>> data(args.size());
+              for (int i=0;i<args.size();++i)
+                data[i] = pybind11::array_t<double>(args[i]);
+              auto dataHandle = numPyCommDataHandle( self, data, operation.template cast<std::function<double(double,double)>>());
+              self.gridView().communicate( dataHandle, interfaceType, BackwardCommunication);
+            }, "from"_a, "to"_a, "operation"_a );
+      }
+    }
 
     // registerMapper
     // --------------
@@ -97,15 +203,31 @@ namespace Dune
       pybind11::options opts;
       opts.disable_function_signatures();
 
-      cls.def( "__len__", [] ( const Mapper &self ) { return self.size(); } );
-      cls.def_property_readonly( "size", [] ( const Mapper &self ) { return self.size(); } );
+      cls.def( "__len__", [] ( const Mapper &self ) { return self.size(); },
+        R"doc(
+          Returns the maximum index plus 1 returns by the mapper.
+          Use to set the number of entries in the dof vector.
+
+          Note: corresponds to the `size()` method on the MCMGMapper.
+        )doc" );
+      cls.def_property_readonly( "size", [] ( const Mapper &self ) { return self.size(); },
+        R"doc(
+          Returns the maximum index plus 1 returns by the mapper.
+          Use to set the number of entries in the dof vector.
+        )doc" );
 
       cls.def( "types", [] ( const Mapper &self, int codim ) {
           pybind11::list types;
           for( GeometryType type : self.types( codim ) )
             types.append( pybind11::cast( type ) );
           return types;
-        }, "codim"_a );
+        }, "codim"_a,
+        R"doc(
+          Args:
+              codim:      codimension
+
+          Returns: list of geometry types of the given codimension with attached data
+        )doc" );
 
       Hybrid::forEach( std::make_integer_sequence< int, GridView::dimension+1 >(), [ &cls ] ( auto &&codim ) {
           typedef typename GridView::template Codim< codim >::Entity Entity;
@@ -118,15 +240,34 @@ namespace Dune
                 return pybind11::cast( index );
               else
                 return pybind11::none();
-            }, "entity"_a );
-          cls.def( "contains", [] ( const Mapper &self, const Entity &e ) {
+            }, "entity"_a,
+            R"doc(
+              Args:
+                  entity:      entity in the grid
+
+              Returns:  the first index for dofs stored on the given entity
+                        if no dofs are stored on this entity the return value is `None`
+            )doc" );
+          cls.def( "contains", [] ( const Mapper &self, const Entity &entity ) {
               typename Mapper::Index index;
-              return self.contains( e, index );
-            }, "entity"_a );
+              return self.contains( entity, index );
+            }, "entity"_a,
+            R"doc(
+              Args:
+                  entity:      entity in the grid
+
+              Returns:  `True` if the data is stored on this entity
+            )doc" );
           detail::registerMapperSubIndex< Entity >( cls );
         } );
 
-      cls.def( "update", [] ( Mapper &self ) { self.update(); } );
+      cls.def( "update", [] ( Mapper &self ) { self.update(); },
+        R"doc(
+          Update the mapper after a grid modification.
+
+          Note: after grid modification the mapper is not valid anymore and needs to
+                be updated!
+        )doc" );
 
       cls.def( "__call__", [] ( const Mapper &self, const typename GridView::template Codim< 0 >::Entity &element ) {
           // need a cache gt(cdim=0) -> nof indices then we could store directly in retArray
@@ -143,7 +284,51 @@ namespace Dune
           for ( auto i : indices )
             y[idx++] = i;
           return retArray;
-        }, "entity"_a );
+        }, "element"_a,
+        R"doc(
+          Args:
+              element:      codim zero entity in the grid
+
+          Returns: list of all degrees of freedom attached to the given and
+                   all subentities of the given entity.
+        )doc" );
+
+
+      registerMapperCommunicate<InteriorBorder_Partition,InteriorBorder_Partition,
+          InteriorBorder_InteriorBorder_Interface>(cls);
+      registerMapperCommunicate<InteriorBorder_Partition,All_Partition,
+          InteriorBorder_All_Interface>(cls);
+      registerMapperCommunicate<Overlap_Partition,OverlapFront_Partition,
+          Overlap_OverlapFront_Interface>(cls);
+      registerMapperCommunicate<Overlap_Partition,All_Partition,
+          Overlap_All_Interface>(cls);
+      registerMapperCommunicate<All_Partition,All_Partition,
+          All_All_Interface>(cls);
+      cls.def( "communicate",
+        [] ( const Mapper &self,
+          pybind11::object from, pybind11::object to, pybind11::object, pybind11::args args ) {
+            throw pybind11::value_error("Combination of 'to' and 'from' partition not legal");
+          }, "from"_a, "to"_a, "commOp"_a,
+        R"doc(
+          Communicate data stored in `numpy` arrays attached using the given mapper
+
+          Args:
+              from:       partition used on sending processor
+              to:         partition used on receiving processor
+              commOp:     function (local,remote)->result defining how to combine received with locally stored data on receiving process
+              *args:      data vectors to communicate
+
+          Returns: None
+
+          Note:
+              - At the moment only double numpy vectors are supported
+              - Not all combinations of `from` and `to` partitions are valid.
+                Possible combinations are
+                - from=interiorBorder and to=interiorBorder|all
+                - from=overlap and to=overlapFront|all
+                - from=overlapFront and to=overlap
+                - from=all and to=interiorBorder|overlap|all,
+        )doc" );
     }
 
     // registerMultipleCodimMultipleGeomTypeMapper
