9b16133734a7d89a381eade3720586c7ff697af1
Oliver Sander <oliver.sander@tu-dresden.de>
2018-08-10 10:53:46 +0200

    Add constructor for YaspGrid with TensorProductCoordinates object

--- dune/grid/test/yasp/test-yaspgrid-yaspfactory-1d.cc
+++ dune/grid/test/yasp/test-yaspgrid-yaspfactory-1d.cc
@@ -33,6 +33,8 @@ int main (int argc , char **argv) {
                YaspFactory<1,Dune::EquidistantCoordinates<double,1> >::buildGrid(true,0,false,true));
     check_yasp(testID + "equidistantoffset-generic-constructor",
                YaspFactory<1,Dune::EquidistantOffsetCoordinates<double,1> >::buildGrid(true,0,false,true));
+    check_yasp(testID + "tensor-generic-constructor",
+               YaspFactory<1,Dune::TensorProductCoordinates<double,1> >::buildGrid(true,0,false,true));
 
   } catch (Dune::Exception &e) {
     std::cerr << e << std::endl;
