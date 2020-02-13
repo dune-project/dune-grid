2a406ca8d81941aae1e220c85b25c1aeb6e1756d
Linus Seelinger <S.Linus@gmx.de>
2020-02-13 15:05:06 +0100

    Work on grid iterator recipe

--- doc/recipes/recipe-iterate-over-grid.cc
+++ doc/recipes/recipe-iterate-over-grid.cc
@@ -6,9 +6,13 @@
  * on it. All DUNE grids provide a unified interface for this and related operations.
  *
  * First, we set up a simple structured grid.
+ * \snippet recipe-iterate-over-grid.cc set up grid
  *
- * Then, we iterate over it
+ * Then, we iterate over it various types of grid entities
  * \snippet recipe-iterate-over-grid.cc iterate over grid view
+ *
+ * Alternatively, we can specify the type of element by codimension
+ * \snippet recipe-iterate-over-grid.cc iterate over codim
  *
  * Full example code: @ref recipe-iterate-over-grid.cc
  * \example recipe-iterate-over-grid.cc
@@ -38,22 +42,15 @@ int main(int argc, char** argv)
     // Maybe initialize Mpi
     Dune::MPIHelper& helper = Dune::MPIHelper::instance(argc, argv);
 
-    // the simplest way to create a grid
+    // [set up grid]
     const int dim = 4;
     using Grid = Dune::YaspGrid<dim>;
-    Dune::FieldVector<double,dim> len; for (auto& l:len) l=1.0;
+    Dune::FieldVector<double,dim> len; for (auto& l : len) l=1.0;
     std::array<int,dim> cells; for (auto& c : cells) c=4;
     Grid grid(len,cells);
 
-    // refine and extract grid view
-    std::cout << "refining grid" << std::endl;
-    grid.globalRefine(1);
     auto gv = grid.leafGridView();
-
-    std::cout << "iterating over codim" << std::endl;
-    const int codim = 2;
-    for (const auto& e : entities(gv,Dune::Codim<codim>{}))
-      if (!e.type().isCube()) std::cout << "not a cube" << std::endl;
+    //! [set up grid]
 
     // [iterate over grid view]
     for (const auto& e : elements(gv)); // codim=0
@@ -61,6 +58,12 @@ int main(int argc, char** argv)
     for (const auto& e : edges(gv));    // codim=dim-1
     for (const auto& e : facets(gv));   // codim=1
     //! [iterate over grid view]
+
+    // [iterate over codim]
+    const int codim = 2;
+    for (const auto& e : entities(gv,Dune::Codim<codim>{}))
+      if (!e.type().isCube()) std::cout << "not a cube" << std::endl;
+    //! [iterate over codim]
 
     // access subentites
     std::cout << "iterating over subentities " << std::endl;
