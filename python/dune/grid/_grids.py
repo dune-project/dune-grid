b7984d9de1e1cb08f367d4e7f2fabd5e1434a8a4
dedner <a.s.dedner@warwick.ac.uk>
2020-03-30 16:44:51 +0100

    remove python 2 support code

--- python/dune/grid/_grids.py
+++ python/dune/grid/_grids.py
@@ -1,5 +1,3 @@
-from __future__ import absolute_import, division, print_function, unicode_literals
-
 from dune.common.checkconfiguration import assertHave, ConfigurationError
 from dune.typeregistry import generateTypeName
 
--- python/dune/grid/grid_generator.py
+++ python/dune/grid/grid_generator.py
@@ -1,6 +1,3 @@
-from __future__ import absolute_import, division, print_function, unicode_literals
-__metaclass__ = type
-
 import os, inspect
 from ..generator.generator import SimpleGenerator
 from dune.common.hashit import hashIt
