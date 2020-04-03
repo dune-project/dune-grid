b60d83c3161b1c03ec80e9fee09d411b97fc838c
dedner <a.s.dedner@warwick.ac.uk>
2020-04-03 12:43:44 +0100

    split test into three to run in parallel

---
+++ dune/python/test/test_gf1.py
@@ -0,0 +1,57 @@
+import numpy
+from dune.grid import gridFunction, structuredGrid
+
+fcalls = 0
+gcalls = 0
+def testGF_first(gridView):
+    global fcalls
+    global gcalls
+    @gridFunction(gridView)
+    def f(x):
+        global fcalls
+        fcalls += 1
+        return x[0]*x[1]
+    @gridFunction(gridView)
+    def g(e,x):
+        global gcalls
+        gcalls += 1
+        return e.geometry.toGlobal(x)
+
+    fcalls = 0
+    gcalls = 0
+    e = gridView.elements.__next__()
+    xLoc = numpy.array([[0,0.1,0.2,0.3],[0,0.4,0.6,0.8]])
+    xGlb = e.geometry.toGlobal(xLoc)
+
+    fcalls = 0
+    gcalls = 0
+    y=f(xGlb)
+    # print( y, fcalls)
+    assert fcalls == 1
+    y = g(e, xLoc)
+    # print( y, gcalls)
+    assert gcalls == 1
+
+    fcalls = 0
+    y = f(e,xLoc)
+    # print( y, fcalls)
+    assert fcalls == 1
+
+    fcalls = 0
+    gcalls = 0
+    lf = f.localFunction()
+    lg = g.localFunction()
+    lf.bind(e)
+    lg.bind(e)
+    y = lf(xLoc)
+    # print( y, fcalls)
+    assert fcalls == 1
+    y = lg(xLoc)
+    # print( y, gcalls)
+    assert gcalls == 1
+    lg.unbind()
+    lf.unbind()
+
+if __name__ == "__main__":
+    gridView = structuredGrid([0,0],[1,1],[10,10])
+    testGF_first(gridView)
