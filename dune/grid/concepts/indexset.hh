c168e9d0779def7a40b135c42fde3ef07fc92217
Santiago Ospina De Los Rios <sospinar@gmail.com>
2020-05-21 02:58:29 +0200

    Add IndexSet C++20 concept

--- dune/grid/concepts/indexset.hh
+++ dune/grid/concepts/indexset.hh
@@ -8,9 +8,37 @@
 
 #include <dune/common/concept.hh>
 
+#if DUNE_HAVE_CXX_CONCEPTS
+#include <dune/common/std/concepts.hh>
+#endif
+
 namespace Dune {
   namespace Concept
   {
+
+
+#if DUNE_HAVE_CXX_CONCEPTS
+namespace Concept{
+
+    template<class IS, int codim>
+    concept IndexSetEntityCodim = requires(IS is, int i, unsigned int sub_codim, const typename IS::template Codim<codim>::Entity& entity)
+    {
+      requires Entity<typename IS::template Codim<codim>::Entity>;
+      { is.template index<codim>(entity)                    } -> Std::convertible_to<typename IS::IndexType   >;
+      { is.index(entity)                                    } -> Std::convertible_to<typename IS::IndexType   >;
+      { is.template subIndex<codim>(entity, i, sub_codim )  } -> Std::convertible_to<typename IS::IndexType   >;
+      { is.subIndex(entity, i, sub_codim )                  } -> Std::convertible_to<typename IS::IndexType   >;
+      { is.contains(entity)                                 } -> Std::convertible_to<bool                     >;
+    };
+
+    template<class E, int codim = E::dimension>
+    struct is_index_set_codim : std::conjunction<std::bool_constant<IndexSetEntityCodim<E,codim>>,is_index_set_codim<E,codim-1>> {};
+
+    // Stop recursion
+    template<class E>
+    struct is_index_set_codim<E,0> : std::bool_constant<IndexSetEntityCodim<E,0>> {};
+}
+#endif
 
     template<int codim>
     struct IndexSetEntityCodim : public Refines<IndexSetEntityCodim<codim-1>>
@@ -18,32 +46,50 @@ namespace Dune {
       template<class IS>
       auto require(IS&& is) -> decltype(
         requireConcept<Dune::Concept::Entity,typename IS::template Codim<codim>::Entity>(),
-        requireConvertible<typename IS::IndexType>(is.template index<codim>(/*entity*/ std::declval<const typename IS::template Codim<codim>::Entity&>())),
-        requireConvertible<typename IS::IndexType>(is.index(/*entity*/ std::declval<const typename IS::template Codim<codim>::Entity&>())),
-        requireConvertible<typename IS::IndexType>(is.template subIndex<codim>(/*entity*/ std::declval<const typename IS::template Codim<codim>::Entity&>(), /*i*/ int{}, /*codim*/ (unsigned int){} )),
-        requireConvertible<typename IS::IndexType>(is.subIndex(/*entity*/ std::declval<const typename IS::template Codim<codim>::Entity&>(), /*i*/ int{}, /*codim*/ (unsigned int){} )),
-        requireConvertible<bool>(is.contains(/*entity*/ std::declval<const typename IS::template Codim<codim>::Entity&>()))
+        requireConvertible<typename IS::IndexType   >( is.template index<codim>(/*entity*/ std::declval<const typename IS::template Codim<codim>::Entity&>())                                                 ),
+        requireConvertible<typename IS::IndexType   >( is.index(/*entity*/ std::declval<const typename IS::template Codim<codim>::Entity&>())                                                                 ),
+        requireConvertible<typename IS::IndexType   >( is.template subIndex<codim>(/*entity*/ std::declval<const typename IS::template Codim<codim>::Entity&>(), /*i*/ int{}, /*sub_codim*/ (unsigned int){} )    ),
+        requireConvertible<typename IS::IndexType   >( is.subIndex(/*entity*/ std::declval<const typename IS::template Codim<codim>::Entity&>(), /*i*/ int{}, /*sub_codim*/ (unsigned int){} )                    ),
+        requireConvertible<bool                     >( is.contains(/*entity*/ std::declval<const typename IS::template Codim<codim>::Entity&>())                                                              )
       );
     };
 
-    // stop recursion
+    // Stop recursion
     template<>
     struct IndexSetEntityCodim<-1> : public AnyType {};
+
+#if DUNE_HAVE_CXX_CONCEPTS
+namespace Concept{
+
+    template<class IS>
+    concept IndexSet = requires(IS is, Dune::GeometryType type, int sub_codim)
+    {
+      { IS::dimension       } -> Std::convertible_to< int                     >;
+      { is.types(sub_codim) } -> Std::convertible_to< typename IS::Types      >;
+      { is.size(type)       } -> Std::convertible_to< typename IS::IndexType  >;
+      { is.size(sub_codim)  } -> Std::convertible_to< typename IS::IndexType  >;
+      requires std::is_integral<typename IS::IndexType>::value;
+      requires (not std::is_copy_constructible<IS>::value);
+      requires (not std::is_copy_assignable<IS>::value);
+      typename IS::Types;
+      requires is_index_set_codim<IS>::value; // Start recursion on codim entities
+    };
+}
+#endif
 
     struct IndexSet
     {
       template<class IS>
       auto require(IS&& is) -> decltype(
-        requireConvertible<int>(IS::dimension),
-        requireType<typename IS::IndexType>(),
-        requireTrue<std::is_integral<typename IS::IndexType>::value>(),
-        requireType<typename IS::Types>(),
-        requireConvertible<typename IS::Types>(is.types(/*codim*/ int{} )),
-        requireConvertible<typename IS::IndexType>(is.size(/*geometry_type*/ Dune::GeometryType{} )),
-        requireConvertible<typename IS::IndexType>(is.size(/*codim*/ int{} )),
-        requireConcept<IndexSetEntityCodim<IS::dimension>,IS>(),
-        requireTrue<not std::is_copy_constructible<IS>::value>(),
-        requireTrue<not std::is_copy_assignable<IS>::value>()
+      requireConvertible< int                     >( IS::dimension                                    ),
+      requireConvertible< typename IS::Types      >( is.types(/*codim*/ int{} )                       ),
+      requireConvertible< typename IS::IndexType  >( is.size(/*geometry_type*/ Dune::GeometryType{} ) ),
+      requireConvertible< typename IS::IndexType  >( is.size(/*codim*/ int{} )                        ),
+      requireTrue< std::is_integral<typename IS::IndexType>::value  >(),
+      requireTrue< not std::is_copy_constructible<IS>::value        >(),
+      requireTrue< not std::is_copy_assignable<IS>::value           >(),
+      requireType<typename IS::Types>(),
+      requireConcept<IndexSetEntityCodim<IS::dimension>,IS>()
       );
     };
 
@@ -78,7 +124,11 @@ namespace Dune {
   template <class IS>
   constexpr void expectIndexSet()
   {
+#if DUNE_HAVE_CXX_CONCEPTS
+    static_assert(Concept::Concept::IndexSet<IS>);
+#else
     static_assert(models<Concept::IndexSet, IS>());
+#endif
   }
 
   template <class IS>
