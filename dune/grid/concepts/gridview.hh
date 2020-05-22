b261227d76c36ea114a0195bc60c77f05436203f
Santiago Ospina De Los Rios <sospinar@gmail.com>
2020-05-22 09:43:12 +0200

    Remove usage of AnyType concept helper
    This type was not in the end all that necessary, so it's better to remove it

--- dune/grid/concepts/gridview.hh
+++ dune/grid/concepts/gridview.hh
@@ -1,7 +1,6 @@
 #ifndef DUNE_GRID_CONCEPTS_GRIDVIEW_HH
 #define DUNE_GRID_CONCEPTS_GRIDVIEW_HH
 
-#include <dune/grid/concepts/anytype.hh>
 #include <dune/grid/concepts/entity.hh>
 #include <dune/grid/concepts/entityiterator.hh>
 #include <dune/grid/concepts/intersection.hh>
@@ -94,7 +93,10 @@ namespace Dune {
 
       // stop recursion
       template<>
-      struct GridViewCodim<-1> : public AnyType {};
+      struct GridViewCodim<-1> {
+        template<class E>
+        std::true_type require(E&& e);
+      };
 
     } // nampespace Fallback
 
