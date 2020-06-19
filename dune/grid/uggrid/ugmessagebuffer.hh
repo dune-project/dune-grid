296b4be2bcf43d5eab4757479fe55ac933d27b71
Oliver Sander <oliver.sander@tu-dresden.de>
2020-06-19 10:18:47 +0200

    Simplify the implementation of UGMessageBuffer
    With 'if constexpr' there is no more need for the different template
    specializations and strange mutual inheritance.

--- dune/grid/uggrid/ugmessagebuffer.hh
+++ dune/grid/uggrid/ugmessagebuffer.hh
@@ -12,21 +12,17 @@
 namespace Dune {
 
   /** converts the UG speak message buffers to DUNE speak and vice-versa */
-  template <class DataHandle, int GridDim, int codim>
-  class UGMessageBufferBase {
-  protected:
-    typedef UGMessageBufferBase<DataHandle, GridDim, codim>  ThisType;
-    typedef UGGrid<GridDim>                              GridType;
+  template <class DataHandle, int gridDim, int codim>
+  class UGMessageBuffer
+  {
+    typedef UGMessageBuffer<DataHandle, gridDim, codim>  ThisType;
+    typedef UGGrid<gridDim>                              GridType;
     typedef typename DataHandle::DataType DataType;
 
-    enum {
-      dim = GridDim
-    };
-
-    using Entity = typename UGGrid<dim>::template Codim<codim>::Entity;
+    using Entity = typename UGGrid<gridDim>::template Codim<codim>::Entity;
     using EntityImp = typename Entity::Implementation;
 
-    UGMessageBufferBase(void *ugData)
+    UGMessageBuffer(void *ugData)
     {
       ugData_ = static_cast<char*>(ugData);
     };
@@ -38,8 +34,8 @@ namespace Dune {
     void read(DataType &t)
     { this->readRaw_<DataType>(t);  }
 
-  protected:
-    friend class Dune::UGGrid<dim>;
+  private:
+    friend class Dune::UGGrid<gridDim>;
 
     template <class ValueType>
     void writeRaw_(const ValueType &v)
@@ -69,21 +65,19 @@ namespace Dune {
 #if DUNE_UGGRID_HAVE_DDDCONTEXT
       DDD::DDDContext&,
 #endif
-      typename UG_NS<dim>::DDD_OBJ obj, void* data)
+      typename UG_NS<gridDim>::DDD_OBJ obj, void* data)
     {
       // cast the DDD object to a UG entity pointer
-      auto ugEP = reinterpret_cast<typename Dune::UG_NS<dim>::template Entity<codim>::T*>(obj);
+      auto ugEP = reinterpret_cast<typename Dune::UG_NS<gridDim>::template Entity<codim>::T*>(obj);
 
       // construct a DUNE entity from the UG entity pointer
-      /** \bug The nullptr argument should actually be the UGGrid object.  But that is hard to obtain here,
-       * and the argument is (currently) only used for the boundarySegmentIndex method, which we don't call. */
       Entity entity(EntityImp(ugEP, grid_));
 
       // safety check to only communicate what is needed
-      if ((level == -1 && UG_NS<dim>::isLeaf(ugEP)) || entity.level() == level)
+      if ((level == -1 && UG_NS<gridDim>::isLeaf(ugEP)) || entity.level() == level)
       {
         ThisType msgBuf(static_cast<DataType*>(data));
-        if (!duneDataHandle_->fixedSize(dim, codim))
+        if (!duneDataHandle_->fixedSize(gridDim, codim))
           msgBuf.template writeRaw_<unsigned>(duneDataHandle_->size(entity));
         duneDataHandle_->gather(msgBuf, entity);
       }
@@ -97,22 +91,20 @@ namespace Dune {
 #if DUNE_UGGRID_HAVE_DDDCONTEXT
       DDD::DDDContext&,
 #endif
-      typename UG_NS<dim>::DDD_OBJ obj, void* data)
+      typename UG_NS<gridDim>::DDD_OBJ obj, void* data)
     {
       // cast the DDD object to a UG entity pointer
-      auto ugEP = reinterpret_cast<typename Dune::UG_NS<dim>::template Entity<codim>::T*>(obj);
+      auto ugEP = reinterpret_cast<typename Dune::UG_NS<gridDim>::template Entity<codim>::T*>(obj);
 
       // construct a DUNE entity from the UG entity pointer
-      /** \bug The nullptr argument should actually the UGGrid object.  But that is hard to obtain here,
-       * and the argument is (currently) only used for the boundarySegmentIndex method, which we don't call. */
       Entity entity(EntityImp(ugEP, grid_));
 
       // safety check to only communicate what is needed
-      if ((level == -1 && UG_NS<dim>::isLeaf(ugEP)) || entity.level() == level)
+      if ((level == -1 && UG_NS<gridDim>::isLeaf(ugEP)) || entity.level() == level)
       {
         ThisType msgBuf(static_cast<DataType*>(data));
         int size;
-        if (!duneDataHandle_->fixedSize(dim, codim))
+        if (!duneDataHandle_->fixedSize(gridDim, codim))
           msgBuf.readRaw_(size);
         else
           size = duneDataHandle_->size(entity);
@@ -124,122 +116,56 @@ namespace Dune {
       return 0;
     }
 
-    static const GridType* grid_;
-    static DataHandle *duneDataHandle_;
-    static int level;
-    char *ugData_;
-  };
-
-  template <class DataHandle, int GridDim, int codim>
-  class UGMessageBuffer
-    : public UGMessageBufferBase<DataHandle, GridDim, codim>
-  {
-    typedef typename DataHandle::DataType DataType;
-    typedef UGMessageBufferBase<DataHandle, GridDim, codim> Base;
-    enum { dim = GridDim };
-
-  protected:
-    friend class Dune::UGGrid<dim>;
-
-    UGMessageBuffer(void *ugData)
-      : Base(ugData)
-    {}
-
     // returns number of bytes required for the UG message buffer
     template <class GridView>
-    static unsigned ugBufferSize_(const GridView &gv)
+    static unsigned ugBufferSize(const GridView &gv)
     {
-      if (Base::duneDataHandle_->fixedSize(dim, codim)) {
-        return sizeof(DataType)
-               * Base::duneDataHandle_->size(*gv.template begin<codim,InteriorBorder_Partition>());
-      }
-
-      // iterate over all entities, find the maximum size for
-      // the current rank
-      int maxSize = 0;
-      for (const auto& entity : entities(gv, Codim<codim>(), Dune::Partitions::all))
+      // If the data handle claims to have the same number of data items for each entity
+      // of the given codimension, then just ask for that numbering giving the first entity.
+      if (duneDataHandle_->fixedSize(gridDim, codim))
       {
-        maxSize = std::max((int) maxSize,
-                           (int) Base::duneDataHandle_->size(entity));
-      }
-
-      // find maximum size for all ranks
-      maxSize = gv.comm().max(maxSize);
-      if (!maxSize)
-        return 0;
-
-      // add the size of an unsigned integer to the actual
-      // buffer size. (we somewhere have to store the actual
-      // number of objects for each entity.)
-      return sizeof(unsigned) + sizeof(DataType)*maxSize;
-    }
-  };
-
-  template <class DataHandle, int GridDim, int codim>
-  class UGEdgeAndFaceMessageBuffer
-    : public UGMessageBufferBase<DataHandle, GridDim, codim>
-  {
-    enum {dim = GridDim};
-    typedef typename DataHandle::DataType DataType;
-    typedef UGMessageBufferBase<DataHandle, GridDim, codim> Base;
-  protected:
-    friend class Dune::UGGrid<dim>;
-
-    UGEdgeAndFaceMessageBuffer(void *ugData)
-      : Base(ugData)
-    {}
-
-    // returns number of bytes required for the UG message buffer
-    template <class GridView>
-    static unsigned ugBufferSize_(const GridView &gv)
-    {
-      if (Base::duneDataHandle_->fixedSize(dim, codim)) {
         auto element = gv.template begin<0, InteriorBorder_Partition>();
         return sizeof(DataType)
-               * Base::duneDataHandle_->size(element->template subEntity<codim>(0));
+               * duneDataHandle_->size(element->template subEntity<codim>(0));
       }
 
       // iterate over all entities, find the maximum size for
       // the current rank
-      int maxSize = 0;
-      for (const auto& element : elements(gv, Dune::Partitions::all))
+      std::size_t maxSize = 0;
+      if constexpr (codim==gridDim || codim==0)
       {
-        int numberOfSubentities = element.subEntities(codim);
-        for (int k = 0; k < numberOfSubentities; k++)
+        for (const auto& entity : entities(gv, Codim<codim>(), Dune::Partitions::all))
+          maxSize = std::max(maxSize, duneDataHandle_->size(entity));
+      }
+      else
+      {
+        for (const auto& element : elements(gv, Dune::Partitions::all))
         {
-          const auto subEntity = element.template subEntity<codim>(k);
+          int numberOfSubentities = element.subEntities(codim);
+          for (int k = 0; k < numberOfSubentities; k++)
+          {
+            const auto subEntity = element.template subEntity<codim>(k);
 
-          maxSize = std::max((int) maxSize,
-                             (int) Base::duneDataHandle_->size(subEntity));
+            maxSize = std::max(maxSize, duneDataHandle_->size(subEntity));
+          }
         }
       }
 
       // find maximum size for all ranks
       maxSize = gv.comm().max(maxSize);
-      if (!maxSize)
+      if (maxSize==0)
         return 0;
 
-      // add the size of an unsigned integer to the actual
-      // buffer size. (we somewhere have to store the actual
-      // number of objects for each entity.)
+      // Add the size of an unsigned integer to the actual buffer size,
+      // for storing the actual number of objects for each entity.
       return sizeof(unsigned) + sizeof(DataType)*maxSize;
     }
+
+    static const GridType* grid_;
+    static DataHandle *duneDataHandle_;
+    static int level;
+    char *ugData_;
   };
-
-  template <class DataHandle>
-  class UGMessageBuffer<DataHandle, 2, 1>
-    : public UGEdgeAndFaceMessageBuffer<DataHandle, 2, 1>
-  {};
-
-  template <class DataHandle>
-  class UGMessageBuffer<DataHandle, 3, 2>
-    : public UGEdgeAndFaceMessageBuffer<DataHandle, 3, 2>
-  {};
-
-  template <class DataHandle>
-  class UGMessageBuffer<DataHandle, 3, 1>
-    : public UGEdgeAndFaceMessageBuffer<DataHandle, 3, 1>
-  {};
 
 }   // end namespace Dune
 
