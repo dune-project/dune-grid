786dd04fe911567ed11e8c3ed01ca67a050133b8
Simon Praetorius <simon.praetorius@tu-dresden.de>
2020-11-06 12:46:54 +0000

    Cleanup the Psurface cmake find module to remove warnings

--- cmake/modules/AddPsurfaceFlags.cmake
+++ cmake/modules/AddPsurfaceFlags.cmake
@@ -10,14 +10,18 @@
 #       the targets to add the Psurface flags to.
 #
 
+# set HAVE_PSURFACE for config.h
+set(HAVE_PSURFACE ${Psurface_FOUND})
+
+# register all psurface related flags
+if(Psurface_FOUND)
+  dune_register_package_flags(LIBRARIES "Psurface::Psurface")
+endif()
+
 function(add_dune_psurface_flags _targets)
-  if(PSURFACE_FOUND)
+  if(Psurface_FOUND)
     foreach(_target ${_targets})
-      target_link_libraries(${_target} ${PSURFACE_DUNE_LIBRARIES})
-      get_target_property(_props ${_target} COMPILE_FLAGS)
-      string(REPLACE "_props-NOTFOUND" "" _props "${_props}")
-      set_target_properties(${_target} PROPERTIES COMPILE_FLAGS
-        "${_props} ${PSURFACE_DUNE_COMPILE_FLAGS}")
+      target_link_libraries(${_target} PUBLIC Psurface::Psurface)
     endforeach(_target ${_targets})
-  endif(PSURFACE_FOUND)
+  endif(Psurface_FOUND)
 endfunction(add_dune_psurface_flags)
