895d48634d7c8e7e26913a234338f6b6101a512a
Robert K <robertk@posteo.org>
2020-11-12 11:13:07 +0100

    [cleanup][Singleton] remove unnecessary code from singleton implementation.

--- dune/python/grid/singleton.hh
+++ dune/python/grid/singleton.hh
@@ -7,11 +7,7 @@
 #include <memory>
 #include <typeindex>
 #include <utility>
-#include <map>
 #include <unordered_map>
-#include <vector>
-
-#include <dune/common/visibility.hh>
 
 namespace Dune
 {
@@ -32,12 +28,7 @@ namespace Dune
       {}
     };
 
-    struct NullDeleter
-    {
-      void operator()(Item *p) {}
-    };
-
-    typedef std::shared_ptr< Item > PointerType; // TODO fix issue with unique_ptr
+    typedef std::shared_ptr< Item > PointerType;
     typedef std::type_index         KeyType;
 
     typedef std::unordered_map< KeyType, PointerType > StorageType;
@@ -45,33 +36,18 @@ namespace Dune
   private:
     StorageType storage_;
 
-    static const bool placeStaticVariableInline = false ;
-
   public:
-    ~SingletonStorage() {std::cout << "SingletonStorage\n";}
     /** \brief return singleton instance of given Object type.
      */
     template <class Object, class... Args>
     Object& instance(Args &&... args)
     {
-      // this way of creating variables only works with gcc, not with clang
-      if constexpr ( placeStaticVariableInline )
       {
-        static Object obj( std::forward< Args >( args )...);
-        return obj;
-      }
-      else
-      {
-        //std::cout << "Accessing Object " << typeid(Object).name() << std::endl;
-        //std::cout << "typeindex = " << std::type_index(typeid(Object)).hash_code() << std::endl;
-
         typedef ItemWrapper< Object > ItemWrapperType;
         PointerType& ptr = storage_[ std::type_index(typeid(Object)) ];
         if( ! ptr )
         {
           ptr.reset( new ItemWrapperType(std::forward< Args >( args )...) );
-          //std::cout << "Create Object " << typeid(Object).name() << std::endl;
-          //std::cout << "typeindex = " << std::type_index(typeid(Object)).hash_code() << std::endl;
         }
         assert( dynamic_cast< ItemWrapperType* > (ptr.operator->()) );
         return static_cast< ItemWrapperType& > (*ptr).obj_;
