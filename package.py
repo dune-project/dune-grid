ca2cef2df6e251221a8d1b96e2fac33824584ab2
Samuel Burbulla <samuel.burbulla@mathematik.uni-stuttgart.de>
2020-11-24 11:04:59 +0100

    Replace setup.py by package.py.

---
+++ package.py
@@ -0,0 +1,11 @@
+# Meta data
+name="dune-grid"
+version="2.8"
+author="The Dune Core developers"
+author_email="dune@lists.dune-project.org"
+description="The Dune grid interface and some grid implementations"
+url="https://gitlab.dune-project.org/core/dune-grid"
+install_requires=[]
+
+# DUNE dependencies
+dune_dependencies=['dune-geometry']
