da21813c74ed1f1a72f181f8ce3914ce54c010a7
Simon Praetorius <simon.praetorius@tu-dresden.de>
2021-01-15 17:00:12 +0000

    Revert "Merge branch 'feature/find_alberta_cleanup' into 'master'"
    This reverts merge request !455

--- cmake/modules/AddAlbertaFlags.cmake
+++ cmake/modules/AddAlbertaFlags.cmake
@@ -9,47 +9,81 @@
 #
 #       The targets to add the Alberta flags to.
 #
+#    .. cmake_param:: OBJECT|SOURCE_ONLY
+#       :option:
+#
+#       TODO: doc me
+#
+#    .. cmake_param:: USE_GENERIC
+#       :option:
+#
+#       TODO doc me
+#
+#    .. cmake_param:: GRIDDIM
+#       :single:
+#
+#       The dimension of the grid, defaults to 2.
+#
 #    .. cmake_param:: WORLDDIM
 #       :single:
 #
-#       The dimension of the world space
+#       The dimension of the world space, defaults to :code:`GRIDDIM`.
 #
 
-# set HAVE_ALBERTA for config.h
-set(HAVE_ALBERTA ${Alberta_FOUND})
-
-# register all Alberta related flags
-if(Alberta_FOUND)
-  include(GridType)
-  dune_define_gridtype(GRID_CONFIG_H_BOTTOM ASSERTION WORLDDIM == ALBERTA_DIM
-    GRIDTYPE ALBERTAGRID DUNETYPE "Dune::AlbertaGrid< dimgrid >"
-    HEADERS dune/grid/albertagrid.hh dune/grid/albertagrid/dgfparser.hh)
-endif(Alberta_FOUND)
-
 macro(add_dune_alberta_flags)
-  if(Alberta_FOUND)
+  if(ALBERTA_FOUND)
     include(CMakeParseArguments)
-    cmake_parse_arguments(_ARG "" "WORLDDIM;GRIDDIM" "" ${ARGN})
-    set(_ARG_TARGETS ${_ARG_UNPARSED_ARGUMENTS})
+    cmake_parse_arguments(ADD_ALBERTA
+      "OBJECT;SOURCE_ONLY;USE_GENERIC;NO_LINK_DUNEALBERTAGRID" "GRIDDIM;WORLDDIM" "" ${ARGN})
 
-    # extract dimension arguments
-    set(ALBERTA_DIM ${_ARG_WORLDDIM} ${_ARG_GRIDDIM})
-    list(SORT ALBERTA_DIM ORDER DESCENDING)
-    list(GET ALBERTA_DIM 0 WORLDDIM)
-    if(NOT WORLDDIM)
-      message(WARNING "Alberta dimension not set. Please set it, e.g. use "
-                      "add_dune_alberta_flags(WORLDDIM 2 <target>). "
-                      "Falling back to dimension 2.")
-      set(WORLDDIM 2)
-    elseif(NOT ${WORLDDIM} IN_LIST ALBERTA_WORLD_DIMS)
-      message(FATAL_ERROR "There is no alberta library for dimension ${WORLDDIM}.")
-    endif()
+    if(ADD_ALBERTA_GRIDDIM AND NOT ADD_ALBERTA_WORLDDIM)
+      set(ADD_ALBERTA_WORLDDIM ${ADD_ALBERTA_GRIDDIM})
+    endif(ADD_ALBERTA_GRIDDIM AND NOT ADD_ALBERTA_WORLDDIM)
 
-    # link to ALBERTA libraries
-    foreach(_target ${_ARG_TARGETS})
-      target_link_libraries(${_target} PUBLIC
-        Alberta::Alberta Alberta::Alberta_${WORLDDIM}D)
-      target_compile_definitions(${_target} PUBLIC "ENABLE_ALBERTA=1")
-    endforeach(_target)
-  endif(Alberta_FOUND)
+    if(ADD_ALBERTA_WORLDDIM AND NOT ADD_ALBERTA_GRIDDIM)
+      set(ADD_ALBERTA_GRIDDIM ${ADD_ALBERTA_WORLDDIM})
+    endif(ADD_ALBERTA_WORLDDIM AND NOT ADD_ALBERTA_GRIDDIM)
+
+    if(NOT ADD_ALBERTA_WORLDDIM)
+      message(WARNING "Alberta dimension not set. Please set it, e.g. use add_dune_alberta_flags(GRIDDIM 2 <target>). Falling back to dimension 2.")
+      set(ADD_ALBERTA_WORLDDIM 2)
+      set(ADD_ALBERTA_GRIDDIM ${ADD_ALBERTA_WORLDDIM})
+    else(NOT ADD_ALBERTA_WORLDDIM)
+      list(FIND ALBERTA_WORLD_DIMS ${ADD_ALBERTA_WORLDDIM} _index)
+      if(_index EQUAL -1)
+        message(FATAL_ERROR "There is no alberta library for dimension ${ADD_ALBERTA_WORLDDIM}.")
+      endif(_index EQUAL -1)
+    endif(NOT ADD_ALBERTA_WORLDDIM)
+
+    if(ADD_ALBERTA_SOURCE_ONLY)
+      set(_prefix SOURCE)
+      set(_source_only SOURCE_ONLY)
+    else()
+      set(_prefix TARGET)
+      if(NOT ADD_ALBERTA_OBJECT)
+        # link to ALBERTA libraries
+        foreach(_target ${ADD_ALBERTA_UNPARSED_ARGUMENTS})
+          if(NOT ADD_ALBERTA_NO_LINK_DUNEALBERTAGRID)
+            target_link_libraries(${_target}
+              PUBLIC dunealbertagrid_${ADD_ALBERTA_GRIDDIM}d)
+          endif(NOT ADD_ALBERTA_NO_LINK_DUNEALBERTAGRID)
+          target_link_libraries(${_target}
+            PUBLIC
+            ${ALBERTA_${ADD_ALBERTA_WORLDDIM}D_LIB}
+            dunegrid ${DUNE_LIBS} ${ALBERTA_UTIL_LIB} ${ALBERTA_EXTRA_LIBS})
+        endforeach(_target ${ADD_ALBERTA_UNPARSED_ARGUMENTS})
+      endif()
+    endif(ADD_ALBERTA_SOURCE_ONLY)
+
+    include_directories(${ALBERTA_INCLUDES})
+    set_property(${_prefix} ${ADD_ALBERTA_UNPARSED_ARGUMENTS}
+      APPEND PROPERTY
+      COMPILE_DEFINITIONS ENABLE_ALBERTA
+      ALBERTA_DIM=${ADD_ALBERTA_WORLDDIM})
+    if(ADD_ALBERTA_USE_GENERIC)
+      set_property(${_prefix} ${ADD_ALBERTA_UNPARSED_ARGUMENTS}
+        APPEND PROPERTY
+        COMPILE_DEFINITIONS DUNE_ALBERTA_USE_GENERICGEOMETRY=1)
+    endif(ADD_ALBERTA_USE_GENERIC)
+  endif(ALBERTA_FOUND)
 endmacro(add_dune_alberta_flags)
--- cmake/modules/FindAlberta.cmake
+++ cmake/modules/FindAlberta.cmake
@@ -1,127 +1,152 @@
-#[=======================================================================[.rst:
-FindAlberta
------------
+# .. cmake_module::
+#
+#    Module that checks whether the Alberta grid manager is available and usable.
+#
+#    Variables used by this module which you may want to set:
+#
+#    :ref:`ALBERTA_ROOT`
+#       Path list to search for psurface
+#
+#    Sets the follwing variables:
+#
+#    :code:`ALBERTA_FOUND`
+#       True if Alberta available and usable.
+#
+#    :code:`ALBERTA_VERSION`
+#       The version number of the found Alberta package
+#
+#    TODO: document this more, there was 0 doc before!
+#
+# .. cmake_variable:: ALBERTA_ROOT
+#
+#    You may set this variable to have :ref:`FindAlberta` look
+#    for the Alberta package in the given path before inspecting
+#    system paths.
+#
 
-Find Alberta, an Adaptive multiLevel finite element toolbox using Bisectioning
-refinement and Error control by Residual Techniques for scientific Applications.
-(see https://gitlab.mathematik.uni-stuttgart.de/ians-nmh/alberta/alberta3)
+macro(_dune_set_alberta val)
+  set(ALBERTA_FOUND ${val})
+  set(HAVE_ALBERTA ${val})
+endmacro(_dune_set_alberta val)
 
-Imported targets
-^^^^^^^^^^^^^^^^
-
-This module defines the following :prop_tgt:`IMPORTED` target:
-
-``Alberta::Alberta``
-  The libraries, flags, and includes to use for Alberta, if found.
-
-``Alberta::Alberta_XD``
-  Dimension dependent library
-
-Result Variables
-^^^^^^^^^^^^^^^^
-
-This module defines the following variables:
-
-``Alberta_FOUND``
-  The Alberta library with all its dependencies is found
-
-Cache Variables
-^^^^^^^^^^^^^^^
-
-The following variables may be set to influence this module's behavior:
-
-``Alberta_ROOT``
-  Base directory of Alberta installation
-
-``ALBERTA_EXTRA_LIBS``
-  Libraries needed to link Alberta, e.g. m
-
-#]=======================================================================]
-
-# text for feature summary
-include(FeatureSummary)
-set_package_properties("Alberta" PROPERTIES
-  DESCRIPTION "An adaptive hierarchical finite element toolbox and grid manager")
-
+set(ALBERTA_LIBCHECK ON CACHE BOOL "Whether to try to link against libalberta_Nd.")
+set(ALBERTA_ROOT "" CACHE FILEPATH "Root directory of Alberta installation.")
 set(ALBERTA_EXTRA_LIBS "m" CACHE FILEPATH "Extra libraries needed by alberta for linking.")
-
-# look for header alberta/alberta.h
-find_path(ALBERTA_INCLUDE_DIR "alberta/alberta.h")
-
-# check version, Alberta 2 or 3
-set(ALBERTA_VERSION 2.0)
-set(DUNE_ALBERTA_VERSION 0x200)
-find_file(ALBERTA_HEADER "alberta/alberta.h"
-  HINTS ${ALBERTA_INCLUDE_DIR}
-  NO_DEFAULT_PATH)
-if(ALBERTA_HEADER)
-  cmake_push_check_state()
-  list(APPEND CMAKE_REQUIRED_DEFINITIONS -DDIM_OF_WORLD=3 -DDEL_INDEX=0)
-  list(APPEND CMAKE_REQUIRED_INCLUDES ${ALBERTA_INCLUDE_DIR})
-
-  include(CheckStructHasMember)
-  check_struct_has_member("struct el_info" wall_bound ${ALBERTA_HEADER} ALBERTA_IS_VERSION_3)
-  if(ALBERTA_IS_VERSION_3)
-    set(ALBERTA_VERSION 3.0)
-    set(DUNE_ALBERTA_VERSION 0x300)
-  endif()
-  cmake_pop_check_state()
-endif()
-unset(ALBERTA_HEADER CACHE)
-
 # check for ltdl
-find_library(ALBERTA_LTDL_LIB "ltdl")
-if(ALBERTA_LTDL_LIB)
+find_library(ALBERTA_LTDL_LIB
+  NAMES ltdl
+  PATH_SUFFIXES lib lib32 lib64
+)
+if (ALBERTA_LTDL_LIB)
   list(APPEND ALBERTA_EXTRA_LIBS "ltdl")
 endif()
 
-# look for alberta utilitiy libraries
-find_library(ALBERTA_UTIL_LIB
-  NAMES alberta_util alberta_utilities)
+# look for header alberta/alberta.h
+find_path(ALBERTA_INCLUDE_DIR
+  NAMES alberta/alberta.h
+  PATHS ${ALBERTA_ROOT}
+  PATH_SUFFIXES alberta include NO_DEFAULT_PATH
+  DOC "Include path of Alberta")
+find_path(ALBERTA_INCLUDE_DIR
+  NAMES
+  alberta/alberta.h
+  PATHS /usr/local /opt
+  PATH_SUFFIXES alberta)
 
+if(NOT ALBERTA_INCLUDE_DIR)
+  _dune_set_alberta(FALSE)
+  return()
+endif(NOT ALBERTA_INCLUDE_DIR)
+
+set(ALBERTA_INCLUDES ${ALBERTA_INCLUDE_DIR} ${ALBERTA_INCLUDE_DIR}/alberta)
+set(ALBERTA_VERSION 2.0)
+
+cmake_push_check_state()
+set(CMAKE_REQUIRED_DEFINITIONS ${CMAKE_REQUIRED_DEFINITIONS} -DDIM_OF_WORLD=3 -DDEL_INDEX=0)
+set(CMAKE_REQUIRED_INCLUDES ${CMAKE_REQUIRED_INCLUDES} ${ALBERTA_INCLUDES})
+
+include(CheckIncludeFiles)
+check_include_files(alberta/alberta.h ALBERTA_FOUND)
+
+if(NOT ALBERTA_FOUND)
+  message(WARNING "Header alberta/alberta.h not found.")
+  cmake_pop_check_state()
+  _dune_set_alberta(FALSE)
+  return()
+endif(NOT ALBERTA_FOUND)
+
+# check version, Alberta 2 or 3
+include(CheckStructHasMember)
+check_struct_has_member ("struct el_info" wall_bound alberta/alberta.h ALBERTA_IS_VERSION_3)
+if(ALBERTA_IS_VERSION_3)
+  set(ALBERTA_VERSION 3.0)
+endif(ALBERTA_IS_VERSION_3)
+
+# look for libraries
+find_library(ALBERTA_UTIL_LIB
+  NAMES alberta_util alberta_utilities
+  PATHS ${ALBERTA_ROOT}
+  PATH_SUFFIXES lib lib32 lib64
+  NO_DEFAULT_PATH)
+find_library(ALBERTA_UTIL_LIB
+  NAMES alberta_util alberta_utilities
+  PATH_SUFFIXES lib lib32 lib64)
+
+if(ALBERTA_UTIL_LIB)
+  set(_CMAKE_REQUIRED_LIBRARIES_SAVE ${CMAKE_REQUIRED_LIBRARIES})
+  set(CMAKE_REQUIRED_LIBRARIES ${CMAKE_REQUIRED_LIBRARIES} ${ALBERTA_UTIL_LIB})
+  check_library_exists(${ALBERTA_UTIL_LIB} alberta_calloc "" _ALBERTA_UTIL_LIB_FUNCTIONAL)
+  if(NOT _ALBERTA_UTIL_LIB_FUNCTIONAL)
+    message(WARNING "Could not find symbol alberta_calloc in ${ALBERTA_UTIL_LIB}")
+    cmake_pop_check_state()
+    _dune_set_alberta(FALSE)
+    return()
+  endif(NOT _ALBERTA_UTIL_LIB_FUNCTIONAL)
+else(ALBERTA_UTIL_LIB)
+  message(WARNING "Could not find library alberta_util or alberta_utilities")
+  cmake_pop_check_state()
+  _dune_set_alberta(FALSE)
+endif(ALBERTA_UTIL_LIB)
+
+set(CMAKE_REQUIRED_LIBRARIES ${CMAKE_REQUIRED_LIBRARIES} ${ALBERTA_EXTRA_LIBS})
 
 # check for which dimensions are supported Alberta installation
-cmake_push_check_state()
-list(APPEND CMAKE_REQUIRED_LIBRARIES "${ALBERTA_EXTRA_LIBS};${ALBERTA_UTIL_LIB}")
-
-foreach(dim RANGE 1 9)
-  find_library(ALBERTA_${dim}D_LIB alberta_${dim}d)
-  if(ALBERTA_${dim}D_LIB)
-    check_library_exists(${ALBERTA_${dim}D_LIB} mesh_traverse "" ALBERTA_${dim}D_LIB_FOUND)
-    if(ALBERTA_${dim}D_LIB_FOUND)
-      list(APPEND ALBERTA_WORLD_DIMS ${dim})
-    endif()
-  endif()
-  mark_as_advanced(ALBERTA_${dim}D_LIB)
-endforeach(dim)
-
-cmake_pop_check_state()
-message(STATUS "Found alberta libraries for dimensions ${ALBERTA_WORLD_DIMS}")
-
-# behave like a CMake module is supposed to behave
-include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args("Alberta"
-  REQUIRED_VARS
-    ALBERTA_INCLUDE_DIR ALBERTA_UTIL_LIB ALBERTA_WORLD_DIMS
-  VERSION_VAR
-    ALBERTA_VERSION
-)
-
-mark_as_advanced(ALBERTA_INCLUDE_DIR ALBERTA_UTIL_LIB ALBERTA_WORLD_DIMS)
-
-if(Alberta_FOUND AND NOT TARGET Alberta::Alberta)
-  add_library(Alberta::Alberta UNKNOWN IMPORTED)
-  set_target_properties(Alberta::Alberta PROPERTIES
-    IMPORTED_LOCATION ${ALBERTA_UTIL_LIB}
-    INTERFACE_INCLUDE_DIRECTORIES ${ALBERTA_INCLUDE_DIR}
-    INTERFACE_LINK_LIBRARIES "${ALBERTA_EXTRA_LIBS}")
-
+if(ALBERTA_LIBCHECK)
   foreach(dim RANGE 1 9)
-    if(ALBERTA_${dim}D_LIB AND NOT Alberta::Alberta_${dim}D)
-      add_library(Alberta::Alberta_${dim}D UNKNOWN IMPORTED)
-      set_target_properties(Alberta::Alberta_${dim}D PROPERTIES
-        IMPORTED_LOCATION ${ALBERTA_${dim}D_LIB}
-        INTERFACE_COMPILE_DEFINITIONS ALBERTA_DIM=${dim})
-    endif()
-  endforeach(dim)
+    find_library(ALBERTA_${dim}D_LIB alberta_${dim}d
+      PATHS ${ALBERTA_ROOT}
+      PATH_SUFFIXES lib lib32 lib64
+      Cache FILEPATH DOC "Alberta lib for ${dim}D" NO_DEFAULT_PATH)
+    find_library(ALBERTA_${dim}D_LIB alberta_${dim}d  PATH_SUFFIXES lib lib32 lib64)
+    if(ALBERTA_${dim}D_LIB)
+      #set(CMAKE_REQUIRED_LIBRARIES ${_CMAKE_REQUIRED_LIBRARIES_OLD} ${ALBERTA_${dim}D_LIB} ${ALBERTA_UTIL_LIB} ${DUNE_LIBS})
+      check_library_exists(${ALBERTA_${dim}D_LIB} mesh_traverse "" ALBERTA_${dim}D_LIB_FOUND)
+      if(ALBERTA_${dim}D_LIB_FOUND)
+        list(APPEND ALBERTA_WORLD_DIMS ${dim})
+      endif(ALBERTA_${dim}D_LIB_FOUND)
+    endif(ALBERTA_${dim}D_LIB)
+  endforeach(dim RANGE 1 9)
+  message(STATUS "Found alberta libraries for dimensions ${ALBERTA_WORLD_DIMS}")
+endif(ALBERTA_LIBCHECK)
+cmake_pop_check_state()
+
+list(LENGTH ALBERTA_WORLD_DIMS _length)
+if(_length GREATER 0)
+  _dune_set_alberta(TRUE)
 endif()
+
+if(ALBERTA_VERSION STREQUAL "3.0")
+  set(DUNE_ALBERTA_VERSION 0x300)
+else()
+  message(WARNING "Internal Inconsistency: Invalid Alberta version reported: $ALBERTA_VERSION.")
+  cmake_pop_check_state()
+  _dune_set_alberta(FALSE)
+endif()
+
+if(ALBERTA_FOUND)
+  include(GridType)
+  dune_define_gridtype(GRID_CONFIG_H_BOTTOM ASSERTION WORLDDIM == ALBERTA_DIM
+    GRIDTYPE ALBERTAGRID DUNETYPE "Dune::AlbertaGrid< dimgrid >"
+    HEADERS dune/grid/albertagrid.hh dune/grid/albertagrid/dgfparser.hh)
+  _dune_set_alberta(TRUE)
+endif(ALBERTA_FOUND)
