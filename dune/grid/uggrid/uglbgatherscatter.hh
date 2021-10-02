dune-project/dune-grid

commit de09963b3563c83c0d648873789178f06da771ba

[ug] Avoid reallocating buffer too often
[ug] Avoid reallocating buffer too often

The changes introduced in 755d86be5cd401033a19c8a51a32a1018abdbeff
introduced reallocations (shrinking) of the buffer after every read.
This reverts this behaviour to only change the internal bufferSize_ variable
and free once all items are read.

dune/grid/uggrid/uglbgatherscatter.hh
@@ -20,7 +20,11 @@ namespace Dune {
       {
         assert(bufferSize_ >= size);
         memcpy(&x, data_+bufferSize_-size, size);
-        resize(bufferSize_-size);
+        // decrease size without shrinking the allocated buffer
+        bufferSize_ =- size;
+        // if all item are read, deallocate the buffer
+        if (bufferSize_ == 0)
+          resize(0);
       }
 
       void write(const DataType& x)

commit 61637107b640fd50ab89aa6509523e493b513477

Fix wrong operator introduced in de09963b3563c83c0d648873789178f06da771ba

dune/grid/uggrid/uglbgatherscatter.hh
@@ -21,7 +21,7 @@ namespace Dune {
         assert(bufferSize_ >= size);
         memcpy(&x, data_+bufferSize_-size, size);
         // decrease size without shrinking the allocated buffer
-        bufferSize_ =- size;
+        bufferSize_ -= size;
         // if all item are read, deallocate the buffer
         if (bufferSize_ == 0)
           resize(0);