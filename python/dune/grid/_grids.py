af4964e527c7e30f4c31092398f2c311b7803d20
Christian Engwer <christi@dune-project.org>
2022-03-11 09:44:29 +0100

    [python] support construction tensor product yaspgrids

--- python/dune/grid/_grids.py
+++ python/dune/grid/_grids.py
@@ -17,35 +17,54 @@ def yaspGrid(constructor, dimgrid=None, coordinates="equidistant", ctype="double
     if not dimgrid:
         dimgrid = getDimgrid(constructor)
     if coordinates == "equidistant":
-        coordinates = "Dune::EquidistantOffsetCoordinates"
+        coordinates_name = "Dune::EquidistantOffsetCoordinates"
     elif coordinates == "tensorproduct":
-        coordinates = "Dune::TensorProductCoordinates"
-    coordinates, _ = generateTypeName(coordinates, ctype, dimgrid)
-    typeName, includes = generateTypeName("Dune::YaspGrid", dimgrid, coordinates)
+        coordinates_name = "Dune::TensorProductCoordinates"
+    coordinates_type, _ = generateTypeName(coordinates_name, ctype, dimgrid)
+    typeName, includes = generateTypeName("Dune::YaspGrid", dimgrid, coordinates_type)
     includes += ["dune/grid/yaspgrid.hh", "dune/grid/io/file/dgfparser/dgfyasp.hh"]
 
-    ctor = Constructor([
-              'Dune::FieldVector<'+ctype+', '+str(dimgrid)+'> lowerleft',
+    if coordinates == "equidistant":
+        ctor = Constructor(
+            [ 'Dune::FieldVector<'+ctype+', '+str(dimgrid)+'> lowerleft',
               'Dune::FieldVector<'+ctype+', '+str(dimgrid)+'> upperright',
               'std::array<int, '+str(dimgrid)+'> elements',
               'std::array<bool, '+str(dimgrid)+'> periodic',
-              'int overlap'],
-              ['std::bitset<'+str(dimgrid)+'> periodic_;',
-               'for (int i=0;i<'+str(dimgrid)+';++i) periodic_.set(i,periodic[i]);',
-               'return new DuneType(lowerleft,upperright,elements,periodic_,overlap);'],
-              ['"lowerleft"_a', '"upperright"_a', '"elements"_a', '"periodic"_a', '"overlap"_a'])
-
-    gridModule = module(includes, typeName, ctor)
-
-    try:
-        lowerleft  = constructor.lower
-        upperright = constructor.upper
-        elements   = constructor.division
-        periodic   = constructor.param.get("periodic", [False,]*dimgrid)
-        overlap    = constructor.param.get("overlap", 0)
-        return gridModule.HierarchicalGrid(lowerleft,upperright,elements,periodic,overlap).leafView
-    except AttributeError:
-        return gridModule.reader(constructor).leafView
+              'int overlap' ],
+            [ 'std::bitset<'+str(dimgrid)+'> periodic_;',
+              'for (int i=0;i<'+str(dimgrid)+';++i) periodic_.set(i,periodic[i]);',
+              'return new DuneType(lowerleft,upperright,elements,periodic_,overlap);' ],
+            [ '"lowerleft"_a', '"upperright"_a', '"elements"_a', '"periodic"_a', '"overlap"_a' ])
+        gridModule = module(includes, typeName, ctor)
+        try:
+            lowerleft  = constructor.lower
+            upperright = constructor.upper
+            elements   = constructor.division
+            periodic   = constructor.param.get("periodic", [False,]*dimgrid)
+            overlap    = constructor.param.get("overlap", 0)
+            return gridModule.HierarchicalGrid(lowerleft,upperright,elements,periodic,overlap).leafView
+        except AttributeError:
+            return gridModule.reader(constructor).leafView
+    elif coordinates == "tensorproduct":
+        ctor = Constructor(
+            [ 'std::array<std::vector<'+ctype+'>, '+str(dimgrid)+'> coords',
+              'std::array<bool, '+str(dimgrid)+'> periodic',
+              'int overlap' ],
+            [ 'std::bitset<'+str(dimgrid)+'> periodic_;',
+              'for (int i=0;i<'+str(dimgrid)+';++i) periodic_.set(i,periodic[i]);',
+              'return new DuneType(coords,periodic_,overlap);' ],
+            [ '"coords"_a', '"periodic"_a', '"overlap"_a' ])
+        gridModule = module(includes, typeName, ctor)
+        try:
+            coords     = constructor.coords
+            periodic   = constructor.param.get("periodic", [False,]*dimgrid)
+            overlap    = constructor.param.get("overlap", 0)
+        except AttributeError:
+            coords     = constructor
+            periodic   = [False,]*dimgrid
+            overlap    = 0
+        return gridModule.HierarchicalGrid(coords,periodic,overlap).leafView
+    assert(False)
 
 grid_registry = {
         "OneD"       : onedGrid,
