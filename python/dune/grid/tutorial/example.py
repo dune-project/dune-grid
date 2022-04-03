a77dc03dbee302dac52f2b4e072679e548602fed
Christian Engwer <christi@dune-project.org>
2022-04-03 20:52:40 +0200

    [python] update example.py to show how to specify YaspGrid with tensor coords

--- python/dune/grid/tutorial/example.py
+++ python/dune/grid/tutorial/example.py
@@ -1,26 +1,41 @@
 import math
 
+# example of how to perform operations on a given grid
+def runOnGrid(grid):
+
+    # define a grid function and visualize
+    from dune.grid import gridFunction
+    @gridFunction(grid)
+    def f(x):
+        return math.cos(2.*math.pi/(1+abs(x[0]*x[1])))
+    f.plot()
+    grid.writeVTK("example", pointdata={"gf":f})
+
+    # integrate the grid function using a quadrature rule from dune.geometry
+    from dune.geometry import quadratureRules
+    rules = quadratureRules(5)
+    l2norm2 = 0
+    for e in grid.elements:
+        geo = e.geometry
+        for qp in rules(e.type):
+            x,w = qp.position, qp.weight
+            l2norm2 += f(e,x)**2*w*geo.integrationElement(x)
+    print("integral of grid function=",math.sqrt(l2norm2))
+
 # constructed a Cartesian grid
 from dune.grid import structuredGrid
 grid = structuredGrid([-1,-1],[1,1],[10,10])
 print("number of elements of Cartesian grid:",grid.size(0))
 grid.plot()
+runOnGrid(grid)
 
-# define a grid function and visualize
-from dune.grid import gridFunction
-@gridFunction(grid)
-def f(x):
-    return math.cos(2.*math.pi/(1+abs(x[0]*x[1])))
-f.plot()
-grid.writeVTK("example", pointdata={"gf":f})
-
-# integrate the grid function using a quadrature rule from dune.geometry
-from dune.geometry import quadratureRules
-rules = quadratureRules(5)
-l2norm2 = 0
-for e in grid.elements:
-    geo = e.geometry
-    for qp in rules(e.type):
-        x,w = qp.position, qp.weight
-        l2norm2 += f(e,x)**2*w*geo.integrationElement(x)
-print("integral of grid function=",math.sqrt(l2norm2))
+# constructed YaspGrids with different coordinate types
+# yaspGrid allows to specify the coordinate type
+from dune.grid import tensorProductCoordinates
+from dune.grid import yaspGrid
+import numpy as np
+coords = tensorProductCoordinates([np.array([1,2,3,4]), np.array([10,11,33,44])], ctype='float')
+ygrid = yaspGrid(coords)
+print("number of elements of tensor YsapGrid grid:",ygrid.size(0))
+ygrid.plot()
+runOnGrid(ygrid)
